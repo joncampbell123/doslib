27a66f1bb8fabbbf7d76cfa0df83ccfd7a706df1
Jonathan Campbell <jonathan@castus.tv>
2014-06-08 00:54:36 -0700

    time to start pushing DOSLIB to github!

---
+++ git-push-to-github.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git remote add github https://joncampbell123@github.com/joncampbell123/doslib.git # might fail
+git remote set-url github https://joncampbell123@github.com/joncampbell123/doslib.git # might fail
+git push github master
+
