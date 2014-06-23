8190e2d549320771612ff9503c13138c089a4495
Jonathan Campbell <jonathan@castus.tv>
2014-06-23 00:12:38 -0700

    add "TSR" interface for detecting if it's resident, changing options on the fly, etc. added code for forcing 8-char mode.

--- hw/vga/vga240.c
+++ hw/vga/vga240.c
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 #include <conio.h> /* this is where Open Watcom hides the outp() etc. functions */
+#include <ctype.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
@@ -19,7 +20,36 @@ int			blanking_align = 0;
 void			(__interrupt __far *old_int10)();
 
 void __interrupt __far new_int10(union INTPACK ip) {
-	if (ip.h.ah == 0x00) {
+	if (ip.w.ax == 0xDFA0 && ip.w.bx == 0x1AC0) {
+		switch (ip.h.cl) {
+			case 0: /* install check */
+				ip.w.ax = 0xDFAA;
+				ip.w.bx = 0xCACA;
+				break;
+			case 1: /* change force8 option */
+				ip.w.ax = 0xDFAA;
+				ip.w.bx = 0xCACA;
+				force8 = ip.h.ch;
+				break;
+			case 2: /* change blanking fix option */
+				ip.w.ax = 0xDFAA;
+				ip.w.bx = 0xCACA;
+				blanking_fix = ip.h.ch;
+				break;
+			case 3: /* change blanking align option */
+				ip.w.ax = 0xDFAA;
+				ip.w.bx = 0xCACA;
+				blanking_align = (int)((char)ip.h.ch); /* sign extend 0xFF,0x00,0x01 to -1,0,1 */
+				break;
+			default:
+				ip.w.ax = 0xDFFF;
+				ip.w.bx = 0xCACA;
+				break;
+		};
+
+		return;
+	}
+	else if (ip.h.ah == 0x00) {
 		unsigned int vtotal = 0x20B; /* NTS: one less than actual scanlines */
 		unsigned int vretrace_s = 0x1EA;
 		unsigned int vretrace_e = 0x1EC;
@@ -154,20 +184,71 @@ static void help() {
 	printf("Intercept VGA BIOS calls to force video modes with at least 480 scan lines,\n");
 	printf("which may improve video quality with scan converters or enable capture/viewing\n");
 	printf("where devices would normally ignore 400-line output.\n");
+	printf("\n");
+	printf("If the program is resident, you can run this program with other options to\n");
+	printf("change configuration at runtime along with /SET\n");
+	printf("\n");
 	printf("(C) 2014 Jonathan Campbell\n");
 	printf("\n");
-	printf("  /NOBLANKFIX        If given, active display is extended, which may reveal\n");
-	printf("                     offscreen rendering or random data.\n");
+	printf("  /INSTALL           Make the program resident in memory.\n");
+	printf("  /SET               If the program is resident, update settings.\n");
+	printf("  /BLANKFIX          Active display is maintained from original mode,\n");
+	printf("                     blanking interval is extended to make 480 lines.\n");
+	printf("  /NOBLANKFIX        Active display is extended to 480 lines\n");
 	printf("  /BA=N              Adjust vertical retrace to center display area, where\n");
 	printf("                     N is -1 (top), 0 (center), 1 (bottom)\n");
 	printf("  /8                 Force alphanumeric modes to 8 pixels/char. Use this\n");
 	printf("                     option if your VGA scan/capture misdetects the output\n");
 	printf("                     as 640x480 instead of 720x480\n");
+	printf("  /N8                Don't force to 8 pixels/char\n");
+}
+
+int resident() {
+	unsigned int a=0,b=0;
+
+	__asm {
+		push	ax
+		push	bx
+		mov	ax,0xDFA0
+		mov	bx,0x1AC0
+		int	10h
+		mov	a,ax
+		mov	b,bx
+		pop	bx
+		pop	ax
+	}
+
+	return (a == 0xDFAA && b == 0xCACA);
+}
+
+int res_set_opt8(unsigned char func,unsigned char opt) {
+	unsigned int a=0,b=0;
+
+	__asm {
+		push	ax
+		push	bx
+		push	cx
+		mov	ax,0xDFA0
+		mov	bx,0x1AC0
+		mov	cl,func
+		mov	ch,opt
+		int	10h
+		mov	a,ax
+		mov	b,bx
+		pop	cx
+		pop	bx
+		pop	ax
+	}
+
+	return (a == 0xDFAA && b == 0xCACA);
 }
 
 void end_of_resident();
 
 int main(int argc,char **argv) {
+	unsigned char force8_set=0;
+	unsigned char blanking_fix_set=0;
+	unsigned char blanking_align_set=0;
 	char *a,*command=NULL;
 	int i;
 
@@ -183,15 +264,29 @@ int main(int argc,char **argv) {
 			if (!strcasecmp(a,"i") || !strcasecmp(a,"install")) {
 				command = a;
 			}
+			else if (!strcasecmp(a,"s") || !strcasecmp(a,"set")) {
+				command = a;
+			}
+			else if (!strcasecmp(a,"BLANKFIX")) {
+				blanking_fix_set=1;
+				blanking_fix = 1;
+			}
 			else if (!strcasecmp(a,"NOBLANKFIX")) {
+				blanking_fix_set=1;
 				blanking_fix = 0;
 			}
 			else if (!strncasecmp(a,"BA=",3)) {
 				a += 3;
 				blanking_align = atoi(a);
+				blanking_align_set = 1;
 			}
 			else if (!strcmp(a,"8")) {
+				force8_set = 1;
 				force8 = 1;
+			}
+			else if (!strcmp(a,"N8")) {
+				force8_set = 1;
+				force8 = 0;
 			}
 			else {
 				fprintf(stderr,"Unknown switch %s\n",a);
@@ -210,23 +305,65 @@ int main(int argc,char **argv) {
 		help();
 		return 1;
 	}
-	if (!probe_vga()) {
-		printf("VGA BIOS not found\n");
-		return 1;
+
+	if (tolower(*command) == 'i') {
+		if (resident()) {
+			printf("This program is already resident\n");
+			return 1;
+		}
+
+		if (!probe_vga()) {
+			printf("VGA BIOS not found\n");
+			return 1;
+		}
+
+		_cli();
+		old_int10 = _dos_getvect(0x10);
+		_dos_setvect(0x10,new_int10);
+		_sti();
+		__asm {
+			push	ax
+			mov	ax,3
+			int	10h
+			pop	ax
+		}
+		printf("INT 10h hooked\n"); fflush(stdout);
+		_dos_keep(0,(34000)>>4); /* FIXME! */
+	}
+	else if (tolower(*command) == 's') {
+		if (!resident()) {
+			printf("This program is not yet installed\n");
+			return 1;
+		}
+
+		if (force8_set) {
+			if (!res_set_opt8(1,force8)) {
+				printf("Failed to set force8\n");
+				return 1;
+			}
+		}
+		if (blanking_fix_set) {
+			if (!res_set_opt8(2,blanking_fix)) {
+				printf("Failed to set blank fix\n");
+				return 1;
+			}
+		}
+		if (blanking_align_set) {
+			if (!res_set_opt8(3,(unsigned char)blanking_align)) {
+				printf("Failed to set blank align\n");
+				return 1;
+			}
+
+		}
+
+		__asm {
+			push	ax
+			mov	ax,3
+			int	10h
+			pop	ax
+		}
 	}
 
-	_cli();
-	old_int10 = _dos_getvect(0x10);
-	_dos_setvect(0x10,new_int10);
-	_sti();
-	__asm {
-		push	ax
-		mov	ax,3
-		int	10h
-		pop	ax
-	}
-	printf("INT 10h hooked\n"); fflush(stdout);
-	_dos_keep(0,(34000)>>4); /* FIXME! */
 	return 0;
 }
 
