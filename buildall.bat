0fcdf9b2f23b51b833bbc0038be81e65152f0d38
Jonathan Campbell <jonathan@castus.tv>
2014-07-11 22:36:30 -0700

    bat files again

--- buildall.bat
+++ buildall.bat
@@ -13,3 +13,59 @@ cd ext
 call buildall.bat %WHAT%
 cd ..
 
+echo Building: hw
+cd hw
+call buildall.bat %WHAT%
+cd ..
+
+echo Building: mak
+cd mak
+call buildall.bat %WHAT%
+cd ..
+
+echo Building: media
+cd media
+call buildall.bat %WHAT%
+cd ..
+
+echo Building: misc
+cd misc
+call buildall.bat %WHAT%
+cd ..
+
+echo Building: os2
+cd os2
+call buildall.bat %WHAT%
+cd ..
+
+echo Building: tiny
+cd tiny
+call buildall.bat %WHAT%
+cd ..
+
+echo Building: tinyw16
+cd tinyw16
+call buildall.bat %WHAT%
+cd ..
+
+echo Building: tinyw32
+cd tinyw32
+call buildall.bat %WHAT%
+cd ..
+
+echo Building: tool
+cd tool
+call buildall.bat %WHAT%
+cd ..
+
+echo Building: vir
+cd vir
+call buildall.bat %WHAT%
+cd ..
+
+echo Building: windows
+cd windows
+call buildall.bat %WHAT%
+cd ..
+
+echo All done
