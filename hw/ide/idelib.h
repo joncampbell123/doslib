4c7b2f10c892f09749e206919952856f88bf735a
Jonathan Campbell <jonathan@castus.tv>
2014-08-03 03:56:46 -0700

    new IDE taskfile struct

--- hw/ide/idelib.h
+++ hw/ide/idelib.h
@@ -10,12 +10,41 @@ struct ide_controller_flags {
 	uint8_t		_reserved_:1;
 };
 
+struct ide_taskfile {
+	union {							/* 0x1F1. error/feature */
+		uint8_t			error;
+		uint8_t			features;		
+	};
+	uint16_t			sector_count;		/* 0x1F2. double wide for LBA48 */
+	union {
+		uint16_t		lba0_3;			/* 0x1F3. if LBA48, contains [15:8 = byte 3] and [7:0 = byte 0] of LBA,
+								          or contains only byte 0 of LBA, or sector number of CHS */
+		uint16_t		chs_sector;
+	};
+	union {
+		uint16_t		lba1_4;			/* 0x1F4. if LBA48, contains [15:8 = byte 4] and [7:0 = byte 1] of LBA,
+								          or contains only byte 1 of LBA, or [7:0] of cylinder in CHS */
+		uint16_t		chs_cylinder_low;
+	};
+	union {
+		uint16_t		lba2_5;			/* 0x1F5. if LBA48, contains [15:8 = byte 5] and [7:0 = byte 2] of LBA,
+								          or contains only byte 2 of LBA, or [15:8] of cylinder in CHS */
+		uint16_t		chs_cylinder_high;
+	};
+	uint8_t				head_select;		/* 0x1F6. mode [bits 7:5] select [bit 4] head [bits 3:0] */
+	union {							/* 0x1F7. command(W) and status(R) */
+		uint8_t			command;
+		uint8_t			status;
+	};
+};
+
 struct ide_controller {
 	uint16_t			base_io;		/* 0x1F0, 0x170, etc */
 	uint16_t			alt_io;			/* 0x3F6, 0x3F7, etc */
 	volatile uint16_t		irq_fired;		/* IRQ counter */
 	int8_t				irq;
 	struct ide_controller_flags	flags;
+	struct ide_taskfile		taskfile;
 	uint8_t				last_status;
 };
 
