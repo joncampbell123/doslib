ab3c04b7d61efe3229e3749ae022c54f804f6d87
Jonathan Campbell <jonathan@castus.tv>
2014-08-08 15:09:10 -0700

    more

--- hw/ide/testpwr.c
+++ hw/ide/testpwr.c
@@ -28,6 +28,8 @@
 #include "testnop.h"
 #include "testpwr.h"
 
+/* =================================== test functions ================================= */
+
 void do_drive_standby_test(struct ide_controller *ide,unsigned char which) {
 	struct vga_msg_box vgabox;
 
@@ -174,5 +176,195 @@ void do_drive_check_power_mode(struct ide_controller *ide,unsigned char which) {
 
 	wait_for_enter_or_escape();
 	vga_msg_box_destroy(&vgabox);
+}
+
+void do_drive_device_reset_test(struct ide_controller *ide,unsigned char which) {
+	struct vga_msg_box vgabox;
+
+	if (do_ide_controller_user_wait_busy_controller(ide) != 0 || do_ide_controller_user_wait_drive_ready(ide) < 0)
+		return;
+
+	idelib_controller_ack_irq(ide); /* <- make sure to ack IRQ */
+	idelib_controller_reset_irq_counter(ide);
+	idelib_controller_write_command(ide,0x08); /* <- device reset */
+	do_ide_controller_user_wait_busy_controller(ide);
+
+	/* NTS: Device reset doesn't necessary seem to signal an IRQ, at least not
+	 *      immediately. On some implementations the IRQ will have fired by now,
+	 *      on others, it will have fired by now for hard drives but for CD-ROM
+	 *      drives will not fire until the registers are read back, and others
+	 *      don't fire at all. So don't count on the IRQ, just poll and busy
+	 *      wait for the drive to signal readiness. */
+
+	if (idelib_controller_update_taskfile(ide,0xFF,IDELIB_TASKFILE_LBA48_UPDATE/*clear LBA48*/) == 0) {
+		struct ide_taskfile *tsk = idelib_controller_get_taskfile(ide,-1/*selected drive*/);
+
+		sprintf(tmp,"Device response: (0x1F1-0x1F7) %02X %02X %02X %02X %02X %02X %02X",
+			tsk->error,	tsk->sector_count,
+			tsk->lba0_3,	tsk->lba1_4,
+			tsk->lba2_5,	tsk->head_select,
+			tsk->status);
+		vga_msg_box_create(&vgabox,tmp,0,0);
+	}
+	else {
+		common_failed_to_read_taskfile_vga_msg_box(&vgabox);
+	}
+
+	/* it MIGHT have fired an IRQ... */
+	idelib_controller_ack_irq(ide);
+
+	wait_for_enter_or_escape();
+	vga_msg_box_destroy(&vgabox);
+	do_ide_controller_atapi_device_check_post_host_reset(ide);
+	do_ide_controller_user_wait_drive_ready(ide);
+}
+
+/* =================================== menu for tests ================================= */
+
+static const char *drive_main_power_states_strings[] = {
+	"Show IDE register taskfile",		/* 0 */
+	"Device reset",
+	"Standby",
+	"Sleep",
+	"Idle",
+	"Power mode"				/* 5 */
+};
+
+void do_drive_power_states_test(struct ide_controller *ide,unsigned char which) {
+	VGA_ALPHA_PTR vga;
+	struct menuboxbounds mbox;
+	char backredraw=1;
+	unsigned int x,y;
+	char redraw=1;
+	int select=-1;
+	int c;
+
+	/* UI element vars */
+	menuboxbounds_set_def_list(&mbox,/*ofsx=*/4,/*ofsy=*/7,/*cols=*/1);
+	menuboxbounds_set_item_strings_arraylen(&mbox,drive_main_power_states_strings);
+
+	/* most of the commands assume a ready controller. if it's stuck,
+	 * we'd rather the user have a visual indication that it's stuck that way */
+	c = do_ide_controller_user_wait_busy_controller(ide);
+	if (c != 0) return;
+
+	/* select the drive we want */
+	idelib_controller_drive_select(ide,which,/*head*/0,IDELIB_DRIVE_SELECT_MODE_CHS);
+
+	/* in case the IDE controller is busy for that time */
+	c = do_ide_controller_user_wait_busy_controller(ide);
+	if (c != 0) return;
+
+	/* read back: did the drive select take effect? if not, it might not be there. another common sign is the head/drive select reads back 0xFF */
+	c = do_ide_controller_drive_check_select(ide,which);
+	if (c < 0) return;
+
+	/* it might be a CD-ROM drive, which in some cases might not raise the Drive Ready bit */
+	do_ide_controller_atapi_device_check_post_host_reset(ide);
+
+	/* wait for the drive to indicate readiness */
+	/* NTS: If the drive never becomes ready even despite our reset hacks, there's a strong
+	 *      possibility that the device doesn't exist. This can happen for example if there
+	 *      is a master attached but no slave. */
+	c = do_ide_controller_user_wait_drive_ready(ide);
+	if (c < 0) return;
+
+	/* for completeness, clear pending IRQ */
+	idelib_controller_ack_irq(ide);
+
+	while (1) {
+		if (backredraw) {
+			vga = vga_alpha_ram;
+			backredraw = 0;
+			redraw = 1;
+
+			for (y=0;y < vga_height;y++) {
+				for (x=0;x < vga_width;x++) {
+					*vga++ = 0x1E00 + 177;
+				}
+			}
+
+			vga_moveto(0,0);
+
+			vga_write_color(0x1F);
+			vga_write("        IDE controller ");
+			sprintf(tmp,"@%X",ide->base_io);
+			vga_write(tmp);
+			if (ide->alt_io != 0) {
+				sprintf(tmp," alt %X",ide->alt_io);
+				vga_write(tmp);
+			}
+			if (ide->irq >= 0) {
+				sprintf(tmp," IRQ %d",ide->irq);
+				vga_write(tmp);
+			}
+			vga_write(which ? " Slave" : " Master");
+			vga_write(" << Power states");
+			while (vga_pos_x < vga_width && vga_pos_x != 0) vga_writec(' ');
+
+			vga_write_color(0xC);
+			vga_write("WARNING: This code talks directly to your hard disk controller.");
+			while (vga_pos_x < vga_width && vga_pos_x != 0) vga_writec(' ');
+			vga_write_color(0xC);
+			vga_write("         If you value the data on your hard drive do not run this program.");
+			while (vga_pos_x < vga_width && vga_pos_x != 0) vga_writec(' ');
+		}
+
+		if (redraw) {
+			redraw = 0;
+
+			vga_moveto(mbox.ofsx,mbox.ofsy - 2);
+			vga_write_color((select == -1) ? 0x70 : 0x0F);
+			vga_write("Back to IDE drive main menu");
+			while (vga_pos_x < (mbox.width+mbox.ofsx) && vga_pos_x != 0) vga_writec(' ');
+
+			menuboxbound_redraw(&mbox,select);
+		}
+
+		c = getch();
+		if (c == 0) c = getch() << 8;
+
+		if (c == 27) {
+			break;
+		}
+		else if (c == 13) {
+			if (select == -1)
+				break;
+
+			switch (select) {
+				case 0: /* show IDE register taskfile */
+					do_common_show_ide_taskfile(ide,which);
+					redraw = backredraw = 1;
+					break;
+				case 1: /* device reset */
+					do_drive_device_reset_test(ide,which);
+					break;
+				case 2: /* standby */
+					do_drive_standby_test(ide,which);
+					break;
+				case 3: /* sleep */
+					do_drive_sleep_test(ide,which);
+					break;
+				case 4: /* idle */
+					do_drive_idle_test(ide,which);
+					break;
+				case 5: /* check power mode */
+					do_drive_check_power_mode(ide,which);
+					break;
+			};
+		}
+		else if (c == 0x4800) {
+			if (--select < -1)
+				select = mbox.item_max;
+
+			redraw = 1;
+		}
+		else if (c == 0x5000) {
+			if (++select > mbox.item_max)
+				select = -1;
+
+			redraw = 1;
+		}
+	}
 }
 
--- hw/ide/testpwr.h
+++ hw/ide/testpwr.h
@@ -3,4 +3,7 @@ void do_drive_idle_test(struct ide_controller *ide,unsigned char which);
 void do_drive_sleep_test(struct ide_controller *ide,unsigned char which);
 void do_drive_standby_test(struct ide_controller *ide,unsigned char which);
 void do_drive_check_power_mode(struct ide_controller *ide,unsigned char which);
+void do_drive_device_reset_test(struct ide_controller *ide,unsigned char which);
+
+void do_drive_power_states_test(struct ide_controller *ide,unsigned char which);
 
