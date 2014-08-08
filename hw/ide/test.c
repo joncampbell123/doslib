0f0fa0e2396f9efb6dfa4e68bd8ee32b4c031934
Jonathan Campbell <jonathan@castus.tv>
2014-08-08 13:14:35 -0700

    NOP test ported.

--- hw/ide/test.c
+++ hw/ide/test.c
@@ -2983,6 +2983,61 @@ void do_drive_pio_mode(struct ide_controller *ide,unsigned char which) {
 	}
 }
 
+void do_ide_controller_drive_nop_test(struct ide_controller *ide,unsigned char which) {
+	struct vga_msg_box vgabox;
+	struct ide_taskfile *tsk;
+
+	if (do_ide_controller_user_wait_busy_controller(ide) != 0 || do_ide_controller_user_wait_drive_ready(ide) < 0)
+		return;
+	idelib_controller_ack_irq(ide); /* <- make sure to ack IRQ */
+
+	/* fill the registers with a pattern and then issue the NOP command.
+	 * the NOP command should come back with unmodified registers */
+	tsk = idelib_controller_get_taskfile(ide,-1/*selected drive*/);
+	tsk->features = 0x00; /* will read back into error reg */
+	tsk->sector_count = 0x12;
+	tsk->lba0_3 = 0x34;
+	tsk->lba1_4 = 0x56;
+	tsk->lba2_5 = 0x78;
+	tsk->command = 0x00; /* <- NOP */
+	idelib_controller_apply_taskfile(ide,0xBE/*base_io+1-5&7*/,IDELIB_TASKFILE_LBA48_UPDATE/*clear LBA48*/);
+	if (ide->flags.io_irq_enable) {
+		do_ide_controller_user_wait_irq(ide,1);
+		idelib_controller_ack_irq(ide); /* <- or else it won't fire again */
+	}
+	do_ide_controller_user_wait_busy_controller(ide);
+	do_ide_controller_user_wait_drive_ready(ide);
+	if (!(ide->last_status&1)) {
+		vga_msg_box_create(&vgabox,"Success?? (It's not supposed to!)",0,0);
+	}
+	else if ((ide->last_status&0xC9) != 0x41) {
+		sprintf(tmp,"Device rejected with status %02X (not the way it's supposed to)",ide->last_status);
+		vga_msg_box_create(&vgabox,tmp,0,0);
+	}
+	else {
+		idelib_controller_update_taskfile(ide,0xBE,0);
+		if ((tsk->error&0x04) != 0x04) {
+			vga_msg_box_create(&vgabox,"Device rejected with non-abort (not the way it's supposed to)",0,0);
+		}
+		else {
+			unsigned char c=0,cc=0;
+
+			c=tsk->sector_count; cc += (c == 0x12)?1:0;
+			c=tsk->lba0_3; cc += (c == 0x34)?1:0;
+			c=tsk->lba1_4; cc += (c == 0x56)?1:0;
+			c=tsk->lba2_5; cc += (c == 0x78)?1:0;
+
+			if (cc == 4)
+				vga_msg_box_create(&vgabox,"Success",0,0);
+			else
+				vga_msg_box_create(&vgabox,"Failed. Registers were modified.",0,0);
+		}
+	}
+
+	wait_for_enter_or_escape();
+	vga_msg_box_destroy(&vgabox);
+}
+
 static const char *drive_main_menustrings[] = {
 	"Show IDE register taskfile",		/* 0 */
 	"Identify (ATA)",
@@ -2993,7 +3048,8 @@ static const char *drive_main_menustrings[] = {
 	"Read HDD tests >>",
 	"Read verify HDD tests >>",
 	"Write HDD tests >>",
-	"Media status notification >>"
+	"Media status notification >>",
+	"No-op"					/* 10 */
 };
 
 void do_ide_controller_drive(struct ide_controller *ide,unsigned char which) {
@@ -3219,63 +3275,11 @@ void do_ide_controller_drive(struct ide_controller *ide,unsigned char which) {
 					do_ide_controller_drive_media_status_notify(ide,which);
 					redraw = backredraw = 1;
 					break;
+				case 10: /* NOP */
+					do_ide_controller_drive_nop_test(ide,which);
+					break;
 			};
 #if 0 /* PORTME */
-			else if (select == 28) { /* NOP */
-				if (do_ide_controller_user_wait_busy_controller(ide) == 0 &&
-					do_ide_controller_user_wait_drive_ready(ide) >= 0) {
-					struct ide_taskfile *tsk;
-
-					idelib_controller_reset_irq_counter(ide);
-
-					/* in case command doesn't do anything, fill sector count value with 0x0A */
-					tsk = idelib_controller_get_taskfile(ide,-1/*selected drive*/);
-					tsk->features = 0x00; /* will read back into error reg */
-					tsk->sector_count = 0x12;
-					tsk->lba0_3 = 0x34;
-					tsk->lba1_4 = 0x56;
-					tsk->lba2_5 = 0x78;
-					tsk->command = 0x00; /* <- NOP */
-					idelib_controller_apply_taskfile(ide,0xBE/*base_io+1-5&7*/,IDELIB_TASKFILE_LBA48_UPDATE/*clear LBA48*/);
-					if (ide->flags.io_irq_enable) {
-						do_ide_controller_user_wait_irq(ide,1);
-						idelib_controller_ack_irq(ide); /* <- or else it won't fire again */
-					}
-					do_ide_controller_user_wait_busy_controller(ide);
-					do_ide_controller_user_wait_drive_ready(ide);
-					if (!(ide->last_status&1)) {
-						vga_msg_box_create(&vgabox,"Success?? (It's not supposed to!)",0,0);
-					}
-					else if ((ide->last_status&0xC9) != 0x41) {
-						sprintf(tmp,"Device rejected with error %02X (not the way it's supposed to)",x);
-						vga_msg_box_create(&vgabox,tmp,0,0);
-					}
-					else {
-						idelib_controller_update_taskfile(ide,0xBE,0);
-						if ((tsk->error&0x04) != 0x04) {
-							sprintf(tmp,"Device rejected with non-abort (not the way it's supposed to)",x);
-							vga_msg_box_create(&vgabox,tmp,0,0);
-						}
-						else {
-							unsigned char c=0,cc=0;
-
-							c=tsk->sector_count; cc += (c == 0x12)?1:0;
-							c=tsk->lba0_3; cc += (c == 0x34)?1:0;
-							c=tsk->lba1_4; cc += (c == 0x56)?1:0;
-							c=tsk->lba2_5; cc += (c == 0x78)?1:0;
-
-							if (cc == 4)
-								vga_msg_box_create(&vgabox,"Success",0,0);
-							else
-								vga_msg_box_create(&vgabox,"Failed. Registers were modified.",0,0);
-						}
-					}
-
-					wait_for_enter_or_escape();
-					vga_msg_box_destroy(&vgabox);
-				}
-			}
-
 			else if (select == 21) { /* media eject */
 				if (do_ide_controller_user_wait_busy_controller(ide) == 0 &&
 					do_ide_controller_user_wait_drive_ready(ide) == 0) {
