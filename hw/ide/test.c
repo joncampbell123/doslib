3f2e164293b56cf4af5ce3b912ce3bd147856fe8
Jonathan Campbell <jonathan@castus.tv>
2014-09-13 22:11:46 -0700

    flush cache command

--- hw/ide/test.c
+++ hw/ide/test.c
@@ -96,6 +96,153 @@ unsigned char			cdrom_sector[512U*64U];	/* ~32KB, enough for 16 CD-ROM sector or
 # endif
 #endif
 
+int do_ide_flush(struct ide_controller *ide,unsigned char which,unsigned char lba48) {
+	if (do_ide_controller_user_wait_busy_controller(ide) != 0 || do_ide_controller_user_wait_drive_ready(ide) < 0)
+		return -1;
+
+	idelib_controller_ack_irq(ide); /* <- make sure to ack IRQ */
+	idelib_controller_reset_irq_counter(ide);
+	idelib_controller_write_command(ide,lba48?0xEA:0xE7); /* <- (E7h) FLUSH CACHE (EA) FLUSH CACHE EXT */
+	if (ide->flags.io_irq_enable) {
+		do_ide_controller_user_wait_irq(ide,1);
+		idelib_controller_ack_irq(ide); /* <- or else it won't fire again */
+	}
+	do_ide_controller_user_wait_busy_controller(ide);
+	do_ide_controller_user_wait_drive_ready(ide);
+	if (ide->last_status&1) return 1;
+	return 0;
+}
+
+/*-----------------------------------------------------------------*/
+
+const char *drive_misc_tests_menustrings[] = {
+	"Show IDE register taskfile",		/* 0 */
+	"Flush cache",
+	"Flush cache ext (LBA48)"
+};
+
+void do_drive_misc_tests(struct ide_controller *ide,unsigned char which) {
+	struct menuboxbounds mbox;
+	struct vga_msg_box vgabox;
+	char backredraw=1;
+	VGA_ALPHA_PTR vga;
+	unsigned int x,y;
+	int select=-1;
+	char redraw=1;
+	int c;
+
+	/* UI element vars */
+	menuboxbounds_set_def_list(&mbox,/*ofsx=*/4,/*ofsy=*/7,/*cols=*/1);
+	menuboxbounds_set_item_strings_arraylen(&mbox,drive_misc_tests_menustrings);
+
+	while (1) {
+		if (backredraw) {
+			vga = vga_alpha_ram;
+			backredraw = 0;
+			redraw = 1;
+
+			for (y=0;y < vga_height;y++) {
+				for (x=0;x < vga_width;x++) {
+					*vga++ = 0x1E00 + 177;
+				}
+			}
+
+			vga_moveto(0,0);
+
+			vga_write_color(0x1F);
+			vga_write("        IDE controller read/write tests ");
+			sprintf(tmp,"@%X",ide->base_io);
+			vga_write(tmp);
+			if (ide->alt_io != 0) {
+				sprintf(tmp," alt %X",ide->alt_io);
+				vga_write(tmp);
+			}
+			if (ide->irq >= 0) {
+				sprintf(tmp," IRQ %d",ide->irq);
+				vga_write(tmp);
+			}
+			vga_write(which ? " Slave" : " Master");
+			while (vga_pos_x < vga_width && vga_pos_x != 0) vga_writec(' ');
+
+			vga_write_color(0xC);
+			vga_write("WARNING: This code talks directly to your hard disk controller.");
+			while (vga_pos_x < vga_width && vga_pos_x != 0) vga_writec(' ');
+			vga_write_color(0xC);
+			vga_write("         If you value the data on your hard drive do not run this program.");
+			while (vga_pos_x < vga_width && vga_pos_x != 0) vga_writec(' ');
+		}
+
+		if (redraw) {
+			redraw = 0;
+
+			vga_moveto(mbox.ofsx,mbox.ofsy - 2);
+			vga_write_color((select == -1) ? 0x70 : 0x0F);
+			vga_write("Back to IDE controller main menu");
+			while (vga_pos_x < (mbox.width+mbox.ofsx) && vga_pos_x != 0) vga_writec(' ');
+
+			menuboxbound_redraw(&mbox,select);
+		}
+
+		c = getch();
+		if (c == 0) c = getch() << 8;
+
+		if (c == 27) {
+			break;
+		}
+		else if (c == 13) {
+			if (select == -1)
+				break;
+
+			switch (select) {
+				case 0: /* show IDE register taskfile */
+					do_common_show_ide_taskfile(ide,which);
+					redraw = backredraw = 1;
+					break;
+				case 1: /* Flush cache */
+					do_ide_flush(ide,which,/*lba48*/0);
+					if (!idelib_controller_is_error(ide))
+						vga_msg_box_create(&vgabox,"Success",0,0);
+					else
+						common_ide_success_or_error_vga_msg_box(ide,&vgabox);
+
+					wait_for_enter_or_escape();
+					vga_msg_box_destroy(&vgabox);
+					redraw = 1;
+					break;
+				case 2: /* Flush cache ext */
+					do_ide_flush(ide,which,/*lba48*/1);
+					if (!idelib_controller_is_error(ide))
+						vga_msg_box_create(&vgabox,"Success",0,0);
+					else
+						common_ide_success_or_error_vga_msg_box(ide,&vgabox);
+
+					wait_for_enter_or_escape();
+					vga_msg_box_destroy(&vgabox);
+					redraw = 1;
+					break;
+			};
+		}
+		else if (c == 0x4800) {
+			if (--select < -1)
+				select = mbox.item_max;
+
+			redraw = 1;
+		}
+		else if (c == 0x4B00) { /* left */
+			redraw = 1;
+		}
+		else if (c == 0x4D00) { /* right */
+			redraw = 1;
+		}
+		else if (c == 0x5000) {
+			if (++select > mbox.item_max)
+				select = -1;
+
+			redraw = 1;
+		}
+	}
+}
+
 /*-----------------------------------------------------------------*/
 
 static const char *drive_main_menustrings[] = {
@@ -109,7 +256,8 @@ static const char *drive_main_menustrings[] = {
 	"CD-ROM eject/load >>",
 	"CD-ROM reading >>",
 	"Multiple mode >>",
-	"Read/Write tests >>"			/* 10 */
+	"Read/Write tests >>",			/* 10 */
+	"Miscellaneous >>"
 };
 
 void do_ide_controller_drive(struct ide_controller *ide,unsigned char which) {
@@ -259,6 +407,10 @@ void do_ide_controller_drive(struct ide_controller *ide,unsigned char which) {
 					break;
 				case 10: /* read/write tests */
 					do_drive_readwrite_tests(ide,which);
+					redraw = backredraw = 1;
+					break;
+				case 11: /* misc */
+					do_drive_misc_tests(ide,which);
 					redraw = backredraw = 1;
 					break;
 			};
