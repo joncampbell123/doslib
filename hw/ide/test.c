65d09e6c5ddedbe7d7f46bd15a01ab5394fa6c02
Jonathan Campbell <jonathan@castus.tv>
2014-09-19 23:41:03 -0700

    SMARTDRV 3.xx (MS-DOS 5.0) detection and flushing.

--- hw/ide/test.c
+++ hw/ide/test.c
@@ -93,6 +93,16 @@
 
 /* TODO: add to DOS library */
 unsigned short			smartdrv_version = 0xFFFF;
+int				smartdrv_fd = -1;
+
+int smartdrv_close() {
+	if (smartdrv_fd >= 0) {
+		close(smartdrv_fd);
+		smartdrv_fd = -1;
+	}
+
+	return 0;
+}
 
 int smartdrv_flush() {
 	if (smartdrv_version == 0xFFFF || smartdrv_version == 0)
@@ -120,6 +130,46 @@ int smartdrv_flush() {
 			pop	ax
 		}
 #endif
+	}
+	else if (smartdrv_version >= 0x300 && smartdrv_version <= 0x3FF) { /* SMARTDRV 3.xx */
+		char buffer[0x1];
+#if TARGET_MSDOS == 32
+#else
+		char far *bptr = (char far*)buffer;
+#endif
+		int rd=0;
+
+		if (smartdrv_fd < 0)
+			return 0;
+
+		buffer[0] = 0; /* flush cache */
+#if TARGET_MSDOS == 32
+		/* FIXME: We do not yet have a 32-bit protected mode version.
+		 *        DOS extenders do not appear to translate AX=0x4403 properly */
+#else
+		__asm {
+			push	ax
+			push	bx
+			push	cx
+			push	dx
+			push	ds
+			mov	ax,0x4403		; IOCTL SMARTAAR CACHE CONTROL
+			mov	bx,smartdrv_fd
+			mov	cx,0x1			; 0x01 bytes
+			lds	dx,word ptr [bptr]
+			int	0x21
+			jc	err1
+			mov	rd,ax			; CF=0, AX=bytes written
+err1:			pop	ds
+			pop	dx
+			pop	cx
+			pop	bx
+			pop	ax
+		}
+#endif
+
+		if (rd == 0)
+			return 0;
 	}
 
 	return 1;
@@ -189,11 +239,64 @@ int smartdrv_detect() {
 
 		if ((rvax&0xFFFF) == 0xBABE && (rvbp&0xFFFF) >= 0x400 && (rvbp&0xFFFF) <= 0x5FF) {
 			/* yup. SMARTDRV 4.xx! */
-			smartdrv_version = rvbp&0xFFFF;
+			smartdrv_version = (rvbp&0xFF00) + (((rvbp>>4)&0xF) * 10) + (rvbp&0xF); /* convert BCD to decimal */
 		}
-		else {
+
+		if (smartdrv_version == 0xFFFF) {
+			char buffer[0x28];
+#if TARGET_MSDOS == 32
+#else
+			char far *bptr = (char far*)buffer;
+#endif
+			int rd=0;
+
+			memset(buffer,0,sizeof(buffer));
+
+			/* check for SMARTDRV 3.xx */
+			smartdrv_fd = open("SMARTAAR",O_RDONLY);
+			if (smartdrv_fd >= 0) {
+				/* FIXME: The DOS library should provide a common function to do IOCTL read/write character "control channel" functions */
+#if TARGET_MSDOS == 32
+				/* FIXME: We do not yet have a 32-bit protected mode version.
+				 *        DOS extenders do not appear to translate AX=0x4402 properly */
+#else
+				__asm {
+					push	ax
+					push	bx
+					push	cx
+					push	dx
+					push	ds
+					mov	ax,0x4402		; IOCTL SMARTAAR GET CACHE STATUS
+					mov	bx,smartdrv_fd
+					mov	cx,0x28			; 0x28 bytes
+					lds	dx,word ptr [bptr]
+					int	0x21
+					jc	err1
+					mov	rd,ax			; CF=0, AX=bytes read
+err1:					pop	ds
+					pop	dx
+					pop	cx
+					pop	bx
+					pop	ax
+				}
+#endif
+
+				/* NTS: Despite reading back 0x28 bytes of data, this IOCTL
+				 *      returns AX=1 for some reason. */
+				if (rd > 0 && rd <= 0x28) {
+					if (buffer[0] == 1/*write through flag*/ && buffer[15] == 3/*major version number*/) { /* SMARTDRV 3.xx */
+						smartdrv_version = ((unsigned short)buffer[15] << 8) + ((unsigned short)buffer[14]);
+					}
+					else {
+						close(smartdrv_fd);
+					}
+				}
+			}
+		}
+
+		/* didn't find anything. then no SMARTDRV */
+		if (smartdrv_version == 0xFFFF)
 			smartdrv_version = 0;
-		}
 	}
 
 	return (smartdrv_version != 0);
@@ -754,7 +857,7 @@ int main(int argc,char **argv) {
 	printf("IDE ATA/ATAPI test program\n");
 
 	if (smartdrv_detect()) {
-		printf("WARNING: SMARTDRV %x.%02x or equivalent disk cache detected!\n",smartdrv_version>>8,smartdrv_version&0xFF);
+		printf("WARNING: SMARTDRV %u.%02u or equivalent disk cache detected!\n",smartdrv_version>>8,smartdrv_version&0xFF);
 		printf("         Running this program with SMARTDRV enabled is NOT RECOMMENDED,\n");
 		printf("         especially when using the snapshot functions!\n");
 		printf("         If you choose to test anyway, this program will attempt to flush\n");
@@ -811,7 +914,7 @@ int main(int argc,char **argv) {
 		printf("Hit ENTER to continue, ESC to cancel\n");
 		i = wait_for_enter_or_escape();
 		if (i == 27) {
-			do_main_menu();
+			smartdrv_close();
 			free_idelib();
 			return 0;
 		}
@@ -822,6 +925,7 @@ int main(int argc,char **argv) {
 		update_state_from_vga();
 	}
 
+	smartdrv_close();
 	do_main_menu();
 	free_idelib();
 	return 0;
