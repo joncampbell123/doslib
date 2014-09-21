67a0dc667e35bb1c7118410306654a5954c13d93
Jonathan Campbell <jonathan@castus.tv>
2014-09-20 19:34:24 -0700

    ack IRQ, then check IRQ counter

--- hw/ide/test.c
+++ hw/ide/test.c
@@ -325,6 +325,10 @@ static void interrupt my_ide_irq() {
 		 * side, and we'll crash from an IRQ storm (level-triggered, obviously!) */
 		idelib_controller_ack_irq(my_ide_irq_ide);
 
+		/* ack PIC */
+		if (my_ide_irq_ide->irq >= 8) p8259_OCW2(8,P8259_OCW2_NON_SPECIFIC_EOI);
+		p8259_OCW2(0,P8259_OCW2_NON_SPECIFIC_EOI);
+
 		/* If too many IRQs fired, then unhook the IRQ and use polling from now on.
 		 * apparently some newer SATA controllers in IDE mode needs this, else we
 		 * get an infinite storm of IRQs and we'll crash. */
@@ -333,10 +337,6 @@ static void interrupt my_ide_irq() {
 			vga_alpha_ram[i+12] = 0x1C00 | '!';
 			my_ide_irq_ide->irq_fired = ~0; /* make sure the IRQ counter is as large as possible */
 		}
-
-		/* ack PIC */
-		if (my_ide_irq_ide->irq >= 8) p8259_OCW2(8,P8259_OCW2_NON_SPECIFIC_EOI);
-		p8259_OCW2(0,P8259_OCW2_NON_SPECIFIC_EOI);
 	}
 
 	/* we CANNOT use sprintf() here. sprintf() doesn't work to well from within an interrupt handler,
