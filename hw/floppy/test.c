07fe73a732d06d93e628a03e31bba441bdd5144a
Jonathan Campbell <jonathan@castus.tv>
2014-09-25 00:46:19 -0700

    menu for FDC. making progress!

--- hw/floppy/test.c
+++ hw/floppy/test.c
@@ -27,6 +27,7 @@ struct floppy_controller {
 	uint8_t			ps2_status_regs[2];	/* 0x3F0-0x3F1 */
 	uint8_t			digital_out;		/* last value written to 0x3F2 */
 	uint8_t			main_status;		/* last value read from 0x3F4 */
+	uint16_t		irq_fired;
 
 	/* desired state */
 	uint8_t			use_dma:1;		/* if set, use DMA. else, use PIO data transfer */
@@ -35,6 +36,7 @@ struct floppy_controller {
 	uint8_t			version;		/* result of command 0x10 (determine controller version) or 0x00 if not yet queried */
 	uint8_t			ps2_mode:1;		/* controller is in PS/2 mode (has status registers A and B I/O ports) */
 	uint8_t			digital_out_rw:1;	/* digital out (0x3F2) is readable */
+
 };
 
 /* standard I/O ports for floppy controllers */
@@ -192,11 +194,176 @@ void free_floppy_controller_lib() {
 
 char	tmp[1024];
 
+static void (interrupt *my_floppy_old_irq)() = NULL;
+static struct floppy_controller *my_floppy_irq_floppy = NULL;
+static unsigned long floppy_irq_counter = 0;
+static int my_floppy_irq_number = -1;
+
+void do_floppy_controller_unhook_irq(struct floppy_controller *fdc);
+
+static void interrupt my_floppy_irq() {
+	int i;
+
+	i = vga_width*(vga_height-1);
+
+	_cli();
+	if (my_floppy_irq_floppy != NULL) {
+		my_floppy_irq_floppy->irq_fired++;
+
+		/* ack PIC */
+		if (my_floppy_irq_floppy->irq >= 8) p8259_OCW2(8,P8259_OCW2_NON_SPECIFIC_EOI);
+		p8259_OCW2(0,P8259_OCW2_NON_SPECIFIC_EOI);
+
+		/* If too many IRQs fired, then unhook the IRQ and use polling from now on. */
+		if (my_floppy_irq_floppy->irq_fired >= 0xFFFEU) {
+			do_floppy_controller_unhook_irq(my_floppy_irq_floppy);
+			vga_alpha_ram[i+12] = 0x1C00 | '!';
+			my_floppy_irq_floppy->irq_fired = ~0; /* make sure the IRQ counter is as large as possible */
+		}
+	}
+
+	/* we CANNOT use sprintf() here. sprintf() doesn't work to well from within an interrupt handler,
+	 * and can cause crashes in 16-bit realmode builds. */
+	vga_alpha_ram[i++] = 0x1F00 | 'I';
+	vga_alpha_ram[i++] = 0x1F00 | 'R';
+	vga_alpha_ram[i++] = 0x1F00 | 'Q';
+	vga_alpha_ram[i++] = 0x1F00 | ':';
+	vga_alpha_ram[i++] = 0x1F00 | ('0' + ((floppy_irq_counter / 100000UL) % 10UL));
+	vga_alpha_ram[i++] = 0x1F00 | ('0' + ((floppy_irq_counter /  10000UL) % 10UL));
+	vga_alpha_ram[i++] = 0x1F00 | ('0' + ((floppy_irq_counter /   1000UL) % 10UL));
+	vga_alpha_ram[i++] = 0x1F00 | ('0' + ((floppy_irq_counter /    100UL) % 10UL));
+	vga_alpha_ram[i++] = 0x1F00 | ('0' + ((floppy_irq_counter /     10UL) % 10UL));
+	vga_alpha_ram[i++] = 0x1F00 | ('0' + ((floppy_irq_counter /       1L) % 10UL));
+	vga_alpha_ram[i++] = 0x1F00 | ' ';
+
+	floppy_irq_counter++;
+}
+
+void do_floppy_controller_hook_irq(struct floppy_controller *fdc) {
+	if (my_floppy_irq_number >= 0 || my_floppy_old_irq != NULL || fdc->irq < 0)
+		return;
+
+	/* let the IRQ know what floppy controller */
+	my_floppy_irq_floppy = fdc;
+
+	/* enable on floppy controller */
+	p8259_mask(fdc->irq);
+	floppy_controller_enable_dma(fdc,1);
+
+	/* hook IRQ */
+	my_floppy_old_irq = _dos_getvect(irq2int(fdc->irq));
+	_dos_setvect(irq2int(fdc->irq),my_floppy_irq);
+	my_floppy_irq_number = fdc->irq;
+
+	/* enable at PIC */
+	p8259_unmask(fdc->irq);
+}
+
+void do_floppy_controller_unhook_irq(struct floppy_controller *fdc) {
+	if (my_floppy_irq_number < 0 || my_floppy_old_irq == NULL || fdc->irq < 0)
+		return;
+
+	/* disable on floppy controller, then mask at PIC */
+	p8259_mask(fdc->irq);
+	floppy_controller_enable_dma(fdc,0);
+
+	/* restore the original vector */
+	_dos_setvect(irq2int(fdc->irq),my_floppy_old_irq);
+	my_floppy_irq_number = -1;
+	my_floppy_old_irq = NULL;
+}
+
+void do_floppy_controller_enable_irq(struct floppy_controller *fdc,unsigned char en) {
+	if (!en || fdc->irq < 0 || fdc->irq != my_floppy_irq_number)
+		do_floppy_controller_unhook_irq(fdc);
+	if (en && fdc->irq >= 0)
+		do_floppy_controller_hook_irq(fdc);
+}
+
+void do_floppy_controller(struct floppy_controller *fdc) {
+	char backredraw=1;
+	VGA_ALPHA_PTR vga;
+	unsigned int x,y;
+	char redraw=1;
+	int select=-1;
+	int c;
+
+	/* if the floppy struct says to use interrupts, then do it */
+	do_floppy_controller_enable_irq(fdc,fdc->use_dma);
+
+	while (1) {
+		if (backredraw) {
+			vga = vga_alpha_ram;
+			backredraw = 0;
+			redraw = 1;
+
+			for (y=0;y < vga_height;y++) {
+				for (x=0;x < vga_width;x++) {
+					*vga++ = 0x1E00 + 177;
+				}
+			}
+
+			vga_moveto(0,0);
+
+			vga_write_color(0x1F);
+			vga_write("        Floppy controller ");
+			sprintf(tmp,"@%X",fdc->base_io);
+			vga_write(tmp);
+			if (fdc->irq >= 0) {
+				sprintf(tmp," IRQ %d",fdc->irq);
+				vga_write(tmp);
+			}
+			if (fdc->dma >= 0) {
+				sprintf(tmp," DMA %d",fdc->dma);
+				vga_write(tmp);
+			}
+			while (vga_pos_x < vga_width && vga_pos_x != 0) vga_writec(' ');
+		}
+
+		if (redraw) {
+			redraw = 0;
+
+			y = 5;
+			vga_moveto(8,y++);
+			vga_write_color((select == -1) ? 0x70 : 0x0F);
+			vga_write("Main menu");
+			while (vga_pos_x < (vga_width-8) && vga_pos_x != 0) vga_writec(' ');
+			y++;
+		}
+
+		c = getch();
+		if (c == 0) c = getch() << 8;
+
+		if (c == 27) {
+			break;
+		}
+		else if (c == 13) {
+			if (select == -1) {
+				break;
+			}
+		}
+		else if (c == 0x4800) {
+			if (--select < -1)
+				select = -1;
+
+			redraw = 1;
+		}
+		else if (c == 0x5000) {
+			if (++select > -1)
+				select = -1;
+
+			redraw = 1;
+		}
+	}
+
+	do_floppy_controller_enable_irq(fdc,0);
+}
+
 void do_main_menu() {
 	char redraw=1;
 	char backredraw=1;
 	VGA_ALPHA_PTR vga;
-	struct floppy_controller *ide;
+	struct floppy_controller *floppy;
 	unsigned int x,y,i;
 	int select=-1;
 	int c;
@@ -230,21 +397,21 @@ void do_main_menu() {
 			y++;
 
 			for (i=0;i < MAX_FLOPPY_CONTROLLER;i++) {
-				ide = floppy_get_controller(i);
-				if (ide != NULL) {
+				floppy = floppy_get_controller(i);
+				if (floppy != NULL) {
 					vga_moveto(8,y++);
 					vga_write_color((select == (int)i) ? 0x70 : 0x0F);
 
-					sprintf(tmp,"Controller @ %04X",ide->base_io);
+					sprintf(tmp,"Controller @ %04X",floppy->base_io);
 					vga_write(tmp);
 
-					if (ide->irq >= 0) {
-						sprintf(tmp," IRQ %2d",ide->irq);
+					if (floppy->irq >= 0) {
+						sprintf(tmp," IRQ %2d",floppy->irq);
 						vga_write(tmp);
 					}
 
-					if (ide->dma >= 0) {
-						sprintf(tmp," DMA %2d",ide->dma);
+					if (floppy->dma >= 0) {
+						sprintf(tmp," DMA %2d",floppy->dma);
 						vga_write(tmp);
 					}
 				}
@@ -262,8 +429,8 @@ void do_main_menu() {
 				break;
 			}
 			else if (select >= 0 && select < MAX_FLOPPY_CONTROLLER) {
-				ide = floppy_get_controller(select);
-//				if (ide != NULL) do_ide_controller(ide);
+				floppy = floppy_get_controller(select);
+				if (floppy != NULL) do_floppy_controller(floppy);
 				backredraw = redraw = 1;
 			}
 		}
