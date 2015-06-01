666fa6ce745d7c68168cbd7922662d91cd5bb9b0
Jonathan Campbell <jonathan@castus.tv>
2015-05-31 20:58:14 -0700

    grtest.bat

---
+++ hw/cpu/grtest.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Make sure you are running this from the dos86l or dos386f dir.
+echo Grind test in progress. Results will be written to root directory of this drive.
+test >/GRIND.TXT
+grind >>/GRIND.TXT
+
