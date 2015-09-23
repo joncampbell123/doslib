543ea6b22c4998331f674844d41451a169dc832f
Jonathan Campbell <jonathan@castus.tv>
2015-09-23 15:41:52 -0700

    another arrangement

--- linux-ow.sh
+++ linux-ow.sh
@@ -12,6 +12,8 @@ if [ -d "/usr/src/ow/open-watcom-v2/rel/binl" ]; then
 fi
 if [ -d "$HOME/src/open-watcom-v2/rel/binl" ]; then
 	export WATCOM=$HOME/src/open-watcom-v2/rel
+elif [ -d "/usr/src/open-watcom-v2/rel/binl" ]; then
+	export WATCOM=/usr/src/open-watcom-v2/rel
 fi
 
 echo "Using: $WATCOM"
