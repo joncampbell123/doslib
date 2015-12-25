fb059fcc5cfbd5536e035e855b7cfd73e95b3bca
Jonathan Campbell <jonathan@castus.tv>
2015-12-24 16:54:24 -0800

    more work

--- hw/isapnp/dump.c
+++ hw/isapnp/dump.c
@@ -28,14 +28,15 @@
 #include <hw/8254/8254.h>		/* 8254 timer */
 #include <isapnp/isapnp.h>
 
-static unsigned char tmp[4096];
+static unsigned char tmp[128];
+static unsigned char devnode_raw[4096];
 
 static int dump_pnp_entry() {
 	FILE *fp;
 
 	printf("ISA PnP BIOS found, dumping info now.\n");
 
-	fp = fopen("PNPENTRY.TXT","wb");
+	fp = fopen("PNPENTRY.TXT","w");
 	if (!fp) { fprintf(stderr,"Error writing dumpfile\n"); return -1; }
 
 	fprintf(fp,"BIOS Provides PnP structure at F000:%04X\n",(unsigned int)isa_pnp_bios_offset);
@@ -79,6 +80,136 @@ static int dump_pnp_entry() {
 	return 0;
 }
 
+static int dump_isa_pnp_bios_devnodes() {
+	const char *dev_class = NULL,*dev_type = NULL,*dev_stype = NULL,*dev_itype = NULL;
+	struct isa_pnp_device_node far *devn;
+	unsigned int ret_ax,nodesize=0xFFFF;
+	unsigned char numnodes=0xFF;
+	char dev_product[256] = {0};
+	unsigned char current_node;
+	struct isapnp_tag tag;
+	unsigned char node;
+	FILE *fp,*fp_raw;
+
+	printf("Asking for sys device nodes..."); fflush(stdout);
+	ret_ax = isa_pnp_bios_number_of_sysdev_nodes(&numnodes,&nodesize);
+	if (ret_ax != 0 || numnodes == 0xFF || nodesize == 0 || nodesize > sizeof(devnode_raw)) {
+		printf(" failed. AX=0x%04X\n",ret_ax);
+		return 0;
+	}
+	printf(" ok. numnodes=%u largest_node_size=%u\n",
+		numnodes,nodesize);
+
+	fp = fopen("PNPBIOSN.TXT","w");
+	if (!fp) { fprintf(stderr,"Error writing dumpfile\n"); return -1; }
+	fprintf(fp,"BIOS reports %u device nodes, largest node is %u bytes\n\n",
+		numnodes,nodesize);
+
+	for (node=0;node != 0xFF;) {
+		current_node = node;
+		ret_ax = isa_pnp_bios_get_sysdev_node(&node,devnode_raw,ISA_PNP_BIOS_GET_SYSDEV_NODE_CTRL_NOW);
+		if (ret_ax != 0) break;
+
+		devn = (struct isa_pnp_device_node far*)devnode_raw;
+		if (devn->size < 11 || devn->size > sizeof(devnode_raw)) {
+			printf("skipping: node=0x%02x invalid size %u\n",
+				current_node,devn->size);
+			continue;
+		}
+
+		printf("dumping: node=0x%02x size=%u handle=0x%02x\n",
+			current_node,devn->size,devn->handle);
+
+		sprintf(tmp,"PNPBDV%02X.BIN",(unsigned char)current_node);
+		fp_raw = fopen((char*)tmp,"wb");
+		if (fp_raw == NULL) {
+			fprintf(stderr,"Unable to write %s\n",tmp);
+			fclose(fp);
+			return 1;
+		}
+		fwrite(devnode_raw,devn->size,1,fp_raw);
+		fclose(fp_raw);
+
+		/* then decoded output */
+		sprintf(tmp,"PNPBDV%02X.TXT",(unsigned char)current_node);
+		fp_raw = fopen((char*)tmp,"w");
+		if (fp_raw == NULL) {
+			fprintf(stderr,"Unable to write %s\n",tmp);
+			fclose(fp);
+			return 1;
+		}
+
+		isa_pnp_product_id_to_str(tmp,devn->product_id);
+		fprintf(fp,"PnP BIOS device: node=0x%02x size=%u handle=%u product_id=0x%08lx (%s) DeviceType=0x%02X,0x%02X,0x%02X\n",
+			current_node,
+			devn->size,
+			devn->handle,
+			(unsigned long)devn->product_id,tmp,
+			(unsigned char)devn->type_code[0],
+			(unsigned char)devn->type_code[1],
+			(unsigned char)devn->type_code[2]);
+		fprintf(fp_raw,	"PnP BIOS device:\n"
+				"  Node:                  0x%02x\n"
+				"  Size:                  %u bytes\n"
+				"  Handle:                %u\n"
+				"  Product ID:            0x%08lx '%s'\n"
+				"  Device type:           0x%02X,0x%02X,0x%02X\n",
+			current_node,
+			devn->size,
+			devn->handle,
+			(unsigned long)devn->product_id,tmp,
+			(unsigned char)devn->type_code[0],
+			(unsigned char)devn->type_code[1],
+			(unsigned char)devn->type_code[2]);
+
+		dev_product[0] = 0;
+		dev_type = isa_pnp_device_type(devn->type_code,&dev_stype,&dev_itype);
+		dev_class = isa_pnp_device_category(devn->product_id);
+		isa_pnp_device_description(dev_product,devn->product_id);
+
+		if (dev_class != NULL)
+			fprintf(fp_raw,	"  Device category:       %s\n",dev_class);
+		if (dev_product[0] != 0)
+			fprintf(fp_raw,	"  Device description:    %s\n",dev_product);
+		if (dev_type != NULL)
+			fprintf(fp_raw,	"  Device:                %s\n",dev_type);
+		if (dev_stype != NULL)
+			fprintf(fp_raw,	"  Device subtype:        %s\n",dev_stype);
+		if (dev_itype != NULL)
+			fprintf(fp_raw,	"  Device interface type: %s\n",dev_itype);
+
+		fprintf(fp_raw,	"  Attributes:            0x%04x\n",devn->attributes);
+		if (devn->attributes & ISAPNP_DEV_ATTR_CANT_DISABLE)
+			fprintf(fp_raw,	"                           - Device cannot be disabled\n");
+		if (devn->attributes & ISAPNP_DEV_ATTR_CANT_CONFIGURE)
+			fprintf(fp_raw,	"                           - Device is not configurable\n");
+		if (devn->attributes & ISAPNP_DEV_ATTR_CAN_BE_PRIMARY_OUTPUT)
+			fprintf(fp_raw, "                           - Device is capable of being primary output device\n");
+		if (devn->attributes & ISAPNP_DEV_ATTR_CAN_BE_PRIMARY_INPUT)
+			fprintf(fp_raw,	"                           - Device is capable of being primary input device\n");
+		if (devn->attributes & ISAPNP_DEV_ATTR_CAN_BE_PRIMARY_IPL)
+			fprintf(fp_raw,	"                           - Device is capable of being primary IPL device\n");
+		if (devn->attributes & ISAPNP_DEV_ATTR_DOCKING_STATION_DEVICE)
+			fprintf(fp_raw,	"                           - Device is a docking station device\n");
+		if (devn->attributes & ISAPNP_DEV_ATTR_REMOVEABLE_STATION_DEVICE)
+			fprintf(fp_raw,	"                           - Device is a removeable station device\n");
+
+		switch (ISAPNP_DEV_ATTR_WHEN_CONFIGURABLE(devn->attributes)) {
+			case ISAPNP_DEV_ATTR_WHEN_CONFIGURABLE_ONLY_NEXT_BOOT:
+				fprintf(fp_raw,	"                           - Device can only be configured for next boot\n"); break;
+			case ISAPNP_DEV_ATTR_WHEN_CONFIGURABLE_AT_RUNTIME:
+				fprintf(fp_raw,	"                           - Device can be configured at runtime\n"); break;
+			case ISAPNP_DEV_ATTR_WHEN_CONFIGURABLE_ONLY_RUNTIME:
+				fprintf(fp_raw,	"                           - Device can only be configured at runtime\n"); break;
+		};
+
+		fclose(fp_raw);
+	}
+
+	fclose(fp);
+	return 0;
+}
+
 int main(int argc,char **argv) {
 	printf("ISA Plug & Play dump program\n");
 
@@ -96,6 +227,7 @@ int main(int argc,char **argv) {
 	}
 
 	if (dump_pnp_entry()) return 1;
+	if (dump_isa_pnp_bios_devnodes()) return 1;
 
 	free_isa_pnp_bios();
 	return 0;
