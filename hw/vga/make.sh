9b997300812b4367e7aba4a52928468b7b2b599f
Jonathan Campbell <jonathan@castus.tv>
2016-02-28 01:26:50 -0800

    more

--- hw/vga/make.sh
+++ hw/vga/make.sh
@@ -10,7 +10,7 @@ win32=1 # Windows 9x/NT/XP/Vista/etc.
 
 if [ "$1" == "clean" ]; then
 	do_clean
-	rm -fv test.dsk test2.dsk nul.err tmp.cmd tmp1.cmd tmp2.cmd
+	rm -fv test.dsk test2.dsk nul.err tmp.cmd tmp1.cmd tmp2.cmd pcx2vrl
 	exit 0
 fi
 
