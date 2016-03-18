88f42dd8a3f11c0ff3ea101b95e0415e1a141703
Jonathan Campbell <jonathan@castus.tv>
2016-03-18 01:22:31 -0700

    more

--- hw/cpu/cpuiopd.asm
+++ hw/cpu/cpuiopd.asm
@@ -1,55 +1,28 @@
+; cpuiopd.asm
+;
+; <fixme>
+; (C) 2009-2012 Jonathan Campbell.
+; Hackipedia DOS library.
+;
+; This code is licensed under the LGPL.
+; <insert LGPL legal text here>
+;
 
-section .text class=CODE %segment_use
+; NTS: We use NASM (Netwide Assembler) to achieve our goals here because WASM (Watcom Assembler) sucks.
+;      I'll consider using their assembler when they get a proper conditional macro system in place.
 
-%if TARGET_MSDOS == 32
-bits 32
-%else
-bits 16
-%endif
+; handy memory model defines
+%include "_memmodl.inc"
 
-; NTS: If we code 'push ax' and 'popf' for the 16-bit tests in 32-bit protected mode we will screw up the stack pointer and crash
-;      so we avoid duplicate code by defining 'native' pushf/popf functions and 'result' to ax or eax depending on CPU mode
-%if TARGET_MSDOS == 32
- %define point_s esi
- %define result eax
- %define pushfn pushfd
- %define popfn popfd
-use32
-%else
- %define point_s si
- %define result ax
- %define pushfn pushf
- %define popfn popf
-use16
-%endif
+; handy defines for watcall handling
+%include "_watcall.inc"
 
-%if TARGET_MSDOS == 16
- %ifndef MMODE
-  %error You must specify MMODE variable (memory model) for 16-bit real mode code
- %endif
-%endif
+; ---------- CODE segment -----------------
+%include "_segcode.inc"
 
-%if TARGET_MSDOS == 16
- %ifidni MMODE,l
-  %define retnative retf
-  %define cdecl_param_offset 6	; RETF addr + PUSH BP
- %else
-  %ifidni MMODE,h
-   %define retnative retf
-   %define cdecl_param_offset 6	; RETF addr + PUSH BP
-  %else
-   %ifidni MMODE,m
-    %define retnative retf
-    %define cdecl_param_offset 6 ; RETF addr + PUSH BP
-   %else
-    %define retnative ret
-    %define cdecl_param_offset 4 ; RET addr + PUSH BP
-   %endif
-  %endif
- %endif
-%else
- %define retnative ret
- %define cdecl_param_offset 8	; RET addr + PUSH EBP
+; NASM won't do it for us... make sure "retnative" is defined
+%ifndef retnative
+ %error retnative not defined
 %endif
 
 %if TARGET_MSDOS == 16
