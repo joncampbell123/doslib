62fcb2a4cec38f9ca3ad6c8a9a0b3d469b41f576
Jonathan Campbell <jonathan@castus.tv>
2016-04-03 10:57:15 -0700

    cleanup

--- hw/vga/pcx2vrl.c
+++ hw/vga/pcx2vrl.c
@@ -8,20 +8,11 @@
 #include <string.h>
 #include <unistd.h>
 
+#include "vrl.h"
+
 #ifndef O_BINARY
 #define O_BINARY (0)
 #endif
-
-#pragma pack(push,1)
-struct vrl_header {
-	uint8_t			vrl_sig[4];		// +0x00  "VRL1"
-	uint8_t			fmt_sig[4];		// +0x04  "VGAX"
-	uint16_t		height;			// +0x08  Sprite height
-	uint16_t		width;			// +0x0A  Sprite width
-	int16_t			hotspot_x;		// +0x0C  Hotspot offset (X) for programmer's reference
-	int16_t			hotspot_y;		// +0x0E  Hotspot offset (Y) for programmer's reference
-};							// =0x10
-#pragma pack(pop)
 
 #pragma pack(push,1)
 struct pcx_header {
@@ -230,7 +221,7 @@ int main(int argc,char **argv) {
 		return 1;
 	}
 	{
-		struct vrl_header hdr;
+		struct vrl1_vgax_header hdr;
 		memset(&hdr,0,sizeof(hdr));
 		memcpy(hdr.vrl_sig,"VRL1",4); // Vertical Run Length v1
 		memcpy(hdr.fmt_sig,"VGAX",4); // VGA mode X
