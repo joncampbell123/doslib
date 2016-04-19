ef2fcb68ef3c2f9ab023bd455acf488fda73c6eb
Jonathan Campbell <jonathan@castus.tv>
2016-04-18 22:51:22 -0700

    Ni-hon-GO, Ni-hon-GO. Japanese language, more than just "Japanese". You suck at Japanese, Google Translate.

--- hw/necpc98/isjp_utf.h
+++ hw/necpc98/isjp_utf.h
@@ -1,5 +1,5 @@
 
 /* This is UTF-8. It will be converted to SHIFT-JIS by utility Perl script for display on PC-98 */
 /* NTS: Google Translate has a bug, I think. It will say the end is "de" but it's supposed to end in "desu". Kore Wa Nihongo Desu, right? --J.C. */
-#define THIS_IS_JAPANESE "これは日本です"
+#define THIS_IS_JAPANESE "これは日本語です"
 
