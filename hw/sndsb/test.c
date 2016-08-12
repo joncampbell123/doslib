4107ddf936158758802b603d46de52d7cc688ab0
Jonathan Campbell <jonathan@castus.tv>
2016-08-11 21:58:34 -0700

    next part, confirm that when mode == 0xF9, reading register 0x83 toggles all bits, as noted on real hardware and emulated by DOSBox/DOSBox-X.

--- hw/sndsb/test.c
+++ hw/sndsb/test.c
@@ -4177,12 +4177,14 @@ int sndsb_check_for_sb16_asp(struct sndsb_ctx *cx) {
     //      Creative's software seems to do it wrong, not sure how it worked (mode=0xFC then mode=0xFA, then test register).
     //      On my SB16 ASP, setting mode=0xFA then writing the register allows ONE write, then further writes have no effect (and then this test fails).
     //      While at the same time, setting mode=0xFC allows the register to change at will.
+    // TODO: What does Creative's DIAGNOSE.EXE accomplish by setting mode == 0xFA?
     if (sndsb_sb16asp_set_mode_register(cx,0xFC) < 0) // 0xFC == ??
         goto fail_need_reset;
 
+    if ((t1=sndsb_sb16asp_get_register(cx,0x83)) < 0)
+        goto fail_need_reset;
+
     for (i=0;i < 4;i++) {
-        if ((t1=sndsb_sb16asp_get_register(cx,0x83)) < 0)
-            goto fail_need_reset;
         if (sndsb_sb16asp_set_register(cx,0x83,(uint8_t)(~t1)))
             goto fail_need_reset;
         if ((t2=sndsb_sb16asp_get_register(cx,0x83)) < 0)
@@ -4197,10 +4199,27 @@ int sndsb_check_for_sb16_asp(struct sndsb_ctx *cx) {
             goto fail;
     }
 
-    // TODO: What does Creative's DIAGNOSE.EXE accomplish by setting mode == 0xFA?
+    // The next test is not done by the Linux kernel, but Creative's DIAGNOSE.EXE carries out this test:
+    // Setting mode == 0xF9, then reading (not writing) register 0x83, causes all bits in the register to toggle.
+    // This behavior is confirmed to happen on real hardware (SB16 non-PnP with ASP).
+    if (sndsb_sb16asp_set_mode_register(cx,0xF9) < 0) // 0xF9 == ??
+        goto fail_need_reset;
 
-    // TODO: Is it true from DOSBox ASP emulation that when mode == 0xF9, reading register 0x83 causes all bits to toggle?
+    // read value
+    if ((t1=sndsb_sb16asp_get_register(cx,0x83)) < 0)
+        goto fail_need_reset;
 
+    // then read the value again and expect all bits to toggle. else the bits fail.
+    t2 = t1 ^ 0xFF;
+    for (i=0;i < 8;i++) {
+        if ((t1=sndsb_sb16asp_get_register(cx,0x83)) < 0)
+            goto fail_need_reset;
+
+        if (t1 != t2) goto fail_need_reset;
+        t2 = t1 ^ 0xFF;
+    }
+
+    // test is done
     if (sndsb_sb16asp_set_mode_register(cx,0x00) < 0) // 0x00 == ??
         goto fail_need_reset;
 
