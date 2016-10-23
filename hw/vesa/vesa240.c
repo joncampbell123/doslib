979eddc7c8c7b92b6905d147c242ef7ea055d847
Jonathan Campbell <jonathan@castus.tv>
2016-10-23 05:52:50 -0700

    use same resident-set hack for vesa240 as from vga240

--- hw/vesa/vesa240.c
+++ hw/vesa/vesa240.c
@@ -29,6 +29,7 @@ struct mode_remap {
 
 #define MAX_REMAP	256
 
+unsigned short      my_psp = 0;
 void			(__interrupt __far *old_int10)();
 struct mode_remap	remap[MAX_REMAP];
 int			remap_count=0;
@@ -70,16 +71,81 @@ void __interrupt __far new_int10(union INTPACK ip) {
 	_chain_intr(old_int10);
 }
 
+void reinit_int10_mode() {
+    unsigned short cur_cx=0,cur_dx=0;
+
+    /* save cursor position.
+     * INT 10h resets cursor to home even if not clearing screen (DOSBox-X behavior too). */
+    __asm {
+            push    ax
+            push    bx
+            push    cx
+            push    dx
+
+            mov     ah,0x03     ; get cursor position and size
+            xor     bx,bx       ; page 0
+            int     10h
+            mov     cur_cx,cx   ; start/end scanline
+            mov     cur_dx,dx   ; row/column position
+
+            pop     dx
+            pop     cx
+            pop     bx
+            pop     ax
+    }
+
+    /* NTS: Get the current video mode, then set the mode we just got with bit 7 set to NOT clear contents.
+     *      This preserves the screen while forcing 60Hz, so that danooct1 no longer has to explain what
+     *      "INT 10h hooked" means (ref https://www.youtube.com/watch?v=Okeyy5jIi9Y) */
+    __asm {
+            push    ax
+            push    bx
+
+            mov     ah,0x0F     ; get video mode
+            int     10h
+
+            xor     ah,ah
+            xor     bx,bx
+            or      al,0x80     ; set bit 7 to tell BIOS not to clear screen contents
+            int	    10h         ; AH=0 set video mode AL=mode | 0x80
+
+            pop     bx
+            pop	    ax
+    }
+
+    /* restore cursor position */
+    __asm {
+            push    ax
+            push    bx
+            push    cx
+            push    dx
+
+            mov     ah,0x01     ; set cursor shape
+            xor     bx,bx       ; page 0
+            mov     cx,cur_cx   ; start/end scanline
+            int     10h
+
+            mov     ah,0x02     ; set cursor position
+            xor     bx,bx       ; page 0
+            mov     dx,cur_dx   ; row/column position
+            int     10h
+
+            pop     dx
+            pop     cx
+            pop     bx
+            pop     ax
+    }
+
+}
+
 static void help() {
 	printf("VESA240 /INSTALL\n");
 	printf("\n");
 	printf("Intercept VESA BIOS calls to force video modes with at least 480 scan lines,\n");
 	printf("which may improve video quality with scan converters or enable capture/viewing\n");
 	printf("where devices would normally ignore 400-line output.\n");
-	printf("(C) 2014 Jonathan Campbell\n");
+	printf("(C) 2014-2016 Jonathan Campbell\n");
 }
-
-void end_of_resident();
 
 void install_320x200x256_remap() {
 	struct vbe_mode_info nmi;
@@ -120,6 +186,7 @@ void install_320x200x256_remap() {
 
 int main(int argc,char **argv) {
 	char *a,*command=NULL;
+        unsigned short resident_size = 0;
 	unsigned int entry;
 	uint16_t mode;
 	int i;
@@ -227,15 +294,63 @@ int main(int argc,char **argv) {
 		return 0;
 	}
 
-	_cli();
-	old_int10 = _dos_getvect(0x10);
-	_dos_setvect(0x10,new_int10);
-	_sti();
-	printf("INT 10h hooked\n"); fflush(stdout);
-	_dos_keep(0,(34000+sizeof(remap))>>4); /* FIXME! */
-	return 0;
-}
+        /* auto-detect the size of this EXE by the MCB preceeding the PSP segment */
+        /* do it BEFORE hooking in case shit goes wrong, then we can safely abort. */
+        /* the purpose of this code is to compensate for Watcom C's lack of useful */
+        /* info at runtime or compile time as to how large we are in memory. */
+        {
+            unsigned short env_seg=0;
+            unsigned short psp_seg=0;
+            unsigned char far *mcb;
 
-void end_of_resident() {
+            __asm {
+                push    ax
+                push    bx
+                mov     ah,0x51     ; get PSP segment
+                int     21h
+                mov     psp_seg,bx
+                pop     bx
+                pop     ax
+            }
+
+            mcb = MK_FP(psp_seg-1,0);
+
+            /* sanity check */
+            if (!(*mcb == 'M' || *mcb == 'Z')/*looks like MCB*/ ||
+                *((unsigned short far*)(mcb+1)) != psp_seg/*it's MY memory block*/) {
+                printf("Can't figure out my resident size, aborting\n");
+                return 1;
+            }
+
+            my_psp = psp_seg;
+            resident_size = *((unsigned short far*)(mcb+3)); /* number of paragraphs */
+            if (resident_size < 17) {
+                printf("Resident size is too small, aborting\n");
+                return 1;
+            }
+
+            /* while we're at it, free our environment block as well, we don't need it */
+            env_seg = *((unsigned short far*)MK_FP(psp_seg,0x2C));
+            if (env_seg != 0) {
+                if (_dos_freemem(env_seg) == 0) {
+                    *((unsigned short far*)MK_FP(psp_seg,0x2C)) = 0;
+                }
+                else {
+                    printf("WARNING: Unable to free environment block\n");
+                }
+            }
+        }
+
+		_cli();
+		old_int10 = _dos_getvect(0x10);
+		_dos_setvect(0x10,new_int10);
+		_sti();
+        reinit_int10_mode();
+
+        printf("INT 10h hooked [VESA240.EXE]. Video modes will be forced to 60Hz.\n");
+        printf("Resident size %uKB (%u paragraphs)\n",(resident_size + 0x3FU) >> 6U/*10-4*/,resident_size);
+
+		_dos_keep(0,resident_size);
+	return 0;
 }
 
