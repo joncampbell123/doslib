991748bf59ec74e9992ac999b8092061cb096d01
Jonathan Campbell <jonathan@castus.tv>
2016-11-02 02:06:53 -0700

    bugfix: typecast the CLSG header fields properly. this fixes crashes in small/medium memory models.

--- hw/dos/execlsg.h
+++ hw/dos/execlsg.h
@@ -8,11 +8,11 @@
 int exeload_clsg_validate_header(const struct exeload_ctx * const exe);
 
 static inline unsigned short exeload_clsg_function_count(const struct exeload_ctx * const exe) {
-    return *((unsigned short*)MK_FP(exe->base_seg,4));
+    return *((unsigned short far*)MK_FP(exe->base_seg,4));
 }
 
 static inline unsigned short exeload_clsg_function_offset(const struct exeload_ctx * const exe,const unsigned int i) {
-    return *((unsigned short*)MK_FP(exe->base_seg,4+2+(i*2U)));
+    return *((unsigned short far*)MK_FP(exe->base_seg,4+2+(i*2U)));
 }
 
 /* NTS: You're supposed to typecast return value into function pointer prototype */
