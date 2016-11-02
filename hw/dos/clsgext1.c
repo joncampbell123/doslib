2de3c83157322bb323312f5354309c5734dd3b6f
Jonathan Campbell <jonathan@castus.tv>
2016-11-02 02:02:37 -0700

    CLSG test please print to stdout not stderr

--- hw/dos/clsgext1.c
+++ hw/dos/clsgext1.c
@@ -16,22 +16,22 @@ struct exeload_ctx      final_exe = exeload_ctx_INIT;
 
 int main() {
     if (!exeload_load(&final_exe,"clsgexm1.dlm")) {
-        fprintf(stderr,"Load failed\n");
+        fprintf(stdout,"Load failed\n");
         return 1;
     }
     if (!exeload_clsg_validate_header(&final_exe)) {
-        fprintf(stderr,"DLM is not valid CLSG\n");
+        fprintf(stdout,"DLM is not valid CLSG\n");
         exeload_free(&final_exe);
         return 1;
     }
 
-    fprintf(stderr,"DLM image loaded to %04x:0000 residentlen=%lu\n",final_exe.base_seg,(unsigned long)final_exe.len_seg << 4UL);
+    fprintf(stdout,"DLM image loaded to %04x:0000 residentlen=%lu\n",final_exe.base_seg,(unsigned long)final_exe.len_seg << 4UL);
     {
         unsigned int i,m=exeload_clsg_function_count(&final_exe);
 
-        fprintf(stderr,"%u functions:\n",m);
+        fprintf(stdout,"%u functions:\n",m);
         for (i=0;i < m;i++)
-            fprintf(stderr,"  [%u]: %04x (%Fp)\n",i,exeload_clsg_function_offset(&final_exe,i),exeload_clsg_function_ptr(&final_exe,i));
+            fprintf(stdout,"  [%u]: %04x (%Fp)\n",i,exeload_clsg_function_offset(&final_exe,i),exeload_clsg_function_ptr(&final_exe,i));
     }
 
     /* let's call some! */
@@ -40,9 +40,9 @@ int main() {
         const char far * (CLSG_EXPORT_PROC *get_message)(void) = exeload_clsg_function_ptr(&final_exe,CLSGEXM1_get_message);
         const char far *msg;
 
-        fprintf(stderr,"Calling entry %u (get_message) now.\n",CLSGEXM1_get_message);
+        fprintf(stdout,"Calling entry %u (get_message) now.\n",CLSGEXM1_get_message);
         msg = get_message();
-        fprintf(stderr,"Result: %Fp = %Fs\n",msg,msg);
+        fprintf(stdout,"Result: %Fp = %Fs\n",msg,msg);
     }
 
     {
@@ -53,15 +53,15 @@ int main() {
         void (CLSG_EXPORT_PROC *set_value1)(const unsigned int v) = exeload_clsg_function_ptr(&final_exe,CLSGEXM1_set_value1);
         void (CLSG_EXPORT_PROC *set_value2)(const unsigned int v) = exeload_clsg_function_ptr(&final_exe,CLSGEXM1_set_value2);
 
-        fprintf(stderr,"Playing with data + bss values.\n");
-        fprintf(stderr,"Initial data value should be 0x1234,bss value should be 0x0000,value=data+bss\n");
-        fprintf(stderr,"value = %04x\n",get_value());
-        fprintf(stderr,"set value 0xABCD\n");
+        fprintf(stdout,"Playing with data + bss values.\n");
+        fprintf(stdout,"Initial data value should be 0x1234,bss value should be 0x0000,value=data+bss\n");
+        fprintf(stdout,"value = %04x\n",get_value());
+        fprintf(stdout,"set value 0xABCD\n");
         set_value1(0xABCD);
-        fprintf(stderr,"value = %04x\n",get_value());
-        fprintf(stderr,"set bss value 0x1111\n");
+        fprintf(stdout,"value = %04x\n",get_value());
+        fprintf(stdout,"set bss value 0x1111\n");
         set_value2(0x1111);
-        fprintf(stderr,"value = %04x\n",get_value());
+        fprintf(stdout,"value = %04x\n",get_value());
     }
 
     exeload_free(&final_exe);
