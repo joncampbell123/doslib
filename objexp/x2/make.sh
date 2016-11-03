80c5adecc0971300303bb107b32ebab78e9d7009
Jonathan Campbell <jonathan@castus.tv>
2016-11-03 07:29:54 -0700

    win95 bootable disk image to test driver. so far, it works!

--- objexp/x2/make.sh
+++ objexp/x2/make.sh
@@ -3,8 +3,12 @@ rel=../..
 if [ x"$TOP" == x ]; then TOP=`pwd`/$rel; fi
 . $rel/linux-ow.sh
 if [ "$1" == "clean" ]; then
-    rm -fv *.obj *.lib *.exe *.com *.bin drv.map
+    rm -fv *.obj *.lib *.exe *.com *.bin drv.map win95.dsk
 	exit 0
+elif [ "$1" == "disk" ]; then
+	# bootable win95 rescue disk with test program
+	gunzip -c -d win95.dsk.gz >win95.dsk
+	mcopy -i win95.dsk drv.exe ::drv.exe
 else
 	wmake -f watcom_c.mak
 fi
