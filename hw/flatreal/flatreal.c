a95c97c99364fd35493924b795440b886b3986a7
Jonathan Campbell <jonathan@castus.tv>
2016-11-13 12:08:39 -0800

    retab

--- hw/flatreal/flatreal.c
+++ hw/flatreal/flatreal.c
@@ -23,56 +23,56 @@
 void __cdecl flatrealmode_force_datasel(void *ptr);
 
 int flatrealmode_setup(uint32_t limit) {
-	const unsigned long segds = 0;
-	uint32_t *ap;
-	void *buf;
+    const unsigned long segds = 0;
+    uint32_t *ap;
+    void *buf;
 
-	/* Requires a 386 or higher. As usual, if the programmer doesn't bother to check the CPU
-	 * we go ahead and do it anyway */
-	if (cpu_basic_level < 3)
-		return 0;
+    /* Requires a 386 or higher. As usual, if the programmer doesn't bother to check the CPU
+     * we go ahead and do it anyway */
+    if (cpu_basic_level < 3)
+        return 0;
 
-	if (!flatrealmode_allowed())
-		return 0;
+    if (!flatrealmode_allowed())
+        return 0;
 
-	if (limit < 0xFFFFUL) limit = 0xFFFFUL;
-	limit >>= 12UL;
+    if (limit < 0xFFFFUL) limit = 0xFFFFUL;
+    limit >>= 12UL;
 
-	buf = malloc(16 + (8*3));
-	if (buf == NULL) return 0;
+    buf = malloc(16 + (8*3));
+    if (buf == NULL) return 0;
 
 #if defined(__LARGE__) || defined(__COMPACT__) /* 16-bit large memory model */
-	{
-		unsigned int seg = FP_SEG(buf);
-		unsigned int ofs = FP_OFF(buf);
-		ofs = (ofs + 15) & (~15);
-		seg += ofs >> 4;
-		ofs &= 0xF;
-		ap = MK_FP(seg,ofs);
-	}
+    {
+        unsigned int seg = FP_SEG(buf);
+        unsigned int ofs = FP_OFF(buf);
+        ofs = (ofs + 15) & (~15);
+        seg += ofs >> 4;
+        ofs &= 0xF;
+        ap = MK_FP(seg,ofs);
+    }
 #else
-	ap = (uint32_t*)(((size_t)buf + 15) & (~15));
+    ap = (uint32_t*)(((size_t)buf + 15) & (~15));
 #endif
 
-	/* GDT[0] = null */
-	ap[0] = ap[1] = 0;
-	/* GDT[1] = 32-bit data segment */
-	ap[2] = (segds << 16UL) | (limit & 0xFFFFUL);
-	ap[3] = (limit & 0xF0000UL) | (0x80UL << 16UL) | (0x93UL << 8UL) | (segds >> 16UL);
-	/* GDT[2] = 16-bit code segment */
-	{
-		void far *ptr = (void far*)flatrealmode_force_datasel;
-		unsigned int seg = FP_SEG(ptr);
-		unsigned long sego = (unsigned long)seg << 4UL;
-/*		printf("%04x:%04x %05lx\n",seg,FP_OFF(ptr),sego); */
-		ap[4] = (sego << 16UL) | 0xFFFFUL;
-		ap[5] = 0xF0000UL | (0x80UL << 16UL) | (0x9BUL << 8UL) | (sego >> 16UL);
-	}
+    /* GDT[0] = null */
+    ap[0] = ap[1] = 0;
+    /* GDT[1] = 32-bit data segment */
+    ap[2] = (segds << 16UL) | (limit & 0xFFFFUL);
+    ap[3] = (limit & 0xF0000UL) | (0x80UL << 16UL) | (0x93UL << 8UL) | (segds >> 16UL);
+    /* GDT[2] = 16-bit code segment */
+    {
+        void far *ptr = (void far*)flatrealmode_force_datasel;
+        unsigned int seg = FP_SEG(ptr);
+        unsigned long sego = (unsigned long)seg << 4UL;
+/*      printf("%04x:%04x %05lx\n",seg,FP_OFF(ptr),sego); */
+        ap[4] = (sego << 16UL) | 0xFFFFUL;
+        ap[5] = 0xF0000UL | (0x80UL << 16UL) | (0x9BUL << 8UL) | (sego >> 16UL);
+    }
 
-	flatrealmode_force_datasel(ap);
+    flatrealmode_force_datasel(ap);
 
-	free(buf);
-	return 1;
+    free(buf);
+    return 1;
 }
 #endif
 
--- hw/flatreal/flatreal.h
+++ hw/flatreal/flatreal.h
@@ -8,91 +8,91 @@
 
 unsigned char flatrealmode_readb(uint32_t addr);
 #pragma aux flatrealmode_readb = \
-	".386p" \
-	"push	es" \
-	"xor	cx,cx" \
-	"mov	es,cx" \
-	"shl	edx,16" \
-	"and	eax,0xFFFF" \
-	"mov	al,es:[eax+edx]" \
-	"pop	es" \
-	modify [cx dx] \
-	parm [dx ax] \
-	value [al]
+    ".386p" \
+    "push   es" \
+    "xor    cx,cx" \
+    "mov    es,cx" \
+    "shl    edx,16" \
+    "and    eax,0xFFFF" \
+    "mov    al,es:[eax+edx]" \
+    "pop    es" \
+    modify [cx dx] \
+    parm [dx ax] \
+    value [al]
 
 uint16_t flatrealmode_readw(uint32_t addr);
 #pragma aux flatrealmode_readw = \
-	".386p" \
-	"push	es" \
-	"xor	cx,cx" \
-	"mov	es,cx" \
-	"shl	edx,16" \
-	"and	eax,0xFFFF" \
-	"mov	ax,es:[eax+edx]" \
-	"pop	es" \
-	modify [cx dx] \
-	parm [dx ax] \
-	value [ax]
+    ".386p" \
+    "push   es" \
+    "xor    cx,cx" \
+    "mov    es,cx" \
+    "shl    edx,16" \
+    "and    eax,0xFFFF" \
+    "mov    ax,es:[eax+edx]" \
+    "pop    es" \
+    modify [cx dx] \
+    parm [dx ax] \
+    value [ax]
 
 uint32_t flatrealmode_readd(uint32_t addr);
 #pragma aux flatrealmode_readd = \
-	".386p" \
-	"push	es" \
-	"xor	cx,cx" \
-	"mov	es,cx" \
-	"shl	edx,16" \
-	"and	eax,0xFFFF" \
-	"mov	eax,es:[eax+edx]" \
-	"mov	edx,eax" \
-	"shr	edx,16" \
-	"pop	es" \
-	modify [cx] \
-	parm [dx ax] \
-	value [dx ax]
+    ".386p" \
+    "push   es" \
+    "xor    cx,cx" \
+    "mov    es,cx" \
+    "shl    edx,16" \
+    "and    eax,0xFFFF" \
+    "mov    eax,es:[eax+edx]" \
+    "mov    edx,eax" \
+    "shr    edx,16" \
+    "pop    es" \
+    modify [cx] \
+    parm [dx ax] \
+    value [dx ax]
 
 void flatrealmode_writeb(uint32_t addr,uint8_t c);
 #pragma aux flatrealmode_writeb = \
-	".386p" \
-	"push	es" \
-	"xor	cx,cx" \
-	"mov	es,cx" \
-	"shl	edx,16" \
-	"and	eax,0xFFFF" \
-	"mov	es:[eax+edx],bl" \
-	"pop	es" \
-	modify [cx dx] \
-	parm [dx ax] [bl]
+    ".386p" \
+    "push   es" \
+    "xor    cx,cx" \
+    "mov    es,cx" \
+    "shl    edx,16" \
+    "and    eax,0xFFFF" \
+    "mov    es:[eax+edx],bl" \
+    "pop    es" \
+    modify [cx dx] \
+    parm [dx ax] [bl]
 
 void flatrealmode_writew(uint32_t addr,uint16_t c);
 #pragma aux flatrealmode_writew = \
-	".386p" \
-	"push	es" \
-	"xor	cx,cx" \
-	"mov	es,cx" \
-	"shl	edx,16" \
-	"and	eax,0xFFFF" \
-	"mov	es:[eax+edx],bx" \
-	"pop	es" \
-	modify [cx dx] \
-	parm [dx ax] [bx]
+    ".386p" \
+    "push   es" \
+    "xor    cx,cx" \
+    "mov    es,cx" \
+    "shl    edx,16" \
+    "and    eax,0xFFFF" \
+    "mov    es:[eax+edx],bx" \
+    "pop    es" \
+    modify [cx dx] \
+    parm [dx ax] [bx]
 
 void flatrealmode_writed(uint32_t addr,uint32_t c);
 #pragma aux flatrealmode_writed = \
-	".386p" \
-	"push	es" \
-	"push	cx" \
-	"xor	cx,cx" \
-	"mov	es,cx" \
-	"pop	cx" \
-	"shl	edx,16" \
-	"and	eax,0xFFFF" \
-	"shl	ecx,16" \
-	"and	ebx,0xFFFF" \
-	"add	ebx,ecx" \
-	"mov	es:[eax+edx],ebx" \
-	"pop	es" \
-	modify [bx cx dx] \
-	parm [dx ax] [cx bx]
+    ".386p" \
+    "push   es" \
+    "push   cx" \
+    "xor    cx,cx" \
+    "mov    es,cx" \
+    "pop    cx" \
+    "shl    edx,16" \
+    "and    eax,0xFFFF" \
+    "shl    ecx,16" \
+    "and    ebx,0xFFFF" \
+    "add    ebx,ecx" \
+    "mov    es:[eax+edx],ebx" \
+    "pop    es" \
+    modify [bx cx dx] \
+    parm [dx ax] [cx bx]
 
 int flatrealmode_setup(uint32_t limit);
 int flatrealmode_test(); /* ASM */ /* 1=flat real mode not active   0=flat real mode active */ /* FIXME: Give this a more descriptive name. It's confusing in it's current form */
@@ -102,9 +102,9 @@ int flatrealmode_test(); /* ASM */ /* 1=flat real mode not active   0=flat real 
 #define flatrealmode_ok() (flatrealmode_test() == 0)
 
 /* feed this constant into the setup function to enable the full 4GB range */
-#define FLATREALMODE_4GB			0xFFFFFFFFUL
+#define FLATREALMODE_4GB            0xFFFFFFFFUL
 /* feed this constant into the setup function to re-impose the 64KB real-mode limit */
-#define FLATREALMODE_64KB			0x0000FFFFUL
+#define FLATREALMODE_64KB           0x0000FFFFUL
 
 #endif /* TARGET_MSDOS == 16 */
 
