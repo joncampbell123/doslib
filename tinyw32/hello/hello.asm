f712c5a3d0d4aaa60c2abeb4937834f8f980630c
Jonathan Campbell <jonathan@castus.tv>
2016-11-13 11:33:31 -0800

    tab conversion

--- tinyw32/hello/hello.asm
+++ tinyw32/hello/hello.asm
@@ -7,19 +7,19 @@ global _WinMain
 _WinMain:
 
 ; int WINAPI MessageBoxA(HWND hwnd,LPCTSTR lpText,LPCTSTR lpCaption,UINT type)
-		push	dword 0x00000000		; MK_OK
-		push	dword HelloCaption
-		push	dword HelloText
-		push	dword 0
-		call	_MessageBoxA@16
+        push    dword 0x00000000        ; MK_OK
+        push    dword HelloCaption
+        push    dword HelloText
+        push    dword 0
+        call    _MessageBoxA@16
 
 ; VOID WINAPI ExitProcess(UINT code)
-		push	dword 0
-		call	_ExitProcess@4
+        push    dword 0
+        call    _ExitProcess@4
 
 ; strings
-HelloCaption:	db	"Title",0
-HelloText:	db	"Hello world!",0
+HelloCaption:   db  "Title",0
+HelloText:      db  "Hello world!",0
 
 ; entry
 extern _MessageBoxA@16
--- tinyw32/hello/make.sh
+++ tinyw32/hello/make.sh
@@ -8,29 +8,29 @@ win32=1 # Windows 3.1
 winnt=1
 
 if [ "$1" == "clean" ]; then
-	do_clean
-	rm -fv test.dsk
-	exit 0
+    do_clean
+    rm -fv test.dsk
+    exit 0
 fi
 
 if [ "$1" == "disk" ]; then
-	make_msdos_data_disk test.dsk || exit 1
-	mcopy -i test.dsk win32/hello.exe ::hello.exe
+    make_msdos_data_disk test.dsk || exit 1
+    mcopy -i test.dsk win32/hello.exe ::hello.exe
 fi
 
 if [[ "$1" == "build" || "$1" == "" ]]; then
-	make_buildlist
-	begin_bat
+    make_buildlist
+    begin_bat
 
-	what=all
-	if [ x"$2" != x ]; then what="$2"; fi
-	if [ x"$3" != x ]; then build_list="$3"; fi
+    what=all
+    if [ x"$2" != x ]; then what="$2"; fi
+    if [ x"$3" != x ]; then build_list="$3"; fi
 
-	for name in $build_list; do
-		do_wmake $name "$what" || exit 1
-		bat_wmake $name "$what" || exit 1
-	done
+    for name in $build_list; do
+        do_wmake $name "$what" || exit 1
+        bat_wmake $name "$what" || exit 1
+    done
 
-	end_bat
+    end_bat
 fi
 
