945e25f5d4d804651609f8d8d1dc744d434834e4
Jonathan Campbell <jonathan@castus.tv>
2016-11-24 22:09:46 -0800

    remove duplicate code

--- tool/omfdump/omfdump.c
+++ tool/omfdump/omfdump.c
@@ -119,27 +119,26 @@ const char *omf_rectype_to_str(unsigned char rt) {
 
 //================================== cstr ================================
 
-int cstr_set_n(char ** const p,const char * const str,const size_t strl) {
-    char *x;
-
-    if (*p != NULL) {
-        free(*p);
-        *p = NULL;
-    }
-
-    x = malloc(strl+1);
-    if (x == NULL) return -1;
-    memcpy(x,str,strl);
-    x[strl] = 0;
-    *p = x;
-    return 0;
-}
-
 void cstr_free(char ** const p) {
     if (*p != NULL) {
         free(*p);
         *p = NULL;
     }
+}
+
+int cstr_set_n(char ** const p,const char * const str,const size_t strl) {
+    char *x;
+
+    cstr_free(p);
+
+    x = malloc(strl+1);
+    if (x == NULL) return -1;
+
+    memcpy(x,str,strl);
+    x[strl] = 0;
+
+    *p = x;
+    return 0;
 }
 
 //================================== records ================================
