97cc7dc63d7e835597f679f9a19c5c2dd72c10d1
Jonathan Campbell <jonathan@castus.tv>
2016-11-24 23:23:56 -0800

    more

--- tool/omfdump/omfdump.c
+++ tool/omfdump/omfdump.c
@@ -11,110 +11,13 @@
 #include <fcntl.h>
 #include <stdio.h>
 
+#include "omfrecs.h"
 #include "omfcstr.h"
 #include "omfrec.h"
 
 #ifndef O_BINARY
 #define O_BINARY (0)
 #endif
-
-//================================== OMF rec types strings ================================
-
-const char *omf_rectype_to_str_long(unsigned char rt) {
-    switch (rt) {
-        case 0x80:  return "Translator Header Record";
-        case 0x82:  return "Library Module Header Record";
-        case 0x88:  return "Comment Record";
-        case 0x8A:  return "Module End Record";
-        case 0x8B:  return "Module End Record (32-bit)";
-        case 0x8C:  return "External Names Definition Record";
-        case 0x90:  return "Public Names Definition Record";
-        case 0x91:  return "Public Names Definition Record (32-bit)";
-        case 0x94:  return "Line Numbers Record";
-        case 0x95:  return "Line Numbers Record (32-bit)";
-        case 0x96:  return "List of Names Record";
-        case 0x98:  return "Segment Definition Record";
-        case 0x99:  return "Segment Definition Record (32-bit)";
-        case 0x9A:  return "Group Definition Record";
-        case 0x9C:  return "Fixup Record";
-        case 0x9D:  return "Fixup Record (32-bit)";
-        case 0xA0:  return "Logical Enumerated Data Record";
-        case 0xA1:  return "Logical Enumerated Data Record (32-bit)";
-        case 0xA2:  return "Logical Iterated Data Record";
-        case 0xA3:  return "Logical Iterated Data Record (32-bit)";
-        case 0xB0:  return "Communal Names Definition Record";
-        case 0xB2:  return "Backpatch Record";
-        case 0xB3:  return "Backpatch Record (32-bit)";
-        case 0xB4:  return "Local External Names Definition Record";
-        case 0xB6:  return "Local Public Names Definition Record";
-        case 0xB7:  return "Local Public Names Definition Record (32-bit)";
-        case 0xB8:  return "Local Communal Names Definition Record";
-        case 0xBC:  return "COMDAT External Names Definition Record";
-        case 0xC2:  return "Initialized Communal Data Record";
-        case 0xC3:  return "Initialized Communal Data Record (32-bit)";
-        case 0xC4:  return "Symbol Line Numbers Record";
-        case 0xC5:  return "Symbol Line Numbers Record (32-bit)";
-        case 0xC6:  return "Alias Definition Record";
-        case 0xC8:  return "Named Backpatch Record";
-        case 0xC9:  return "Named Backpatch Record (32-bit)";
-        case 0xCA:  return "Local Logical Names Definition Record";
-        case 0xCC:  return "OMF Version Number Record";
-        case 0xCE:  return "Vendor-specific OMF Extension Record";
-        case 0xF0:  return "Library Header Record";
-        case 0xF1:  return "Library End Record";
-        default:    break;
-    }
-
-    return "?";
-}
-
-const char *omf_rectype_to_str(unsigned char rt) {
-    switch (rt) {
-        case 0x80:  return "THEADR";
-        case 0x82:  return "LHEADR";
-        case 0x88:  return "COMENT";
-        case 0x8A:  return "MODEND";
-        case 0x8B:  return "MODEND32";
-        case 0x8C:  return "EXTDEF";
-        case 0x90:  return "PUBDEF";
-        case 0x91:  return "PUBDEF32";
-        case 0x94:  return "LINNUM";
-        case 0x95:  return "LINNUM32";
-        case 0x96:  return "LNAMES";
-        case 0x98:  return "SEGDEF";
-        case 0x99:  return "SEGDEF32";
-        case 0x9A:  return "GRPDEF";
-        case 0x9C:  return "FIXUPP";
-        case 0x9D:  return "FIXUPP32";
-        case 0xA0:  return "LEDATA";
-        case 0xA1:  return "LEDATA32";
-        case 0xA2:  return "LIDATA";
-        case 0xA3:  return "LIDATA32";
-        case 0xB0:  return "COMDEF";
-        case 0xB2:  return "BAKPAT";
-        case 0xB3:  return "BAKPAT32";
-        case 0xB4:  return "LEXTDEF";
-        case 0xB6:  return "LPUBDEF";
-        case 0xB7:  return "LPUBDEF32";
-        case 0xB8:  return "LCOMDEF";
-        case 0xBC:  return "CEXTDEF";
-        case 0xC2:  return "COMDAT";
-        case 0xC3:  return "COMDAT32";
-        case 0xC4:  return "LINSYM";
-        case 0xC5:  return "LINSYM32";
-        case 0xC6:  return "ALIAS";
-        case 0xC8:  return "NBKPAT";
-        case 0xC9:  return "NBKPAT32";
-        case 0xCA:  return "LLNAMES";
-        case 0xCC:  return "VERNUM";
-        case 0xCE:  return "VENDEXT";
-        case 0xF0:  return "LIBHEAD"; /* I made up this name */
-        case 0xF1:  return "LIBEND"; /* I also made up this name */
-        default:    break;
-    }
-
-    return "?";
-}
 
 //================================== LNAMES ================================
 
---
+++ tool/omfdump/omfrecs.h
@@ -0,0 +1,4 @@
+
+const char *omf_rectype_to_str_long(unsigned char rt);
+const char *omf_rectype_to_str(unsigned char rt);
+
