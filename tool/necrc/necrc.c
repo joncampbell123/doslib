671961e4c07a6c39b92dafc4464334d594aa60b2
Jonathan Campbell <jonathan@castus.tv>
2017-01-12 01:55:44 -0800

    try CRC

--- tool/necrc/necrc.c
+++ tool/necrc/necrc.c
@@ -1,9 +1,177 @@
 
+#include <stdbool.h>
 #include <limits.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <ctype.h>
 #include <stdio.h>
+
+/**
+ * \file x.h
+ * Functions and types for CRC checks.
+ *
+ * Generated on Thu Jan 12 01:54:09 2017,
+ * by pycrc v0.9, https://pycrc.org
+ * using the configuration:
+ *    Width         = 32
+ *    Poly          = 0x04c11db7
+ *    Xor_In        = 0xffffffff
+ *    ReflectIn     = True
+ *    Xor_Out       = 0xffffffff
+ *    ReflectOut    = True
+ *    Algorithm     = bit-by-bit
+ *****************************************************************************/
+#ifndef __X_H__
+#define __X_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * The definition of the used algorithm.
+ *
+ * This is not used anywhere in the generated code, but it may be used by the
+ * application code to call algoritm-specific code, is desired.
+ *****************************************************************************/
+#define CRC_ALGO_BIT_BY_BIT 1
+
+
+/**
+ * The type of the CRC values.
+ *
+ * This type must be big enough to contain at least 32 bits.
+ *****************************************************************************/
+typedef uint_fast32_t crc_t;
+
+
+/**
+ * Calculate the initial crc value.
+ *
+ * \return     The initial crc value.
+ *****************************************************************************/
+static inline crc_t crc_init(void)
+{
+    return 0x46af6449;
+}
+
+
+/**
+ * Update the crc value with new data.
+ *
+ * \param crc      The current crc value.
+ * \param data     Pointer to a buffer of \a data_len bytes.
+ * \param data_len Number of bytes in the \a data buffer.
+ * \return         The updated crc value.
+ *****************************************************************************/
+crc_t crc_update(crc_t crc, const void *data, size_t data_len);
+
+
+/**
+ * Calculate the final crc value.
+ *
+ * \param crc  The current crc value.
+ * \return     The final crc value.
+ *****************************************************************************/
+crc_t crc_finalize(crc_t crc);
+
+
+#ifdef __cplusplus
+}           /* closing brace for extern "C" */
+#endif
+
+#endif      /* __X_H__ */
+/**
+ * \file x.c
+ * Functions and types for CRC checks.
+ *
+ * Generated on Thu Jan 12 01:54:12 2017,
+ * by pycrc v0.9, https://pycrc.org
+ * using the configuration:
+ *    Width         = 32
+ *    Poly          = 0x04c11db7
+ *    Xor_In        = 0xffffffff
+ *    ReflectIn     = True
+ *    Xor_Out       = 0xffffffff
+ *    ReflectOut    = True
+ *    Algorithm     = bit-by-bit
+ *****************************************************************************/
+
+static crc_t crc_reflect(crc_t data, size_t data_len);
+
+/**
+ * Reflect all bits of a \a data word of \a data_len bytes.
+ *
+ * \param data         The data word to be reflected.
+ * \param data_len     The width of \a data expressed in number of bits.
+ * \return             The reflected data.
+ *****************************************************************************/
+crc_t crc_reflect(crc_t data, size_t data_len)
+{
+    unsigned int i;
+    crc_t ret;
+
+    ret = data & 0x01;
+    for (i = 1; i < data_len; i++) {
+        data >>= 1;
+        ret = (ret << 1) | (data & 0x01);
+    }
+    return ret;
+}
+
+
+/**
+ * Update the crc value with new data.
+ *
+ * \param crc      The current crc value.
+ * \param data     Pointer to a buffer of \a data_len bytes.
+ * \param data_len Number of bytes in the \a data buffer.
+ * \return         The updated crc value.
+ *****************************************************************************/
+crc_t crc_update(crc_t crc, const void *data, size_t data_len)
+{
+    const unsigned char *d = (const unsigned char *)data;
+    unsigned int i;
+    bool bit;
+    unsigned char c;
+
+    while (data_len--) {
+        c = crc_reflect(*d++, 8);
+        for (i = 0; i < 8; i++) {
+            bit = crc & 0x80000000;
+            crc = (crc << 1) | ((c >> (7 - i)) & 0x01);
+            if (bit) {
+                crc ^= 0x04c11db7;
+            }
+        }
+        crc &= 0xffffffff;
+    }
+    return crc & 0xffffffff;
+}
+
+
+/**
+ * Calculate the final crc value.
+ *
+ * \param crc  The current crc value.
+ * \return     The final crc value.
+ *****************************************************************************/
+crc_t crc_finalize(crc_t crc)
+{
+    unsigned int i;
+    bool bit;
+
+    for (i = 0; i < 32; i++) {
+        bit = crc & 0x80000000;
+        crc = (crc << 1) | 0x00;
+        if (bit) {
+            crc ^= 0x04c11db7;
+        }
+    }
+    crc = crc_reflect(crc, 32);
+    return (crc ^ 0xffffffff) & 0xffffffff;
+}
 
 int main(int argc,char **argv) {
     if (argc < 2) {
