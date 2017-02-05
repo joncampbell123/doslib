e0952656c01d9b6b0f4fdb4b6288bc4d19273211
Jonathan Campbell <jonathan@castus.tv>
2017-02-05 13:59:30 -0800

    more formatting

--- tool/decompil/ddkincservices2str.sh
+++ tool/decompil/ddkincservices2str.sh
@@ -2,5 +2,11 @@
 #
 # Example script to take VMM.INC and dump all VMM_Service enumerations
 file=/mnt/main/emu/win31ddk/DDK/386/include/vmm.inc
-grep -R "VMM_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | while read X; do echo "\"$X\","; done
+count=0
+grep -R "VMM_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | while read X; do
+    counth=`printf 0x%04X $count`;
+    echo "    \"$X\", /* $counth */";
+
+    count=$(($count+1))
+done
 
--- tool/decompil/ddkincservices2str_win95.sh
+++ tool/decompil/ddkincservices2str_win95.sh
@@ -2,5 +2,10 @@
 #
 # Example script to take VMM.INC and dump all VMM_Service enumerations (Windows 95 DDK)
 file=~/Downloads/x/ddk95/Inc32/VMM.INC
-grep -R "VMM_Service" $file | sed -e "s/VMM_Service */VMM_Service"$'\t'/ | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | while read X; do echo "\"$X\","; done
+grep -R "VMM_Service" $file | sed -e "s/VMM_Service */VMM_Service"$'\t'/ | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | while read X; do
+    counth=`printf 0x%04X $count`;
+    echo "    \"$X\", /* $counth */";
+
+    count=$(($count+1))
+done
 
