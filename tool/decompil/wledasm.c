129aa55aa5e3cc139313e3fd3aaca555ca0a0755
Jonathan Campbell <jonathan@castus.tv>
2017-02-05 11:51:56 -0800

    move fixup patcher to function

--- tool/decompil/wledasm.c
+++ tool/decompil/wledasm.c
@@ -341,6 +341,111 @@ void dec_label_sort() {
     qsort(dec_label,dec_label_count,sizeof(*dec_label),dec_label_qsortcb);
 }
 
+///////
+
+void le_parser_apply_fixup(unsigned char * const data,const size_t datlen,const uint16_t object,const uint32_t data_object_offset,struct le_header_parseinfo *le_parser) {
+    struct exe_le_header_object_table_entry *objent;
+    struct le_header_fixup_record_table *frtable;
+    unsigned int srcoff_count,srcoff_i;
+    uint32_t page_first,page_last;
+    unsigned char flags,src;
+    unsigned char *raw;
+    uint32_t soffset;
+    uint16_t srcoff;
+    unsigned int ti;
+    uint32_t page;
+
+    if (datlen == 0)
+        return;
+    if (le_parser->le_fixup_records.table == NULL || le_parser->le_fixup_records.length == 0 ||
+        le_parser->le_object_table == NULL || object == 0 || object > le_parser->le_header.object_table_entries)
+        return;
+
+    objent = le_parser->le_object_table + object - 1;
+    page_first = data_object_offset / le_parser->le_header.memory_page_size;
+    page_last = (data_object_offset + datlen - 1) / le_parser->le_header.memory_page_size;
+
+    for (page=page_first;page <= page_last;page++) { // <- in case the DDB struct spans two pages
+        if (page > le_parser->le_fixup_records.length)
+            break;
+        if (page == 0)
+            continue;
+
+        frtable = le_parser->le_fixup_records.table + page - 1; // <- page numbers are 1-based
+        for (ti=0;ti < (unsigned int)frtable->length;ti++) {
+            raw = le_header_fixup_record_table_get_raw_entry(frtable,ti);
+            if (raw == NULL) continue;
+
+            // caller ensures the record is long enough
+            src = *raw++;
+            flags = *raw++;
+
+            if (src & 0xC0)
+                continue;
+
+            if (src & 0x20) {
+                srcoff_count = *raw++; //number of source offsets. object follows, then array of srcoff
+            }
+            else {
+                srcoff_count = 1;
+                srcoff = *((int16_t*)raw); raw += 2;
+            }
+
+            if ((flags&3) == 0) { // internal reference
+                uint16_t tobject;
+                uint32_t trgoff;
+
+                if (flags&0x40) {
+                    tobject = *((uint16_t*)raw); raw += 2;
+                }
+                else {
+                    tobject = *raw++;
+                }
+
+                if ((src&0xF) != 0x2) { /* not 16-bit selector fixup */
+                    if (flags&0x10) { // 32-bit target offset
+                        trgoff = *((uint32_t*)raw); raw += 4;
+                    }
+                    else { // 16-bit target offset
+                        trgoff = *((uint16_t*)raw); raw += 2;
+                    }
+                }
+                else {
+                    trgoff = 0;
+                }
+
+                if (src & 0x20) {
+                    for (srcoff_i=0;srcoff_i < srcoff_count;srcoff_i++) {
+                        srcoff = *((int16_t*)raw); raw += 2;
+
+                        // what is the relocation relative to the struct we just read?
+                        soffset = ((uint32_t)srcoff +
+                                (((uint32_t)page - (uint32_t)objent->page_map_index) * le_parser->le_header.memory_page_size)) - data_object_offset;
+
+                        // if it's within range, and in the same object, patch
+                        if ((soffset+4UL) <= datlen && tobject == object)
+                            *((uint32_t*)(data+soffset)) = trgoff;
+                    }
+                }
+                else {
+                    // what is the relocation relative to the struct we just read?
+                    soffset = ((uint32_t)srcoff +
+                            (((uint32_t)page - (uint32_t)objent->page_map_index) * le_parser->le_header.memory_page_size)) - data_object_offset;
+
+                    // if it's within range, and in the same object, patch
+                    if ((soffset+4UL) <= datlen && tobject == object)
+                        *((uint32_t*)(data+soffset)) = trgoff;
+                }
+            }
+            else {
+                continue;
+            }
+        }
+    }
+}
+
+///////
+
 int main(int argc,char **argv) {
     struct le_header_parseinfo le_parser;
     struct exe_le_header le_header;
@@ -619,8 +724,6 @@ int main(int argc,char **argv) {
             }
 
             if (le_segofs_to_trackio(&io,object,offset,&le_parser)) {
-                uint32_t page_number = io.page_number; // remember this, we'll need it later
-
                 printf("        File offset %lu (0x%lX) (page #%lu at %lu + page offset 0x%lX / 0x%lX)\n",
                         (unsigned long)io.file_ofs + (unsigned long)io.page_ofs,
                         (unsigned long)io.file_ofs + (unsigned long)io.page_ofs,
@@ -634,99 +737,9 @@ int main(int argc,char **argv) {
                 if (rd >= (int)sizeof(*ddb_31)) {
                     ddb_31 = (struct windows_vxd_ddb_win31*)ddb;
 
-                    /* wait---some VXDs have this structure in place but leave the fields empty (zeroed out) in the raw object data.
-                     * it turns out that the fields are filled in by relocation (fixup) data. some VXDs will seem to have empty fields
-                     * unless we apply relocations. */
-                    if (le_parser.le_fixup_records.table != NULL && le_parser.le_fixup_records.length != 0 &&
-                        le_parser.le_object_table != NULL && object > 0 && object <= le_header.object_table_entries) {
-                        struct exe_le_header_object_table_entry *objent = le_parser.le_object_table + object - 1;
-                        struct le_header_fixup_record_table *frtable;
-                        unsigned int srcoff_count,srcoff_i;
-                        unsigned char flags,src;
-                        unsigned char *raw;
-                        uint32_t soffset;
-                        uint16_t srcoff;
-                        unsigned int ti;
-                        uint32_t page;
-
-                        for (page=page_number;page <= io.page_number;page++) { // <- in case the DDB struct spans two pages
-                            if (page > le_parser.le_fixup_records.length)
-                                break;
-                            if (page == 0)
-                                continue;
-
-                            frtable = le_parser.le_fixup_records.table + page - 1; // <- page numbers are 1-based
-                            for (ti=0;ti < (unsigned int)frtable->length;ti++) {
-                                raw = le_header_fixup_record_table_get_raw_entry(frtable,ti);
-                                if (raw == NULL) continue;
-
-                                // caller ensures the record is long enough
-                                src = *raw++;
-                                flags = *raw++;
-
-                                if (src & 0xC0)
-                                    continue;
-
-                                if (src & 0x20) {
-                                    srcoff_count = *raw++; //number of source offsets. object follows, then array of srcoff
-                                }
-                                else {
-                                    srcoff_count = 1;
-                                    srcoff = *((int16_t*)raw); raw += 2;
-                                }
-
-                                if ((flags&3) == 0) { // internal reference
-                                    uint16_t tobject;
-                                    uint32_t trgoff;
-
-                                    if (flags&0x40) {
-                                        tobject = *((uint16_t*)raw); raw += 2;
-                                    }
-                                    else {
-                                        tobject = *raw++;
-                                    }
-
-                                    if ((src&0xF) != 0x2) { /* not 16-bit selector fixup */
-                                        if (flags&0x10) { // 32-bit target offset
-                                            trgoff = *((uint32_t*)raw); raw += 4;
-                                        }
-                                        else { // 16-bit target offset
-                                            trgoff = *((uint16_t*)raw); raw += 2;
-                                        }
-                                    }
-                                    else {
-                                        trgoff = 0;
-                                    }
-
-                                    if (src & 0x20) {
-                                        for (srcoff_i=0;srcoff_i < srcoff_count;srcoff_i++) {
-                                            srcoff = *((int16_t*)raw); raw += 2;
-
-                                            // what is the relocation relative to the struct we just read?
-                                            soffset = ((uint32_t)srcoff +
-                                                    (((uint32_t)page - (uint32_t)objent->page_map_index) * le_parser.le_header.memory_page_size)) - offset;
-
-                                            // if it's within range, and in the same object, patch
-                                            if ((soffset+4UL) <= (uint32_t)rd && tobject == object)
-                                                *((uint32_t*)(ddb+soffset)) = trgoff;
-                                        }
-                                    }
-                                    else {
-                                        // what is the relocation relative to the struct we just read?
-                                        soffset = ((uint32_t)srcoff +
-                                                (((uint32_t)page - (uint32_t)objent->page_map_index) * le_parser.le_header.memory_page_size)) - offset;
-
-                                        // if it's within range, and in the same object, patch
-                                        if ((soffset+4UL) <= (uint32_t)rd && tobject == object)
-                                            *((uint32_t*)(ddb+soffset)) = trgoff;
-                                    }
-                                }
-                                else {
-                                    continue;
-                                }
-                            }
-                        }
-                    }
+                    /* the DDB like anything else within the VXD can be patched by LE fixups.
+                     * if we don't do this the DDB will mysteriously show no entry points whatsoever. */
+                    le_parser_apply_fixup(ddb,(size_t)rd,object,offset,&le_parser);
 
                     printf("        Windows 386/VXD DDB structure:\n");
                     printf("            DDB_Next:               0x%08lX\n",(unsigned long)ddb_31->DDB_Next);
