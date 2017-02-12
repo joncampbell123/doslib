9759e6b71d92fda2ae4dd1c2455cf8afcc275165
Jonathan Campbell <jonathan@castus.tv>
2017-02-11 20:30:29 -0800

    more

--- windrv/dosboxpi/win9x/vxd/dboxvxd.asm
+++ windrv/dosboxpi/win9x/vxd/dboxvxd.asm
@@ -14,35 +14,6 @@ Declare_Virtual_Device DBOXMPI, 3, 0, DBOXMPI_Control, Undefined_Device_ID, Unde
 
 ;----------------------------------------------
 
-VxD_REAL_INIT_SEG
-
-BeginProc Int33_Real_Init
-
-	xor	bx, bx
-	xor	si, si
-	mov	ax, Device_Load_Ok
-	ret
-
-EndProc Int33_Real_Init
-
-VxD_REAL_INIT_ENDS
-
-;----------------------------------------------
-
-VxD_LOCKED_DATA_SEG
-
-    db      'Hello locked data',0
-
-VxD_LOCKED_DATA_ENDS
-
-;----------------------------------------------
-
-VxD_ICODE_SEG
-
-VxD_ICODE_ENDS
-
-;----------------------------------------------
-
 VxD_LOCKED_CODE_SEG
 
 BeginProc DBOXMPI_Control
@@ -52,31 +23,9 @@ BeginProc DBOXMPI_Control
 
 EndProc DBOXMPI_Control
 
-BeginProc ldummycode
-
-    ret
-
-    db      'This is locked code segment',0
-
-EndProc ldummycode
-
 VxD_LOCKED_CODE_ENDS
 
 ;----------------------------------------------
 
-VxD_CODE_SEG
-
-BeginProc dummycode
-
-    ret
-
-    db      'This is code segment',0
-
-EndProc dummycode
-
-VxD_CODE_ENDS
-
-;----------------------------------------------
-
-	END	Int33_Real_Init ; LE entry point always points to real-mode init code
+	END
 
