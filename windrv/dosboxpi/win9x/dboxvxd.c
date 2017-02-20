45c7c0fc26a605cebf82dc6857d2b4d95fe8ee23
Jonathan Campbell <jonathan@castus.tv>
2017-02-20 03:15:03 -0800

    VxD enum and work

--- windrv/dosboxpi/win9x/dboxvxd.c
+++ windrv/dosboxpi/win9x/dboxvxd.c
@@ -24,6 +24,20 @@ void vxd_control_proc(void);
 #define VM_Terminate                0x000A
 #define VM_Not_Executeable          0x000B
 #define Destroy_VM                  0x000C
+#define VM_Suspend                  0x000D
+#define VM_Resume                   0x000E
+#define Set_Device_Focus            0x000F
+#define Begin_Message_Mode          0x0010
+#define End_Message_Mode            0x0011
+#define Reboot_Processor            0x0012
+#define Query_Destroy               0x0013
+#define Debug_Query                 0x0014
+#define Begin_PM_App                0x0015
+#define End_PM_App                  0x0016
+#define Device_Reboot_Notify        0x0017
+#define Crit_Reboot_Notify          0x0018
+#define Close_VM_Notify             0x0019
+#define Power_Event                 0x001A
 
 /* VXD device IDs */
 #define VMM_Device_ID               0x0001
@@ -67,16 +81,114 @@ const struct windows_vxd_ddb_win31 __based( __segname("_CODE") ) DBOXMPI_DDB = {
     0x00000000UL                                            // +0x34 DDB_Service_Table_Size
 };
 
+/* VxD control message Sys_Critical_Init.
+ *
+ * Entry:
+ *   EAX = Sys_Critical_Init
+ *   EBX = handle of Sysem VM
+ *   ESI = Pointer to command tail retrived from PSP of WIN386.EXE
+ *
+ * Exit:
+ *   Carry flag = clear if success, set if failure */
 void __declspec(naked) my_sys_critical_init(void) {
-    VXD_INT3();
+    /* success */
     __asm {
         clc
         ret
     }
 }
 
+/* VxD control message Sys_Critical_Exit.
+ *
+ * Entry:
+ *   EAX = Sys_Critical_Exit
+ *
+ * Exit:
+ *   Carry flag = clear if success, set if failure */
 void __declspec(naked) my_sys_critical_exit(void) {
-    VXD_INT3();
+    /* success */
+    __asm {
+        clc
+        ret
+    }
+}
+
+/* VxD control message Device_Init.
+ *
+ * Entry:
+ *   EAX = Device_Init
+ *   EBX = handle of System VM
+ *   ESI = Pointer to command tail retrieved from PSP of WIN386.EXE
+ *
+ * Exit:
+ *   Carry flag = clear if success, set if failure */
+void __declspec(naked) my_device_init(void) {
+    /* success */
+    __asm {
+        clc
+        ret
+    }
+}
+
+/* VxD control message Init_Complete.
+ *
+ * Entry:
+ *   EAX = Init_Complete
+ *   EBX = handle of System VM
+ *   ESI = Pointer to command tail retrieved from PSP of WIN386.EXE
+ *
+ * Exit:
+ *   Carry flag = clear if success, set if failure */
+void __declspec(naked) my_init_complete(void) {
+    /* success */
+    __asm {
+        clc
+        ret
+    }
+}
+
+/* VxD control message Sys_VM_Init.
+ *
+ * Entry:
+ *   EAX = Sys_VM_Init
+ *   EBX = handle of System VM
+ *
+ * Exit:
+ *   Carry flag = clear if success, set if failure */
+void __declspec(naked) my_sys_vm_init(void) {
+    /* success */
+    __asm {
+        clc
+        ret
+    }
+}
+
+/* VxD control message VM_Init.
+ *
+ * Entry:
+ *   EAX = VM_Init
+ *   EBX = handle of new VM
+ *
+ * Exit:
+ *   Carry flag = clear if success, set if failure */
+void __declspec(naked) my_vm_init(void) {
+    /* success */
+    __asm {
+        clc
+        ret
+    }
+}
+
+/* VxD control message Sys_VM_Terminate.
+ *
+ * Entry:
+ *   EAX = Sys_VM_Terminate
+ *   EBX = handle of System VM
+ *
+ * Exit:
+ *   Carry flag = clear if success, set if failure */
+void __declspec(naked) my_sys_vm_terminate(void) {
+    /* success */
     __asm {
         clc
         ret
@@ -86,6 +198,11 @@ void __declspec(naked) my_sys_critical_exit(void) {
 void __declspec(naked) vxd_control_proc(void) {
     VXD_Control_Dispatch(Sys_Critical_Init, my_sys_critical_init);
     VXD_Control_Dispatch(Sys_Critical_Exit, my_sys_critical_exit);
+    VXD_Control_Dispatch(Sys_VM_Terminate, my_sys_vm_terminate);
+    VXD_Control_Dispatch(Init_Complete, my_init_complete);
+    VXD_Control_Dispatch(Sys_VM_Init, my_sys_vm_init);
+    VXD_Control_Dispatch(Device_Init, my_device_init);
+    VXD_Control_Dispatch(Sys_VM_Init, my_vm_init);
     __asm {
         clc
         ret
