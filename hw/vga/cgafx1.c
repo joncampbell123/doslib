e685a6c164a65dd7be25c30d924dbff6c973602d
Jonathan Campbell <jonathan@castus.tv>
2017-04-19 11:35:00 -0700

    explain keyboard loop processing in cgafx1.c

--- hw/vga/cgafx1.c
+++ hw/vga/cgafx1.c
@@ -56,6 +56,15 @@ int main() {
     do {
         if (inp(0x64) & 1) { // NTS: This is clever because on IBM PC hardware port 64h is an alias of 60h.
             unsigned char c = inp(0x60); // chances are the scancode has bit 0 set.
+            // remember that on AT hardware, port 64h bit 0 is set when a keyboard scancode has come in.
+            // on PC/XT hardware, 64h is an alias of port 60h, which returns the latest scancode transmitted
+            // by the keyboard. to keep the XT keyboard going, we have to set bit 7 of port 61h to clear
+            // and restart the scanning process or else we won't get any more scan codes. setting bit 7
+            // on AT hardware usually has no effect. We also have to ack IRQ 1 to make sure keyboard
+            // interrupts work when we're done with the program.
+            //
+            // by reading port 64h and 60h in this way we can break out of the loop reliably when the user
+            // hits a key on the keyboard, regardless whether the keyboard controller is AT or PC/XT hardware.
 
             if (c != 0 && (c & 0x80) == 0)
                 break;
