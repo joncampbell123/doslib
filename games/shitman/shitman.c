dac2cfe205617d6edcb6d483c23bc3d4254977d1
Jonathan Campbell <jonathan@castus.tv>
2017-05-14 04:32:58 -0700

    shitman begins

---
+++ games/shitman/shitman.c
@@ -0,0 +1,69 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <hw/dos/dos.h>
+#include <hw/dos/doswin.h>
+#include <hw/cpu/cpu.h>
+#include <hw/vga/vga.h>
+#include <hw/8042/8042.h>
+#include <hw/8254/8254.h>
+#include <hw/8259/8259.h>
+
+int initial_sys_check(void) {
+    /*============================*/
+    probe_dos();
+    if (dos_version < 0x500) {
+        puts("MS-DOS 5.00 or higher required");
+        return 0;
+    }
+    detect_windows();
+    if (windows_mode != WINDOWS_NONE) {
+        puts("Running from within Windows is not supported");
+        return 0;
+    }
+
+    /*============================*/
+#if TARGET_MSDOS == 16
+    cpu_probe();
+    if (cpu_basic_level < CPU_286) {
+        puts("286 or higher required");
+        return 0;
+    }
+#endif
+
+    /*============================*/
+    if (!k8042_probe()) {
+        puts("8042 keyboard not present");
+        return 0;
+    }
+
+    /*============================*/
+	if (!probe_8254()) {
+        puts("8254 timer not present");
+		return 0;
+	}
+
+    /*============================*/
+	if (!probe_8259()) {
+        puts("8259 interrupt controller not present");
+		return 0;
+	}
+
+    /*============================*/
+    if (!probe_vga()) {
+        puts("This game requires VGA or compatible graphics");
+		return 0;
+    }
+
+    return 1; /* OK */
+}
+
+int main(int argc,char **argv) {
+    if (!initial_sys_check())
+        return 1;
+
+    return 0;
+}
+
