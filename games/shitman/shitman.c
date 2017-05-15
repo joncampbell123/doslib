c0052d9cbad52b22881aa1062e1840adb17b9cdd
Jonathan Campbell <jonathan@castus.tv>
2017-05-15 10:29:28 -0700

    yay

--- games/shitman/shitman.c
+++ games/shitman/shitman.c
@@ -15,6 +15,8 @@
 #include <hw/8254/8254.h>
 #include <hw/8259/8259.h>
 
+#include "gif_lib.h"
+
 FILE *debug_log = NULL;
 
 void DEBUG(const char *fmt,...) {
@@ -163,8 +165,118 @@ int initial_sys_check(void) {
     return 1; /* OK */
 }
 
-void DisplayGIF(const char *path) {
+#define DisplayGIF_FADEIN       (1U << 0U)
+
+static inline unsigned char fadein_cap(unsigned char fade,unsigned char v) {
+    return (v > fade) ? fade : v;
+}
+
+void DisplayGIF(const char *path,unsigned int how) {
+    GifFileType *gif;
+    int err;
+
     DEBUG("DisplayGIF: %s",path);
+
+    gif = DGifOpenFileName(path,&err);
+    if (gif == NULL) {
+        DEBUG("DGifOpenFileName failed, err=%d",err);
+        return;
+    }
+
+    /* TODO: How do we read only the first image? */
+    if (DGifSlurp(gif) != GIF_OK) {
+        DEBUG("DGifSlurp failed Error=%u",gif->Error);
+        DGifCloseFile(gif,&err);
+        return;
+    }
+    if (gif->ImageCount == 0) {
+        DEBUG("No GIF images");
+        DGifCloseFile(gif,&err);
+        return;
+    }
+    if (gif->SColorMap == NULL) {
+        DEBUG("No colormap");
+        DGifCloseFile(gif,&err);
+        return;
+    }
+
+    { /* blank VGA palette */
+        unsigned int i;
+
+        vga_palette_lseek(0);
+        for (i=0;i < 256;i++) vga_palette_write(0,0,0);
+    }
+
+    /* assume gif->SavedImages != NULL
+     * assume gif->SavedImages[0] != NULL */
+    {
+        SavedImage *img = &gif->SavedImages[0];
+
+        // FIXME
+#if TARGET_MSDOS == 16
+        _fmemcpy(vga_state.vga_graphics_ram,img->RasterBits,320*200);
+#else
+        memcpy(vga_state.vga_graphics_ram,img->RasterBits,320*200);
+#endif
+    }
+
+    {
+        ColorMapObject *c = gif->SColorMap;
+        unsigned int i,fade;
+
+        fade = (how & DisplayGIF_FADEIN) ? 0 : 64;
+
+        do {
+            GifColorType *color = c->Colors;
+
+            vga_wait_for_vsync_end();
+
+            vga_palette_lseek(0);
+
+            i = c->ColorCount;
+            do {
+                vga_palette_write(
+                        fadein_cap(fade,color->Red>>2),
+                        fadein_cap(fade,color->Green>>2),
+                        fadein_cap(fade,color->Blue>>2));
+                color++;
+            } while (--i != 0);
+
+            vga_wait_for_vsync();
+        } while ((fade += 2) < 64);
+    }
+
+    /* wait */
+    while (getch() != 13);
+
+    if (how & DisplayGIF_FADEIN) {
+        ColorMapObject *c = gif->SColorMap;
+        unsigned int i,fade;
+
+        fade = 60;
+
+        do {
+            GifColorType *color = c->Colors;
+
+            vga_wait_for_vsync_end();
+
+            vga_palette_lseek(0);
+
+            i = c->ColorCount;
+            do {
+                vga_palette_write(
+                        fadein_cap(fade,color->Red>>2),
+                        fadein_cap(fade,color->Green>>2),
+                        fadein_cap(fade,color->Blue>>2));
+                color++;
+            } while (--i != 0);
+
+            vga_wait_for_vsync();
+        } while ((fade -= 4) != 0);
+    }
+
+    if (DGifCloseFile(gif,&err) != GIF_OK)
+        DEBUG("DGifCloseFile failed, err=%d",err);
 }
 
 int main(int argc,char **argv) {
@@ -176,9 +288,9 @@ int main(int argc,char **argv) {
     int10_setmode(0x13); /* 320x200x256-color */
     update_state_from_vga();
 
-    DisplayGIF("title1.gif");
-    DisplayGIF("title2.gif");
-    DisplayGIF("title3.gif");
+    DisplayGIF("title1.gif",DisplayGIF_FADEIN);
+    DisplayGIF("title2.gif",DisplayGIF_FADEIN);
+    DisplayGIF("title3.gif",DisplayGIF_FADEIN);
 
     int10_setmode(0x3); /* text mode */
     DEBUG_PRINT_MEM_STATE();
