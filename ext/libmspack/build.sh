2a7068e9807d2a7544fd1ea233fdb711207203e2
Jonathan Campbell <jonathan@castus.tv>
2017-07-28 14:23:17 -0700

    more

--- ext/libmspack/build.sh
+++ ext/libmspack/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 if [[ !( -f Makefile ) ]]; then
-    ./configure --prefix=/tmp/dummy --enable-static --disable-shared || exit 1
+    mkdir -p linux-host || exit 1
+    dst="`pwd`/linux-host"
+    ./configure "--prefix=$dst" --enable-static --disable-shared || exit 1
 fi
 make -j5 || exit 1
 
