c8bbf69dc80d2875e0060befc9e3c598ae2d806a
Jonathan Campbell <jonathan@castus.tv>
2017-07-28 14:29:39 -0700

    more

--- ext/libiconv/build.sh
+++ ext/libiconv/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 if [[ !( -f Makefile ) ]]; then
-    ./configure --prefix=/tmp/dummy --enable-static --disable-shared || exit 1
+    mkdir -p linux-host || exit 1
+    dst="`pwd`/linux-host"
+    ./configure "--prefix=$dst" --enable-static --disable-shared || exit 1
 fi
 make -j5 || exit 1
+make install || exit 1
 
