9b9aa043d6fe23d4f8f8dd9ce779bb0113ad5cd6
Jonathan Campbell <jonathan@castus.tv>
2017-09-30 14:15:53 -0700

    remove outdated comments, add VIM modeline

--- hw/sndsb/sbdspio.c
+++ hw/sndsb/sbdspio.c
@@ -32,7 +32,6 @@
 #endif
 
 int sndsb_read_dsp(struct sndsb_ctx * const cx) {
-    /* TODO: convert this to use cx->dspio_adj_base so we don't compute baseio+READ_STATUS+alias every time */
     const unsigned int status_port = cx->dspio+SNDSB_BIO_DSP_READ_STATUS;
     const unsigned int read_data_port = cx->dspio+SNDSB_BIO_DSP_READ_DATA;
     register unsigned char c; /* encourage C/C++ optimizer to convert c = inp() ... return c to just return inp() */
@@ -93,7 +92,6 @@ int sndsb_read_dsp(struct sndsb_ctx * const cx) {
 }
 
 int sndsb_write_dsp(struct sndsb_ctx * const cx,const uint8_t d) {
-    /* TODO: convert this to use cx->dspio_adj_base so we don't compute baseio+READ_STATUS+alias every time */
     const unsigned int status_port = cx->dspio+SNDSB_BIO_DSP_WRITE_STATUS;
     const unsigned int write_data_port = cx->dspio+SNDSB_BIO_DSP_WRITE_DATA;
 
@@ -152,3 +150,9 @@ int sndsb_write_dsp(struct sndsb_ctx * const cx,const uint8_t d) {
     return 0;
 }
 
+/*
+ * vim: set tabstop=4
+ * vim: set softtabstop=4
+ * vim: set shiftwidth=4
+ * vim: set expandtab
+ */
