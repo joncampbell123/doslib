efdb16ef8d525b6c322931307f524dd84b5232f8
Jonathan Campbell <jonathan@castus.tv>
2017-10-03 13:27:21 -0700

    retab

--- hw/sndsb/sbenvbls.c
+++ hw/sndsb/sbenvbls.c
@@ -11,9 +11,9 @@
 
 #include <hw/dos/dos.h>
 #include <hw/dos/dosbox.h>
-#include <hw/8237/8237.h>		/* 8237 DMA */
-#include <hw/8254/8254.h>		/* 8254 timer */
-#include <hw/8259/8259.h>		/* 8259 PIC interrupts */
+#include <hw/8237/8237.h>       /* 8237 DMA */
+#include <hw/8254/8254.h>       /* 8254 timer */
+#include <hw/8259/8259.h>       /* 8259 PIC interrupts */
 #include <hw/sndsb/sndsb.h>
 #include <hw/dos/doswin.h>
 #include <hw/dos/tgusmega.h>
@@ -24,50 +24,50 @@
 
 struct sndsb_ctx *sndsb_try_blaster_var() {
     signed char D=-1,H=-1,I=-1;
-	struct sndsb_ctx *e;
-	int A=-1,P=-1;
-	char *s;
+    struct sndsb_ctx *e;
+    int A=-1,P=-1;
+    char *s;
 
-	if (sndsb_card_blaster != NULL)
-		return sndsb_card_blaster;
+    if (sndsb_card_blaster != NULL)
+        return sndsb_card_blaster;
 
-	s = getenv("BLASTER");
-	if (s == NULL) return NULL;
+    s = getenv("BLASTER");
+    if (s == NULL) return NULL;
 
-	while (*s != 0) {
-		if (*s == ' ') {
-			s++;
-			continue;
-		}
+    while (*s != 0) {
+        if (*s == ' ') {
+            s++;
+            continue;
+        }
 
-		if (*s == 'A')
-			A = strtol(++s,&s,16);
-		else if (*s == 'P')
-			P = strtol(++s,&s,16);
-		else if (*s == 'I')
-			I = strtol(++s,&s,10);
-		else if (*s == 'D')
-			D = strtol(++s,&s,10);
-		else if (*s == 'H')
-			H = strtol(++s,&s,10);
-		else {
-			while (*s && *s != ' ') s++;
-		}
-	}
+        if (*s == 'A')
+            A = strtol(++s,&s,16);
+        else if (*s == 'P')
+            P = strtol(++s,&s,16);
+        else if (*s == 'I')
+            I = strtol(++s,&s,10);
+        else if (*s == 'D')
+            D = strtol(++s,&s,10);
+        else if (*s == 'H')
+            H = strtol(++s,&s,10);
+        else {
+            while (*s && *s != ' ') s++;
+        }
+    }
 
-	if (A < 0 || I < 0 || D < 0 || I > 15 || D > 7)
-		return NULL;
+    if (A < 0 || I < 0 || D < 0 || I > 15 || D > 7)
+        return NULL;
 
-	if (sndsb_by_base(A) != NULL)
-		return 0;
+    if (sndsb_by_base(A) != NULL)
+        return 0;
 
-	e = sndsb_alloc_card(); /* init and free functions zero this struct */
-	if (e == NULL) return NULL;
-	e->baseio = (uint16_t)A;
-	e->mpuio = (uint16_t)(P > 0 ? P : 0);
-	e->dma8 = (int8_t)D;
-	e->dma16 = (int8_t)H;
-	e->irq = (int8_t)I;
-	return (sndsb_card_blaster=e);
+    e = sndsb_alloc_card(); /* init and free functions zero this struct */
+    if (e == NULL) return NULL;
+    e->baseio = (uint16_t)A;
+    e->mpuio = (uint16_t)(P > 0 ? P : 0);
+    e->dma8 = (int8_t)D;
+    e->dma16 = (int8_t)H;
+    e->irq = (int8_t)I;
+    return (sndsb_card_blaster=e);
 }
 
