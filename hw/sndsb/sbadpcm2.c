51f5c753e1366d92a15658a1f821daed4048f8c2
Jonathan Campbell <jonathan@castus.tv>
2017-10-04 10:59:02 -0700

    more

--- hw/sndsb/sbadpcm2.c
+++ hw/sndsb/sbadpcm2.c
@@ -1,23 +1,24 @@
 
 #include <hw/sndsb/sndsb.h>
 
+/* NTS: This table is correct as tested against real Creative SB
+   hardware. DOSBox's version has a typo on the last row
+   that will make the 2-bit playback sound WORSE in it. */
+static const signed char scaleMap[24] = {
+    0,  1,  0,  -1,  1,  3,  -1,  -3,
+    2,  6, -2,  -6,  4, 12,  -4, -12,
+    8, 24, -8, -24, 16, 48, -16, -48
+};
+static const signed char adjustMap[12] = {
+    0, 1, -1, 1,
+    -1, 1, -1, 1,
+    -1, 1, -1, 0
+};
+
 /* NTS: This is the best documentation I could fine regarding the Sound Blaster ADPCM format.
  *      Tables and method taken from DOSBox 0.74 SB emulation. The information on multimedia.cx's
  *      Wiki is wrong. */
 unsigned char sndsb_encode_adpcm_2bit(const unsigned char samp) {
-    static const signed char scaleMap[24] = {
-        0,  1,  0,  -1,  1,  3,  -1,  -3,
-        2,  6, -2,  -6,  4, 12,  -4, -12,
-        8, 24, -8, -24, 16, 48, -16, -48
-/* NTS: This table is correct as tested against real Creative SB
-        hardware. DOSBox's version has a typo on the last row
-    that will make the 2-bit playback sound WORSE in it. */
-    };
-    static const signed char adjustMap[12] = {
-         0, 1, -1, 1,
-        -1, 1, -1, 1,
-        -1, 1, -1, 0
-    };
     signed int sdelta = (signed int)((signed char)(samp - sndsb_adpcm_pred));
     unsigned char sign = 0;
 
