08ccc13091220a728c2e194ff671a3b129602072
Jonathan Campbell <jonathan@castus.tv>
2017-10-03 23:59:46 -0700

    more

--- hw/sndsb/sbhcdma.c
+++ hw/sndsb/sbhcdma.c
@@ -1,28 +1,5 @@
 
-#include <stdio.h>
-#include <conio.h> /* this is where Open Watcom hides the outp() etc. functions */
-#include <stdlib.h>
-#include <string.h>
-#include <assert.h>
-#include <unistd.h>
-#include <malloc.h>
-#include <direct.h>
-#include <ctype.h>
-#include <fcntl.h>
-#include <dos.h>
-
-#include <hw/vga/vga.h>
-#include <hw/dos/dos.h>
-#include <hw/8237/8237.h>		/* 8237 DMA */
-#include <hw/8254/8254.h>		/* 8254 timer */
-#include <hw/8259/8259.h>		/* 8259 PIC interrupts */
 #include <hw/sndsb/sndsb.h>
-#include <hw/vga/vgagui.h>
-#include <hw/vga/vgatty.h>
-#include <hw/dos/doswin.h>
-#include <hw/dos/tgusmega.h>
-#include <hw/dos/tgussbos.h>
-#include <hw/dos/tgusumid.h>
 
 int sndsb_halt_dma(struct sndsb_ctx *cx) {
 	if (!cx->dsp_playing) return 1;
