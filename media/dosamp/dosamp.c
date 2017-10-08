336970468848bf2ef5ac22bccb76dabd39930dda
Jonathan Campbell <jonathan@castus.tv>
2017-10-07 19:05:12 -0700

    timer source abstraction

--- media/dosamp/dosamp.c
+++ media/dosamp/dosamp.c
@@ -59,6 +59,90 @@ typedef struct windows_WAVEFORMATPCM {
 # define dosamp_FAR far
 #endif
 
+enum {
+    dosamp_time_source_id_null = 0,                 /* null */
+    dosamp_time_source_id_8254 = 1                  /* MS-DOS / Windows 3.x 8254 PIT timer */
+};
+
+struct dosamp_time_source;
+typedef struct dosamp_time_source dosamp_FAR * dosamp_time_source_t;
+
+/* Time source.
+ * NTS: For maximum portability, the caller should not expect accurate timekeeping UNLESS polling
+ *      the time source on a semi-regular basis as indicated in the structure. The 8254 PIT time
+ *      source especially needs this since the code does NOT hook the IRQ 0 interrupt.
+ *
+ *      Unlike file sources, these are statically declared and are NOT malloc'd or free'd.
+ *      But you must open/close them regardless. */
+struct dosamp_time_source {
+    unsigned int                        obj_id;     /* what exactly this is */
+    volatile unsigned int               refcount;   /* reference count. will NOT auto-free when zero. */
+    unsigned int                        open_flags; /* zero if not open, nonzero if open */
+    unsigned long                       clock_rate; /* tick rate of the time source */
+    unsigned long long                  counter;    /* counter */
+    unsigned long                       poll_requirement; /* you must poll the source this often (clock ticks) to keep time (Intel 8254 source) */
+    int                                 (dosamp_FAR *close)(dosamp_time_source_t inst);
+    int                                 (dosamp_FAR *open)(dosamp_time_source_t inst);
+    unsigned long long                  (dosamp_FAR *poll)(dosamp_time_source_t inst);
+};
+
+/* previous count, and period (in case host changes timer) */
+static uint16_t ts_8254_pcnt = 0;
+static uint32_t ts_8254_pper = 0;
+
+int dosamp_FAR dosamp_time_source_8254_close(dosamp_time_source_t inst) {
+    inst->open_flags = 0;
+    inst->counter = 0;
+    return 0;
+}
+
+int dosamp_FAR dosamp_time_source_8254_open(dosamp_time_source_t inst) {
+    inst->open_flags = (unsigned int)(-1);
+    inst->clock_rate = T8254_REF_CLOCK_HZ;
+    ts_8254_pcnt = read_8254(T8254_TIMER_INTERRUPT_TICK);
+    ts_8254_pper = t8254_counter[T8254_TIMER_INTERRUPT_TICK];
+    return 0;
+}
+
+unsigned long long dosamp_FAR dosamp_time_source_8254_poll(dosamp_time_source_t inst) {
+    if (inst->open_flags == 0) return 0UL;
+
+    if (ts_8254_pper == t8254_counter[T8254_TIMER_INTERRUPT_TICK]) {
+        uint16_t ncnt = read_8254(T8254_TIMER_INTERRUPT_TICK);
+        uint16_t delta;
+
+        /* NTS: remember the timer chip counts DOWN to zero.
+         *      so normally, ncnt < ts_8254_pcnt unless 16-bit rollover happened. */
+        if (ncnt <= ts_8254_pcnt)
+            delta = ts_8254_pcnt - ncnt;
+        else
+            delta = ts_8254_pcnt + (uint16_t)ts_8254_pper - ncnt;
+
+        /* count */
+        inst->counter += (unsigned long long)delta;
+
+        /* store the prev counter */
+        ts_8254_pcnt = ncnt;
+    }
+    else {
+        ts_8254_pcnt = read_8254(T8254_TIMER_INTERRUPT_TICK);
+        ts_8254_pper = t8254_counter[T8254_TIMER_INTERRUPT_TICK];
+    }
+
+    return inst->counter;
+}
+
+struct dosamp_time_source               dosamp_time_source_8254 = {
+    .obj_id =                           dosamp_time_source_id_8254,
+    .open_flags =                       0,
+    .clock_rate =                       0,
+    .counter =                          0,
+    .poll_requirement =                 0xE000UL, /* counter is 16-bit */
+    .close =                            dosamp_time_source_8254_close,
+    .open =                             dosamp_time_source_8254_open,
+    .poll =                             dosamp_time_source_8254_poll
+};
+
 #if TARGET_MSDOS == 32
 static inline unsigned char *dosamp_ptr_add_normalize(unsigned char * const p,const unsigned int o) {
     return p + o;
@@ -93,6 +177,11 @@ static struct dma_8237_allocation*      sb_dma = NULL;
 
 /* Sound Blaster sound card */
 static struct sndsb_ctx*	            sb_card = NULL;
+
+/* chosen time source.
+ * NTS: Don't forget that by design, some time sources (8254 for example)
+ *      require you to poll often enough to get a correct count of time. */
+static dosamp_time_source_t             time_source = NULL;
 
 #if TARGET_MSDOS == 32
 static const unsigned int               dosamp_file_io_maxb = (unsigned int)INT_MAX - 1U;
@@ -834,6 +923,25 @@ static int parse_argv(int argc,char **argv) {
     return 1;
 }
 
+void display_idle_timesource(void) {
+    printf("\x0D");
+
+    if (time_source == &dosamp_time_source_8254)
+        printf("8254-pit ");
+    else
+        printf("? ");
+
+    time_source->poll(time_source);
+
+    printf("counter=%-10llu (%lu) rate=%lu req=%lu  ",
+        (unsigned long long)time_source->counter,
+        (unsigned long)(time_source->counter / (unsigned long long)time_source->clock_rate),
+        (unsigned long)time_source->clock_rate,
+        (unsigned long)time_source->poll_requirement);
+
+    fflush(stdout);
+}
+
 void display_idle_time(void) {
     unsigned long w,f;
     unsigned char hour,min,sec,centisec;
@@ -916,6 +1024,7 @@ void display_idle(unsigned char disp) {
     switch (disp) {
         case 1:     display_idle_time(); break;
         case 2:     display_idle_buffer(); break;
+        case 3:     display_idle_timesource(); break;
     }
 }
 
@@ -943,6 +1052,21 @@ int main(int argc,char **argv) {
 		printf("Cannot init ISA PnP\n");
 		return 1;
 	}
+
+    /* for now, always use 8254 PIT as time source */
+    time_source = &dosamp_time_source_8254;
+
+    /* NTS: When using the PIT source we NEED to do this, or else DOSBox-X (and possibly
+     *      many other motherboards) will leave the PIT in a mode that produces the correct
+     *      IRQ 0 rate but counts down twice as fast. We need the PIT to count down by 1,
+     *      not by 2, in order to keep time. */
+    write_8254_system_timer(0); /* 18.2 tick/sec on our terms (proper PIT mode) */
+
+    /* open the time source */
+    if (time_source->open(time_source) < 0) {
+        printf("Cannot open time source\n");
+        return 1;
+    }
 
     /* we want to know if certain emulation TSRs exist */
     gravis_mega_em_detect(&megaem_info);
@@ -1158,6 +1282,10 @@ int main(int argc,char **argv) {
 
 	sndsb_free_card(sb_card);
 	free_sndsb(); /* will also de-ref/unhook the NMI reflection */
+
+    /* open the time source */
+    time_source->close(time_source);
+
 	return 0;
 }
 
