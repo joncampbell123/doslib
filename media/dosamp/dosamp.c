b7b4e53fed43ce24ae613d65cd6eb7f772176bc5
Jonathan Campbell <jonathan@castus.tv>
2017-10-15 09:33:52 -0700

    16-bit MS-DOS optimized stereo to mono conversion (to overcome far pointer overhead)

--- media/dosamp/dosamp.c
+++ media/dosamp/dosamp.c
@@ -952,13 +952,40 @@ void convert_rdbuf_stereo2mono_ip_s16(uint32_t samples) {
     /* in-place stereo to mono conversion (up to convert_rdbuf_len)
      * from file_codec channels (2) to play_codec channels (1) */
     int16_t dosamp_FAR * buf = (int16_t dosamp_FAR *)convert_rdbuf;
-    int16_t dosamp_FAR * sp = buf;
-    uint32_t i = samples;
 
-    while (i-- != 0UL) {
-        *buf++ = (int16_t)(((long)sp[0] + (long)sp[1] + 1) >> 1);
-        sp += 2;
+#if defined(__WATCOMC__) && defined(__I86__) && TARGET_MSDOS == 16
+    /* NTS: SAR = Shift Arithmetic Right (signed shift, fill in upper bits with sign bit) */
+    __asm {
+        push    ds
+        push    es
+        cld
+        lds     si,buf
+        mov     di,si
+        push    ds
+        pop     es
+        mov     cx,word ptr samples
+l1:     lodsw               ; BX = one sample >> 1
+        mov     bx,ax
+        sar     bx,1
+        lodsw               ; AX = another sample >> 1
+        sar     ax,1
+        add     ax,bx
+        stosw               ; store AX + BX
+        loop    l1
+        pop     es
+        pop     ds
     }
+#else
+    {
+        int16_t dosamp_FAR * sp = buf;
+        uint32_t i = samples;
+
+        while (i-- != 0UL) {
+            *buf++ = (int16_t)(((long)sp[0] + (long)sp[1] + 1) >> 1);
+            sp += 2;
+        }
+    }
+#endif
 
     convert_rdbuf_len = samples * 2UL;
     assert(convert_rdbuf_len <= convert_rdbuf_sz);
