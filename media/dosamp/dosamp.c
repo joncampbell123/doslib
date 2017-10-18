cda18f32508dec7716ba6a38809f434e4dd3e2e1
Jonathan Campbell <jonathan@castus.tv>
2017-10-18 16:29:26 -0700

    fix inline asm resample interpolation to make result act like conditional NEGate rather than allow the off-by-1 error. You can't hear the difference with 16-bit PCM, but 8-bit PCM has low enough resolution you can hear the difference as sort of a slight FM static atop the audio. This commit changes the behavior, which improves resampling quality on 8-bit PCM audio.

--- media/dosamp/dosamp.c
+++ media/dosamp/dosamp.c
@@ -599,6 +599,7 @@ static unsigned int resample_interpolate_asm86(unsigned int c,unsigned int p,uns
  * EAX -= EBX
  * ESI = 0 if EAX >= 0, 0xFFFFFFFF if EAX < 0
  * EAX ^= ESI  (convert to absolute... well almost... not quite NEG but close enough... off by 1 at most)
+ * EAX -= ESI  (this corrects the inversion to act like NEG)
  * EDX:EAX = EAX * ECX
  * EDX ^= ESI
  * EDX += EBX
@@ -607,12 +608,13 @@ static unsigned int resample_interpolate_asm86(unsigned int c,unsigned int p,uns
  *      "SUB dest, src" will set CF if dest < src at the time of execution (because it carries and becomes negative)
  *      So we use SBB to subtract ESI by itself to that ESI either becomes zero (CF=0) or -1 (CF=1). On the x86,
  *      -1 is 0xFFFFFFFF (all bits set). We XOR the value by ESI as a cheap way to get the absolute value of the
- *      result we just computed. Except... it's not quite the absolute value (for negative results, the value is
- *      off by 1) but to the human ear the difference is inaudible anyway. */
+ *      result we just computed. But that's not QUITE the absolute value, so we subtract EAX by ESI so the result
+ *      is the same as the NEG instruction. In this way, we accomplish a conditional negate. */
 #pragma aux resample_interpolate_asm86 = \
     "sub    eax,ebx" \
     "sbb    esi,esi" \
     "xor    eax,esi" \
+    "sub    eax,esi" \
     "mul    ecx" \
     "xor    edx,esi" \
     "add    edx,ebx" \
@@ -637,6 +639,7 @@ static unsigned int resample_interpolate_asm86(unsigned int c,unsigned int p,uns
  * AX -= BX
  * SI = 0 if AX >= 0, 0xFFFF if AX < 0
  * AX ^= SI  (convert to absolute... well almost... not quite NEG but close enough... off by 1 at most)
+ * AX -= SI  (correct the negative value so the result is like NEG)
  * DX:AX = AX * CX
  * DX ^= SI
  * DX += BX */
@@ -644,6 +647,7 @@ static unsigned int resample_interpolate_asm86(unsigned int c,unsigned int p,uns
     "sub    ax,bx" \
     "sbb    si,si" \
     "xor    ax,si" \
+    "sub    ax,si" \
     "mul    cx" \
     "xor    dx,si" \
     "add    dx,bx" \
