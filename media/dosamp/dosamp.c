e3d187e64764d37d446414128cfaed9a8c5a504f
Jonathan Campbell <jonathan@castus.tv>
2017-10-18 13:28:57 -0700

    break out 8254 time source

--- media/dosamp/dosamp.c
+++ media/dosamp/dosamp.c
@@ -38,6 +38,9 @@
 #include "dosamp.h"
 #include "timesrc.h"
 
+/* 8254 time source */
+extern struct dosamp_time_source                dosamp_time_source_8254;
+
 static unsigned long        prefer_rate = 0;
 static unsigned char        prefer_channels = 0;
 static unsigned char        prefer_bits = 0;
@@ -49,63 +52,6 @@ static char                 stuck_test = 0;
 #ifdef __TINY__
 # error Open Watcom C tiny memory model not supported
 #endif
-
-/* previous count, and period (in case host changes timer) */
-static uint16_t ts_8254_pcnt = 0;
-static uint32_t ts_8254_pper = 0;
-
-int dosamp_FAR dosamp_time_source_8254_close(dosamp_time_source_t inst) {
-    inst->open_flags = 0;
-    inst->counter = 0;
-    return 0;
-}
-
-int dosamp_FAR dosamp_time_source_8254_open(dosamp_time_source_t inst) {
-    inst->open_flags = (unsigned int)(-1);
-    inst->clock_rate = T8254_REF_CLOCK_HZ;
-    ts_8254_pcnt = read_8254(T8254_TIMER_INTERRUPT_TICK);
-    ts_8254_pper = t8254_counter[T8254_TIMER_INTERRUPT_TICK];
-    return 0;
-}
-
-unsigned long long dosamp_FAR dosamp_time_source_8254_poll(dosamp_time_source_t inst) {
-    if (inst->open_flags == 0) return 0UL;
-
-    if (ts_8254_pper == t8254_counter[T8254_TIMER_INTERRUPT_TICK]) {
-        uint16_t ncnt = read_8254(T8254_TIMER_INTERRUPT_TICK);
-        uint16_t delta;
-
-        /* NTS: remember the timer chip counts DOWN to zero.
-         *      so normally, ncnt < ts_8254_pcnt unless 16-bit rollover happened. */
-        if (ncnt <= ts_8254_pcnt)
-            delta = ts_8254_pcnt - ncnt;
-        else
-            delta = ts_8254_pcnt + (uint16_t)ts_8254_pper - ncnt;
-
-        /* count */
-        inst->counter += (unsigned long long)delta;
-
-        /* store the prev counter */
-        ts_8254_pcnt = ncnt;
-    }
-    else {
-        ts_8254_pcnt = read_8254(T8254_TIMER_INTERRUPT_TICK);
-        ts_8254_pper = t8254_counter[T8254_TIMER_INTERRUPT_TICK];
-    }
-
-    return inst->counter;
-}
-
-struct dosamp_time_source               dosamp_time_source_8254 = {
-    .obj_id =                           dosamp_time_source_id_8254,
-    .open_flags =                       0,
-    .clock_rate =                       0,
-    .counter =                          0,
-    .poll_requirement =                 0xE000UL, /* counter is 16-bit */
-    .close =                            dosamp_time_source_8254_close,
-    .open =                             dosamp_time_source_8254_open,
-    .poll =                             dosamp_time_source_8254_poll
-};
 
 static uint64_t ts_rdtsc_prev;
 
