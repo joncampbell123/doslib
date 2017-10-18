c460da7d840ca7cc4f65aaa72aa06255a55c9f78
Jonathan Campbell <jonathan@castus.tv>
2017-10-18 15:52:21 -0700

    use params, please

--- media/dosamp/dosamp.c
+++ media/dosamp/dosamp.c
@@ -486,33 +486,36 @@ void card_poll(void) {
     update_wav_play_delay();
 }
 
-uint32_t convert_rdbuf_8_to_16_ip(const uint32_t total_samples,const void dosamp_FAR *buf,const uint32_t buf_max) {
+uint32_t convert_rdbuf_8_to_16_ip(const uint32_t total_samples,const void dosamp_FAR *proc_buf,const uint32_t buf_max) {
     uint32_t ret = total_samples * (uint32_t)2; /* return: how many bytes of converted audio */
 
     /* buffer range check! */
     assert(ret <= buf_max);
 
-    /* in-place 16-bit to 8-bit conversion (up to convert_rdbuf_len) from file_codec (8) to play_codec (16).
+    /* in-place 16-bit to 8-bit conversion (up to buf_max) from file_codec (8) to play_codec (16).
      * due to data expansion, this converts backwards in place. */
 
+    /* NTS: Caller is expected to normalize the proc_buf pointer in 16-bit MS-DOS builds so that
+     *      FP_OFF(proc_buf) + buf_max does not overflow while processing */
+
 #if defined(__WATCOMC__) && defined(__I86__) && TARGET_MSDOS == 16
-    /* DS:SI = convert_rdbuf + total_samples - 1
-     * ES:DI = convert_rdbuf + total_samples + total_samples - 2
+    /* DS:SI = proc_buf + total_samples - 1
+     * ES:DI = proc_buf + total_samples + total_samples - 2
      * CX = samples
      */
     __asm {
         push    ds
         push    es
         std                         ; scan backwards
-        lds     si,convert_rdbuf
+        lds     si,proc_buf
         mov     di,si
         push    ds
         pop     es
         mov     cx,word ptr total_samples
         dec     cx                  ; CX = total samples - 1
-        add     si,cx               ; therefore SI = convert_rdbuf + total_samples - 1
-        add     di,cx               ; therefore DI = convert_rdbuf + (total_samples - 1) + (total_samples - 1)
-        add     di,cx               ; ...or....... = convert_rdbuf + total_samples + total_samples - 2
+        add     si,cx               ; therefore SI = proc_buf + total_samples - 1
+        add     di,cx               ; therefore DI = proc_buf + (total_samples - 1) + (total_samples - 1)
+        add     di,cx               ; ...or....... = proc_buf + total_samples + total_samples - 2
         inc     cx                  ; restore CX
 l1:     lodsb
         mov     ah,al
@@ -526,8 +529,8 @@ l1:     lodsb
     }
 #else
     {
-        int16_t dosamp_FAR * buf = (int16_t dosamp_FAR *)convert_rdbuf + total_samples - 1;
-        uint8_t dosamp_FAR * sp = (uint8_t dosamp_FAR *)convert_rdbuf + total_samples - 1;
+        int16_t dosamp_FAR * buf = (int16_t dosamp_FAR *)proc_buf + total_samples - 1;
+        uint8_t dosamp_FAR * sp = (uint8_t dosamp_FAR *)proc_buf + total_samples - 1;
         uint32_t i = total_samples;
 
         while (i-- != 0UL)
@@ -539,14 +542,16 @@ l1:     lodsb
 }
 
 uint32_t convert_rdbuf_16_to_8_ip(const uint32_t total_samples,const void dosamp_FAR *proc_buf,const uint32_t buf_max) {
-    uint32_t ret = total_samples;
-    /* in-place 16-bit to 8-bit conversion (up to convert_rdbuf_len)
+    /* in-place 16-bit to 8-bit conversion (up to buf_max)
      * from file_codec (16) to play_codec (8). this must happen AFTER
      * channel conversion, therefore use play_codec.number_of_channels */
-    uint8_t dosamp_FAR * buf = (uint8_t dosamp_FAR *)convert_rdbuf;
+    uint8_t dosamp_FAR * buf = (uint8_t dosamp_FAR *)proc_buf;
 
     /* buffer range check (source is 16-bit) */
-    assert((ret*(uint32_t)2) <= buf_max);
+    assert((total_samples*(uint32_t)2) <= buf_max);
+
+    /* NTS: Caller is expected to normalize the proc_buf pointer in 16-bit MS-DOS builds so that
+     *      FP_OFF(proc_buf) + buf_max does not overflow while processing */
 
 #if defined(__WATCOMC__) && defined(__I86__) && TARGET_MSDOS == 16
     __asm {
@@ -568,7 +573,7 @@ l1:     lodsw               ; AX = 16-bit sample
     }
 #else
     {
-        int16_t dosamp_FAR * sp = (int16_t dosamp_FAR *)convert_rdbuf;
+        int16_t dosamp_FAR * sp = (int16_t dosamp_FAR *)proc_buf;
         uint32_t i = total_samples;
 
         while (i-- != 0UL)
@@ -576,7 +581,7 @@ l1:     lodsw               ; AX = 16-bit sample
     }
 #endif
 
-    return ret;
+    return total_samples;
 }
 
 void convert_rdbuf_stereo2mono_ip_u8(uint32_t samples) {
