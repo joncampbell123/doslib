4d784adf1230fca73148e76f8d1e0495de7ab6a6
Jonathan Campbell <jonathan@castus.tv>
2017-10-25 08:41:03 -0700

    more

--- hw/sndsb/test-results/ts_pd/sb2gnuplot.pl
+++ hw/sndsb/test-results/ts_pd/sb2gnuplot.pl
@@ -9,179 +9,59 @@ $filenn =~ s/\.txt$//i;
 
 open(I,"<",$file) || die;
 
-my @sb1x;
-my @sb2x;
-my @ess;
-
 my $line;
-my $ref = undef;
+my $test = undef;
+my $subtest = undef;
 while ($line = <I>) {
     chomp $line;
+    $line =~ s/[\x0D\x0A]//g;
 
-    if ($line =~ m/^SB 1\.x single cycle/i) {
-        $ref = "sb1";
+    if ($line =~ m/^SB 1\.x DMA single cycle DSP/i) {
+        $test = $line;
+        $test =~ s/\.$//g;
+        $subtest = undef;
     }
-    elsif ($line =~ m/^SB 2\.x single cycle/i) {
-        $ref = "sb2";
+    elsif ($line =~ s/^ - Test at +//i) {
+        $subtest = $line;
+
+        $name = "$filenn - $test, $subtest";
+
+        print "Processing $name\n";
+
+        die if $name =~ m/[^0-9a-z \.\-\,]/i;
+
+        $csv = "gnuplot/$name.csv";
+        $gnuplot = "gnuplot/$name.gnuplot";
+        $png = "gnuplot/$name.png";
+
+        open(O,">",$csv) || die;
+        print O "# pos, time\n";
+
+        while ($line = <I>) {
+            chomp $line;
+            $line =~ s/[\x0D\x0A]//g;
+
+            last if $line eq "";
+
+            if ($line =~ s/^ +\>\> +POS +//i) {
+                my @a = split(/ +/,$line);
+
+                $pos = $a[0] + 0;
+                $time = $a[2] + 0.0;
+
+                print O "$pos, $time\n";
+            }
+        }
+
+        close(O);
     }
-    elsif ($line =~ m/^ESS688 single cycle/i) {
-        $ref = "ess";
-    }
-    #  - TC 0x06: expecting 4000Hz, 4000b/0.985s @ 4060.103Hz
-    elsif ($line =~ s/^ *- TC *//) {
-        my @a = split(/ +/,$line);
-
-        $idx = shift(@a);
-        next unless $idx =~ m/^0x[0-9a-f]+: */i;
-
-        $idx = hex($idx);
-        next unless $idx >= 0 && $idx <= 0xFF;
-
-        $x = shift(@a);
-        next unless $x =~ m/expecting/i;
-
-        $expect = shift(@a);
-        next unless $expect =~ m/\d+Hz,/;
-        $expect = int($expect);
-
-        shift(@a); # throw away 4000b/0.985s
-
-        $x = shift(@a);
-        next unless $x eq "@";
-
-        $rate = shift(@a);
-        next unless (defined($rate) && $rate ne "");
-        $rate = $rate + 0.0;
-
-        if ($ref eq "sb1") {
-            $sb1[$idx] = $rate;
-        }
-        elsif ($ref eq "sb2") {
-            $sb2[$idx] = $rate;
-        }
-        elsif ($ref eq "ess") {
-            $ess[$idx] = $rate;
-        }
+    elsif ($line eq "") {
     }
     else {
-        $ref = undef;
+        $test = undef;
+        $subtest = undef;
     }
 }
 
 close(I);
-
-
-
-
-
-$png1 = "gnuplot/$file.sb1.png";
-$png2 = "gnuplot/$file.sb2.png";
-$csv = $ofile = "gnuplot/$file.sb.csv";
-open(O,">",$ofile) || die;
-
-print O "# tc, sb1";
-print O ", sb2" if @sb2 > 0;
-print O "\n";
-
-for ($i=0;$i < 256;$i++) {
-    next unless (defined($sb1[$i]) || defined($sb2[$i]));
-
-    # tc
-    print O "$i";
-
-    # sb1
-    print O ", ".$sb1[$i];
-
-    # sb2
-    print O ", ".$sb2[$i] if @sb2 > 0;
-
-    print O "\n";
-}
-
-close(O);
-
-$ofile = "gnuplot/$file.sb.gnuplot";
-open(O,">",$ofile) || die;
-
-print O "reset\n";
-
-print O "set term png size 1920,1080\n";
-print O "set output '$png1'\n";
-
-print O "set grid\n";
-print O "set autoscale\n";
-print O "set xrange [0:255]\n";
-print O "set title 'Sound Blaster Time Constant and Playback rate ($filenn)'\n";
-print O "set xlabel 'Time constant byte'\n";
-print O "set ylabel 'Sample rate (Hz)'\n";
-
-print O "plot '$csv' using 1:2 with lines title 'SB 1.x non-highspeed TC'\n";
-
-if (@sb2 > 0) {
-
-print O "reset\n";
-
-print O "set term png size 1920,1080\n";
-print O "set output '$png2'\n";
-
-print O "set grid\n";
-print O "set autoscale\n";
-print O "set xrange [0:255]\n";
-print O "set title 'Sound Blaster Time Constant and Playback rate ($filenn)'\n";
-print O "set xlabel 'Time constant byte'\n";
-print O "set ylabel 'Sample rate (Hz)'\n";
-
-print O "plot '$csv' using 1:3 with lines title 'SB 2.x highspeed TC'\n";
-
-}
-
-close(O);
-
-system("gnuplot '$ofile'");
-
-
-
-if (@ess > 0) {
-    $png1 = "gnuplot/$file.ess.png";
-    $csv = $ofile = "gnuplot/$file.ess.csv";
-    open(O,">",$ofile) || die;
-
-    print O "# tc, ess";
-    print O "\n";
-
-    for ($i=0;$i < 256;$i++) {
-        next unless defined($ess[$i]);
-
-        # tc
-        print O "$i";
-
-        # ess
-        print O ", ".$ess[$i];
-
-        print O "\n";
-    }
-
-    close(O);
-
-    $ofile = "gnuplot/$file.ess.gnuplot";
-    open(O,">",$ofile) || die;
-
-    print O "reset\n";
-
-    print O "set term png size 1920,1080\n";
-    print O "set output '$png1'\n";
-
-    print O "set grid\n";
-    print O "set autoscale\n";
-    print O "set xrange [0:255]\n";
-    print O "set title 'ESS688 sample rate divider and Playback rate ($filenn)'\n";
-    print O "set xlabel 'Sample rate divider byte'\n";
-    print O "set ylabel 'Sample rate (Hz)'\n";
-
-    print O "plot '$csv' using 1:2 with lines title 'Sample rate divider byte'\n";
-
-    close(O);
-
-    system("gnuplot '$ofile'");
-}
 
