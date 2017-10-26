bb4cd3b1650dead74def586df5bf0f4f835dd906
Jonathan Campbell <jonathan@castus.tv>
2017-10-26 05:42:53 -0700

    remove IRQ from graphs

--- hw/sndsb/test-results/ts_pd/sb2gnuplot.pl
+++ hw/sndsb/test-results/ts_pd/sb2gnuplot.pl
@@ -39,7 +39,7 @@ while ($line = <I>) {
         $pngc = "gnuplot/$name.start.png";
 
         open(O,">",$csv) || die;
-        print O "# pos, time, irq\n";
+        print O "# pos, time\n";
 
         $min_pos = 0;
         $max_pos = 1;
@@ -47,11 +47,7 @@ while ($line = <I>) {
         $min_time = 0;
         $max_time = 0.01;
 
-        $min_irq = 0;
-        $max_irq = 1;
-
         $p_pos = undef;
-        $p_irq = undef;
         $p_time = undef;
 
         $f_pos = 1;
@@ -72,14 +68,10 @@ while ($line = <I>) {
                 $p_time = $time;
                 $time = $a[2] + 0.0;
 
-                $p_irq = $irq;
-                $irq = $a[4] + 0;
-
-                if (defined($p_time) && $time > ($p_time + 0.1) && $p_irq == $irq && $p_pos == $pos) {
+                if (defined($p_time) && $time > ($p_time + 0.1) && $p_pos == $pos) {
                 }
                 else {
                     $max_pos  = $pos  if $max_pos  < $pos;
-                    $max_irq  = $irq  if $max_irq  < $irq;
                     $max_time = $time if $max_time < $time;
                 }
 
@@ -90,17 +82,15 @@ while ($line = <I>) {
                     }
                 }
 
-                print O "$pos, $time, $irq\n";
+                print O "$pos, $time\n";
             }
         }
 
         # keep original
-        $o_max_irq  = $max_irq;
         $o_max_pos  = $max_pos;
         $o_max_time = $max_time;
 
         # expand out a bit, to show the graph in full
-        $max_irq  += $max_irq  / 4;
         $max_pos  += $max_pos  / 4;
         $max_time += $max_time / 100;
 
@@ -114,29 +104,14 @@ while ($line = <I>) {
         print O "set term png size 1920,1080\n";
         print O "set output '$png'\n";
 
-        print O "set multiplot\n";
-
         print O "set grid\n";
 
-        print O "set size 1.0,0.85\n";
-        print O "set origin 0.0,0.0\n";
         print O "set title '$name vs DMA'\n";
         print O "set xrange [$min_time:$max_time]\n";
         print O "set yrange [$min_pos:$max_pos]\n";
         print O "set xlabel 'Time (s)'\n";
         print O "set ylabel 'DMA transfer count (bytes)'\n";
         print O "plot '$csv' using 2:1 with steps title 'DMA transfer count over time'\n";
-
-        print O "set size 1.0,0.15\n";
-        print O "set origin 0.0,0.85\n";
-        print O "set title '$name vs IRQ'\n";
-        print O "set xrange [$min_time:$max_time]\n";
-        print O "set yrange [$min_irq:$max_irq]\n";
-        print O "set xlabel 'Time (s)'\n";
-        print O "set ylabel 'IRQ count'\n";
-        print O "plot '$csv' using 2:3 with steps title 'IRQ count over time'\n";
-
-        print O "unset multiplot\n";
 
         # the initial burst when loading the FIFO, and ending part, is too subtle for the full graph
         print O "reset\n";
