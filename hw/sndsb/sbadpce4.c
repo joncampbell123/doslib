1d2eb39f1104e1f3b9b139fa8b654a7cce907159
Jonathan Campbell <jonathan@castus.tv>
2017-10-28 18:46:32 -0700

    tweak ADPCM 4-bit encoding

--- hw/sndsb/sbadpce4.c
+++ hw/sndsb/sbadpce4.c
@@ -8,7 +8,7 @@ unsigned char sndsb_encode_adpcm_4bit(const unsigned char samp) {
     signed int sdelta = (signed int)((signed char)(samp - sndsb_adpcm_pred));
     unsigned char sign = 0;
 
-    sdelta = (sdelta * 2) + (sndsb_adpcm_step < 3 ? sndsb_adpcm_error : 0);
+    sdelta = (sdelta * 2) + (sndsb_adpcm_step < 2 ? sndsb_adpcm_error : 0);
     sndsb_adpcm_error = sdelta & ((1 << (sndsb_adpcm_step + 1)) - 1);
     sdelta >>= sndsb_adpcm_step+1;
     if (sdelta < 0) {
