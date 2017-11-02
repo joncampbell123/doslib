3f156c89fe5249c452d20d3fe7a56b828b8ad178
Jonathan Campbell <jonathan@castus.tv>
2017-11-02 10:30:43 -0700

    the 16-bit real-mode pointer normalization tricks don't work in 16-bit protected mode.

--- media/dosamp/dosptrnm.h
+++ media/dosamp/dosptrnm.h
@@ -7,6 +7,18 @@ static inline unsigned char *dosamp_ptr_add_normalize(unsigned char * const p,co
 static inline const unsigned char *dosamp_cptr_add_normalize(const unsigned char * const p,const unsigned int o) {
     return p + o;
 }
+#elif defined(TARGET_WINDOWS) && TARGET_MSDOS == 16
+/* pointer normalization must be constrained because of 16-bit protected mode Windows.
+ * There is a scheme in 16-bit protected mode where larger than 64KB blocks are possible
+ * and each 64KB segment is one selector away from another (FP_SEG(x) + 8), but we don't
+ * do that. */
+static inline unsigned char far *dosamp_ptr_add_normalize(unsigned char far * const p,const unsigned int o) {
+    return p + o;
+}
+
+static inline const unsigned char far *dosamp_cptr_add_normalize(const unsigned char far * const p,const unsigned int o) {
+    return p + o;
+}
 #else
 static inline uint32_t dosamp_ptr_to_linear(const unsigned char far * const p) {
     return ((unsigned long)FP_SEG(p) << 4UL) + (unsigned long)FP_OFF(p);
