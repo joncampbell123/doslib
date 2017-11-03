f83d6a444ef682c5333123a972ecccccec5adf28
Jonathan Campbell <jonathan@castus.tv>
2017-11-02 17:45:29 -0700

    Revert "begin Win386 target of DOSAMP."
    This reverts commit 173aae531e416e01f8c9f3e9e1f252861540c914.
    
    I'm having difficulty getting Win386 to LoadLibrary things without
    crashing. The direct thunks don't seem to work either. Maybe later.

--- media/dosamp/dosamp.h
+++ media/dosamp/dosamp.h
@@ -4,7 +4,7 @@
 /* making the API use FAR CALL function pointers will allow future development
  * where file access and media playback code can exist in external DLLs, whether
  * 16-bit or 32-bit code */
-#if TARGET_MSDOS == 32
+#if TARGET_MSDOS == 32 && !defined(WIN386)
 # define dosamp_FAR
 #elif defined(LINUX)
 # define dosamp_FAR
