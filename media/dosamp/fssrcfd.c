c9862e648d3da3ebea3982192a1b4a701c7e51c7
Jonathan Campbell <jonathan@castus.tv>
2017-11-08 23:33:30 -0800

    cleanup

--- media/dosamp/fssrcfd.c
+++ media/dosamp/fssrcfd.c
@@ -5,8 +5,6 @@
 #include <endian.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#else
-#include <hw/cpu/endian.h>
 #endif
 #ifndef LINUX
 #include <conio.h> /* this is where Open Watcom hides the outp() etc. functions */
@@ -21,30 +19,12 @@
 #include <errno.h>
 #include <ctype.h>
 #include <fcntl.h>
-#ifndef LINUX
-#include <dos.h>
-#endif
 
 #ifndef LINUX
 #include <hw/dos/dos.h>
-#include <hw/cpu/cpu.h>
-#include <hw/8237/8237.h>       /* 8237 DMA */
-#include <hw/8254/8254.h>       /* 8254 timer */
-#include <hw/8259/8259.h>       /* 8259 PIC interrupts */
-#include <hw/sndsb/sndsb.h>
-#include <hw/cpu/cpurdtsc.h>
-#include <hw/dos/doswin.h>
-#include <hw/dos/tgusmega.h>
-#include <hw/dos/tgussbos.h>
-#include <hw/dos/tgusumid.h>
-#include <hw/isapnp/isapnp.h>
-#include <hw/sndsb/sndsbpnp.h>
 #endif
 
-#include "wavefmt.h"
 #include "dosamp.h"
-#include "timesrc.h"
-#include "dosptrnm.h"
 #include "filesrc.h"
 
 #ifndef O_BINARY
