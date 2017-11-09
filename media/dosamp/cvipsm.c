389fce50c30b45943b4def86cf95944f59cb7818
Jonathan Campbell <jonathan@castus.tv>
2017-11-08 23:16:04 -0800

    cleanup

--- media/dosamp/cvipsm.c
+++ media/dosamp/cvipsm.c
@@ -1,50 +1,13 @@
 
+#if defined(TARGET_WINDOWS)
+# include <windows.h>
+#endif
+
 #include <stdio.h>
 #include <stdint.h>
-#ifdef LINUX
-#include <endian.h>
-#else
-#include <hw/cpu/endian.h>
-#endif
-#ifndef LINUX
-#include <conio.h> /* this is where Open Watcom hides the outp() etc. functions */
-#include <direct.h>
-#endif
-#include <stdlib.h>
-#include <string.h>
 #include <assert.h>
-#include <unistd.h>
-#include <malloc.h>
-#include <limits.h>
-#include <errno.h>
-#include <ctype.h>
-#include <fcntl.h>
-#ifndef LINUX
-#include <dos.h>
-#endif
 
-#ifndef LINUX
-#include <hw/dos/dos.h>
-#include <hw/cpu/cpu.h>
-#include <hw/8237/8237.h>       /* 8237 DMA */
-#include <hw/8254/8254.h>       /* 8254 timer */
-#include <hw/8259/8259.h>       /* 8259 PIC interrupts */
-#include <hw/sndsb/sndsb.h>
-#include <hw/cpu/cpurdtsc.h>
-#include <hw/dos/doswin.h>
-#include <hw/dos/tgusmega.h>
-#include <hw/dos/tgussbos.h>
-#include <hw/dos/tgusumid.h>
-#include <hw/isapnp/isapnp.h>
-#include <hw/sndsb/sndsbpnp.h>
-#endif
-
-#include "wavefmt.h"
 #include "dosamp.h"
-#include "timesrc.h"
-#include "dosptrnm.h"
-#include "filesrc.h"
-#include "resample.h"
 #include "cvip.h"
 
 uint32_t convert_ip_stereo2mono(uint32_t samples,void dosamp_FAR * const proc_buf,const uint32_t buf_max,const uint8_t bits_per_sample) {
