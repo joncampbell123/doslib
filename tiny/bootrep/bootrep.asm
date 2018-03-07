9d4eef90ed694bb620038932bed6e6a20585c78a
Jonathan Campbell <jonathan@castus.tv>
2018-03-06 23:11:28 -0800

    reg load, store, prep

--- tiny/bootrep/bootrep.asm
+++ tiny/bootrep/bootrep.asm
@@ -4,12 +4,19 @@ org 0		    ; this code is as PIC as possible
 
 ; entry point
 
+; video segment
+%ifdef TARGET_PC98
+ %define VIDSEG 0xA000
+%else
+ %define VIDSEG 0xB800
+%endif
+
 ; stack storage
 %define S_BASE  (-28)
 
 ; use CALL to store IP (instruction pointer)
 start:                              ; -> S_BASE =   0
-    call        start2              ; -> S_BASE =  -2
+    call        start2              ; -> S_BASE =  -2    NTS: Stored value is orig IP + 3
 start2:
     push        ax                  ; -> S_BASE =  -4
     push        bx                  ; -> S_BASE =  -6
@@ -23,13 +30,31 @@ start2:
     push        ds                  ; -> S_BASE = -22
     push        es                  ; -> S_BASE = -24
     push        ss                  ; -> S_BASE = -26
-    push        ax                  ; -> S_BASE = -28
+    push        ax                  ; -> S_BASE = -28   will become SP
 
     mov         bp,sp               ; -> make stack ptr accessible
-    mov         [bp],sp             ; -> S_BASE = -28 = SP
+    lea         ax,[bp-S_BASE]
+    mov         [bp],ax             ; -> S_BASE = -28
+
+    sub         word [bp-2-S_BASE],3; -> S_BASE =  -2   Adjust IP
+
+    cld                             ; prepare video RAM writing
+    mov         ax,VIDSEG
+    mov         es,ax
+    xor         di,di               ; ES:DI where to write (upper left corner)
+
+    mov         bx,[bp-2-S_BASE]    ; get IP (the base of our data)
+    add         bx,string2x
 
 ; TODO
     jmp         short $
+
+; strings
+string2x:
+    db          'SP','SS','ES','DS'
+    db          'CS','FL','BP','DI'
+    db          'SI','DX','CX','BX'
+    db          'AX','IP'
 
 ; padding
 
