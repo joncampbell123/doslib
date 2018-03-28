e31835b8c16e91af019bea3d547bd5a866c29b00
Jonathan Campbell <jonathan@castus.tv>
2018-03-28 15:32:27 -0700

    C++ fixup

--- cxx/test.cpp
+++ cxx/test.cpp
@@ -1,24 +1,5 @@
-/* test.c
- *
- * Test program: Various info about DOS
- * (C) 2009-2012 Jonathan Campbell.
- * Hackipedia DOS library.
- *
- * This code is licensed under the LGPL.
- * <insert LGPL legal text here>
- */
 
-#include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
-#include <assert.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <unistd.h>
 #include <stdio.h>
-#include <conio.h>
-#include <fcntl.h>
-#include <dos.h>
 
 int main() {
     printf("Hello world\n");
