9aea1c1da67c4414c9be50df3f9715e9256267e2
Jonathan Campbell <jonathan@castus.tv>
2018-04-08 13:02:05 -0700

    more

--- hw/floppy/floppy.c
+++ hw/floppy/floppy.c
@@ -9,13 +9,10 @@
 #include <fcntl.h>
 #include <dos.h>
 
-#include <hw/vga/vga.h>
 #include <hw/dos/dos.h>
 #include <hw/8237/8237.h>		/* DMA controller */
 #include <hw/8254/8254.h>		/* 8254 timer */
 #include <hw/8259/8259.h>		/* 8259 PIC interrupts */
-#include <hw/vga/vgagui.h>
-#include <hw/vga/vgatty.h>
 #include <hw/dos/doswin.h>
 #include <hw/floppy/floppy.h>
 
