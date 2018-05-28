8c924f3cb3e91049fdeaa5e1038e151db4744a0f
Jonathan Campbell <jonathan@castus.tv>
2018-05-27 18:41:47 -0700

    more

--- games/textdng1/game.c
+++ games/textdng1/game.c
@@ -18,6 +18,7 @@
 
 #include <hw/dos/dos.h>
 #include <hw/cpu/cpu.h>
+#include <hw/vga/vga.h>
 #include <hw/8237/8237.h>       /* 8237 DMA */
 #include <hw/8254/8254.h>       /* 8254 timer */
 #include <hw/8259/8259.h>       /* 8259 PIC interrupts */
@@ -25,6 +26,25 @@
 #include <hw/dos/doswin.h>
 
 int main(int argc,char **argv,char **envp) {
+    probe_dos();
+    cpu_probe();
+    detect_windows();
+    probe_8237();
+    if (!probe_8259()) {
+        printf("Cannot init 8259 PIC\n");
+        return 1;
+    }
+    if (!probe_8254()) {
+        printf("Cannot init 8254 timer\n");
+        return 1;
+    }
+
+	if (!probe_vga()) { /* NTS: By "VGA" we mean any VGA, EGA, CGA, MDA, or other common graphics hardware on the PC platform
+                                that acts in common ways and responds to I/O ports 3B0-3BF or 3D0-3DF as well as 3C0-3CF */
+		printf("VGA probe failed\n");
+		return 1;
+	}
+
     return 0;
 }
 
