6ce53bf621517a16f7d65a9f481e9df7711c4c10
Jonathan Campbell <jonathan@castus.tv>
2018-05-27 18:55:35 -0700

    more shit

--- games/textdng1/game.c
+++ games/textdng1/game.c
@@ -23,7 +23,45 @@
 #include <hw/8254/8254.h>       /* 8254 timer */
 #include <hw/8259/8259.h>       /* 8259 PIC interrupts */
 #include <hw/sndsb/sndsb.h>
+#include <hw/vga/vgatty.h>
 #include <hw/dos/doswin.h>
+
+static unsigned char apploop = 1;
+
+void clear_screen(void) {
+    vga_clear();
+    vga_moveto(0,0);
+    vga_write_color(0x7);
+}
+
+void do_title_screen(void) {
+    unsigned int i;
+    int c;
+
+    /* clear video RAM with background */
+    for (i=0;i < (80*25);i++)
+        vga_state.vga_alpha_ram[i] = 0x0100 + 176U;
+
+    /* message */
+    vga_write_color(0xF);
+    vga_moveto(30,8);    vga_write("Text Dungeon 1");
+    vga_moveto(22,9);    vga_write("A crappy example 2D dungeon map.");
+    vga_moveto(14,10);    vga_write("(C) 2018 Hackipedia/DOSLIB/TheGreatCodeholio");
+
+    vga_moveto(22,12);    vga_write("Hit ENTER or SPACE to continue.");
+    vga_moveto(28,13);    vga_write("Hit ESC to exit to DOS.");
+
+    do {
+        c = getch();
+        if (c == 27) {
+            apploop = 0;
+            break;
+        }
+        else if (c == ' ' || c == 13) {
+            break;
+        }
+    } while (1);
+}
 
 int main(int argc,char **argv,char **envp) {
     probe_dos();
@@ -50,6 +88,14 @@ int main(int argc,char **argv,char **envp) {
                              /* text is at either B000:0000 (mono/MDA) or B800:0000 (color/CGA) */
 
 
+    while (apploop) {
+        do_title_screen();
+        if (!apploop) break;
+    }
+
+    clear_screen();
+    vga_moveto(0,22); /* x,y near bottom of screen */
+	vga_sync_bios_cursor();
     return 0;
 }
 
