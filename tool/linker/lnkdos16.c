4a72dbf1e22327a2b80b651ad298727f7f44ed96
Jonathan Campbell <jonathan@castus.tv>
2018-07-28 17:40:47 -0700

    16-bit offset fixup: Reject fixup if self-relative and different segment bases

--- tool/linker/lnkdos16.c
+++ tool/linker/lnkdos16.c
@@ -866,8 +866,17 @@ int apply_FIXUPP(struct omf_context_t *omf_state,unsigned int first,unsigned int
             case OMF_FIXUPP_LOCATION_16BIT_OFFSET: /* 16-bit offset */
                 assert((ptr+2) <= fence);
 
-                if (!ent->segment_relative)
+                if (!ent->segment_relative) {
+                    /* sanity check: self-relative is only allowed IF the same segment */
+                    /* we could fidget about with relative fixups across real-mode segments, but I'm not going to waste my time on that */
+                    if (current_link_segment->segment_relative != targ_sdef->segment_relative) {
+                        fprintf(stderr,"FIXUPP: self-relative offset fixup across segments with different bases not allowed\n");
+                        return -1;
+                    }
+
+                    /* do it */
                     final_ofs -= ptch+2+current_link_segment->segment_offset;
+                }
 
                 *((uint16_t*)ptr) += (uint16_t)final_ofs;
                 break;
