76a3696f139f97232cf68e460fcd5a75326c84b7
Jonathan Campbell <jonathan@castus.tv>
2018-07-29 13:17:15 -0700

    more

--- tool/linker/drv3/make.sh
+++ tool/linker/drv3/make.sh
@@ -20,6 +20,7 @@ if [ x"$TOP" == x ]; then TOP=`pwd`/$rel; fi
 if [ "$1" == "clean" ]; then
     do_clean
     rm -f win95.dsk
+    rm -f win95s.dsk
     rm -Rf linux-host
     exit 0
 fi
@@ -28,6 +29,9 @@ if [ "$1" == "disk" ]; then
     # bootable win95 rescue disk with test program
     gunzip -c -d win95.dsk.gz >win95.dsk
     mcopy -i win95.dsk dos86t/test.sys ::drv.sys
+
+    gunzip -c -d win95.dsk.gz >win95s.dsk
+    mcopy -i win95s.dsk dos86s/test.sys ::drv.sys
 fi
 
 if [[ "$1" == "build" || "$1" == "" ]]; then
