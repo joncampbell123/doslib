5ee2d9746deec25c35ba7e5fd87b8ebd1e51c7dc
Jonathan Campbell <jonathan@castus.tv>
2018-08-20 17:58:23 -0700

    more

--- tool/dsxmenu/dsxmenu.c
+++ tool/dsxmenu/dsxmenu.c
@@ -17,7 +17,7 @@
 
 static const char*                  menu_ini = NULL;            // points at argv[] do not free
 
-static const char*                  menu_default_name = NULL;   // strdup(), free at end
+static char*                        menu_default_name = NULL;   // strdup(), free at end
 static int                          menu_default_timeout = 5;
 
 struct menuitem {
@@ -25,11 +25,40 @@ struct menuitem {
     char*                           menu_item_name;             // strdup()'d
 };
 
+#define MAX_MENUCHOICE_COMMANDS     100
+
+struct menuchoice {
+    char*                           menu_item_name;             // strdup()'d
+    char*                           command[MAX_MENUCHOICE_COMMANDS];
+    int                             command_items;
+};
+
+/* [menu] */
 #define MAX_MENU_ITEMS              20
 
 struct menuitem                     menu[MAX_MENU_ITEMS];
 int                                 menu_items = 0;
 int                                 menu_sel = 0;
+
+struct menuitem* menu_alloc(void) {
+    if (menu_items >= MAX_MENU_ITEMS)
+        return NULL;
+
+    return &menu[menu_items++];
+}
+
+/* [common] */
+#define MAX_COMMON_COMMANDS         100
+
+char*                               common_command[MAX_COMMON_COMMANDS];
+int                                 common_command_items = 0;
+
+/* [section] */
+
+#define MAX_MENU_CHOICES            (MAX_MENU_ITEMS)
+
+struct menuchoice                   menu_choice[MAX_MENU_CHOICES];
+int                                 menu_choice_items = 0;
 
 #define TEMP_SZ                     4096
 
@@ -115,8 +144,16 @@ static void chomp(char *s) {
     while (e >= s && (*e == 13 || *e == 10)) *e-- = 0;
 }
 
+enum {
+    LOAD_NONE=0,
+    LOAD_MENU,
+    LOAD_COMMON,
+    LOAD_CHOICE
+};
+
 int load_menu(void) {
     char *current_section = NULL;
+    int load_mode = LOAD_NONE;
     char *s = NULL;
     FILE *fp;
 
@@ -153,8 +190,68 @@ int load_menu(void) {
 
             /* 's' should point to a section header, without brackets */
             cstr_set(&current_section,s);
+            assert(current_section != NULL);
 
-            // TODO
+            if (!strcmp(current_section,"menu")) {
+                load_mode = LOAD_MENU;
+            }
+            else {
+                load_mode = LOAD_NONE;
+            }
+
+            continue;
+        }
+
+        if (load_mode == LOAD_MENU) {
+            /* name=value pairs */
+            char *name = s;
+            char *value = strchr(name,'=');
+
+            if (value != NULL) {
+                *value++ = 0; /* cut out the equals sign */
+
+                if (!strcmp(name,"menuitem")) {
+                    /* <choice>,<display text> */
+                    char *choice = value;
+                    char *display = NULL;
+
+                    while (*value != 0 && *value != ',') value++;
+                    if (*value == ',') {
+                        *value++ = 0;
+                        display = value;
+                    }
+
+                    if (display != NULL && *display != 0) {
+                        struct menuitem *item = menu_alloc();
+                        if (item == NULL) {
+                            fprintf(stderr,"Too many menu items\n");
+                            break;
+                        }
+
+                        item->menu_text = strdup(display);
+                        item->menu_item_name = strdup(choice);
+                    }
+                }
+                else if (!strcmp(name,"menudefault")) {
+                    char *choice = value;
+                    char *timeout = NULL;
+
+                    while (*value != 0 && *value != ',') value++;
+                    if (*value == ',') {
+                        *value++ = 0;
+                        timeout = value;
+                    }
+
+                    menu_default_timeout = 0;
+                    if (timeout != NULL && *timeout != 0)
+                        menu_default_timeout = atoi(timeout);
+
+                    if (menu_default_timeout <= 0)
+                        menu_default_timeout = 5;
+
+                    cstr_set(&menu_default_name,choice);
+                }
+            }
         }
     }
 
