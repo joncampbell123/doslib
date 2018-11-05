30aeecb8a652347fb30ecc7266c3aaca198b9969
Jonathan Campbell <jonathan@castus.tv>
2018-11-04 23:42:52 -0800

    DOSBOXID enable PC-98 target

--- hw/dosboxid/make.sh
+++ hw/dosboxid/make.sh
@@ -4,6 +4,7 @@ if [ x"$TOP" == x ]; then TOP=`pwd`/$rel; fi
 . $rel/linux-ow.sh
 
 dos=1 # MS-DOS
+dospc98=1 # MS-DOS PC-98
 dostiny=1 # MS-DOS tiny model
 doshuge=1 # MS-DOS huge
 win30=1 # Windows 3.0
