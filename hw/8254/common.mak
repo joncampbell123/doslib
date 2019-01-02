a4a0967ea04ebe0889256ab273285ca988ea2468
Jonathan Campbell <jonathan@castus.tv>
2019-01-02 15:59:33 -0800

    new test

--- hw/8254/common.mak
+++ hw/8254/common.mak
@@ -10,6 +10,7 @@ TEST_EXE =    $(SUBDIR)$(HPS)test.$(EXEEXT)
 TPCRAPID_EXE = $(SUBDIR)$(HPS)tpcrapid.$(EXEEXT)
 TPCRAPI2_EXE = $(SUBDIR)$(HPS)tpcrapi2.$(EXEEXT)
 TPCRAPI3_EXE = $(SUBDIR)$(HPS)tpcrapi3.$(EXEEXT)
+TPCMON1_EXE  = $(SUBDIR)$(HPS)tpcmon1.$(EXEEXT)
 
 $(HW_8254_LIB): $(OBJS)
 	wlib -q -b -c $(HW_8254_LIB) -+$(SUBDIR)$(HPS)8254.obj     -+$(SUBDIR)$(HPS)8254rd.obj   -+$(SUBDIR)$(HPS)8254tick.obj
@@ -28,7 +29,7 @@ all: $(OMFSEGDG) lib exe
 
 lib: $(HW_8254_LIB) .symbolic
 
-exe: $(TEST_EXE) $(TPCRAPID_EXE) $(TPCRAPI2_EXE) $(TPCRAPI3_EXE) .symbolic
+exe: $(TEST_EXE) $(TPCRAPID_EXE) $(TPCRAPI2_EXE) $(TPCRAPI3_EXE) $(TPCMON1_EXE) .symbolic
 
 $(TEST_EXE): $(HW_8259_LIB) $(HW_8259_LIB_DEPENDENCIES) $(HW_8254_LIB) $(HW_8254_LIB_DEPENDENCIES) $(HW_DOS_LIB) $(HW_DOS_LIB_DEPENDENCIES) $(SUBDIR)$(HPS)test.obj
 	%write tmp.cmd option quiet system $(WLINK_SYSTEM) file $(SUBDIR)$(HPS)test.obj $(HW_8259_LIB_WLINK_LIBRARIES) $(HW_8254_LIB_WLINK_LIBRARIES) $(HW_DOS_LIB_WLINK_LIBRARIES) name $(TEST_EXE) option map=$(TEST_EXE).map
@@ -47,6 +48,11 @@ $(TPCRAPI2_EXE): $(HW_8259_LIB) $(HW_8259_LIB_DEPENDENCIES) $(HW_8254_LIB) $(HW_
 
 $(TPCRAPI3_EXE): $(HW_8259_LIB) $(HW_8259_LIB_DEPENDENCIES) $(HW_8254_LIB) $(HW_8254_LIB_DEPENDENCIES) $(HW_DOS_LIB) $(HW_DOS_LIB_DEPENDENCIES) $(SUBDIR)$(HPS)tpcrapi3.obj
 	%write tmp.cmd option quiet system $(WLINK_SYSTEM) file $(SUBDIR)$(HPS)tpcrapi3.obj $(HW_8259_LIB_WLINK_LIBRARIES) $(HW_8254_LIB_WLINK_LIBRARIES) $(HW_DOS_LIB_WLINK_LIBRARIES) name $(TPCRAPI3_EXE) option map=$(TPCRAPI3_EXE).map
+	@wlink @tmp.cmd
+	@$(COPY) ..$(HPS)..$(HPS)dos32a.dat $(SUBDIR)$(HPS)dos4gw.exe
+
+$(TPCMON1_EXE): $(HW_8259_LIB) $(HW_8259_LIB_DEPENDENCIES) $(HW_8254_LIB) $(HW_8254_LIB_DEPENDENCIES) $(HW_DOS_LIB) $(HW_DOS_LIB_DEPENDENCIES) $(SUBDIR)$(HPS)tpcmon1.obj
+	%write tmp.cmd option quiet system $(WLINK_SYSTEM) file $(SUBDIR)$(HPS)tpcmon1.obj $(HW_8259_LIB_WLINK_LIBRARIES) $(HW_8254_LIB_WLINK_LIBRARIES) $(HW_DOS_LIB_WLINK_LIBRARIES) name $(TPCMON1_EXE) option map=$(TPCMON1_EXE).map
 	@wlink @tmp.cmd
 	@$(COPY) ..$(HPS)..$(HPS)dos32a.dat $(SUBDIR)$(HPS)dos4gw.exe
 
---
+++ hw/8254/tpcmon1.c
@@ -0,0 +1,115 @@
+/* Test whether the PC speaker counter can be read back, and if it counts down within the range given.
+ * Some tests in this collection require this to work. */
+
+#include <stdio.h>
+#include <conio.h> /* this is where Open Watcom hides the outp() etc. functions */
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dos.h>
+
+#include <hw/8254/8254.h>
+
+#include <hw/dos/dos.h>
+#include <hw/dos/doswin.h>
+#include <hw/dos/dosbox.h>
+#include <hw/dos/dosntvdm.h>
+
+#ifdef TARGET_WINDOWS
+# define WINFCON_STOCK_WIN_MAIN
+# include <hw/dos/winfcon.h>
+#endif
+
+int main() {
+    t8254_time_t tpc,tcc;
+    t8254_time_t spc,scc;
+    t8254_time_t delta;
+    unsigned long spreset;
+    unsigned long sprange;
+    unsigned long tmcnt;
+    unsigned long spcnt;
+    unsigned long cn;
+    int32_t tim;
+
+#if defined(TARGET_WINDOWS) && TARGET_WINDOWS == 32 && !defined(WIN386)
+    /* As a 32-bit Windows application, we *can* talk directly to the 8254 but only if running under Windows 3.1/95/98/ME.
+     * Windows NT would never permit us to directly talk to IO.
+     *
+     * However if we're a Win16 program or Watcom 386 application, Windows NT will trap and emulate the 8254 through NTVDM.EXE */
+    detect_windows();
+    if (!(windows_mode == WINDOWS_REAL || windows_mode == WINDOWS_STANDARD || windows_mode == WINDOWS_ENHANCED)) {
+        printf("This test program is only applicable to Windows 3.x/9x\n");
+        return 1;
+    }
+#endif
+
+	printf("8254 library test program\n");
+	if (!probe_8254()) {
+		printf("Chip not present. Your computer might be 2010-era hardware that dropped support for it.\n");
+		return 1;
+	}
+
+    printf("This program tests whether the PIT timer tied to PC speaker works.\n");
+
+	write_8254_system_timer(0); /* restore normal function to prevent BIOS from going crazy */
+
+    t8254_pc_speaker_set_gate(PC_SPEAKER_GATE_ON);
+    for (cn=0x10000;cn >= 0xB0/*routine may be too slow for smaller values*/;) {
+        if (cn >= 0x1000)
+            cn -= 0xFB3;
+        else if (cn >= 0x100)
+            cn -= 0x51;
+        else
+            cn -= 0x7;
+
+        printf("Testing: count=%lu %.3fHz... ",(unsigned long)cn,((double)T8254_REF_CLOCK_HZ) / cn); fflush(stdout);
+
+        write_8254_pc_speaker(cn);
+
+        tim = T8254_REF_CLOCK_HZ;
+        spcnt = tmcnt = 0;
+        spreset = 0;
+        sprange = 0;
+
+        tcc = read_8254(0);
+        scc = read_8254(T8254_TIMER_PC_SPEAKER);
+
+        do {
+            tpc = tcc;
+            spc = scc;
+
+            tcc = read_8254(0);
+            scc = read_8254(T8254_TIMER_PC_SPEAKER);
+
+            delta = tpc - tcc; /* 8254 counts DOWN. interval of 0 (0x10000) means we just use unsigned integer overflow */
+            tmcnt += delta;
+            tim -= delta;
+
+            if ((unsigned long)scc > cn) /* TODO: Should we check scc >= cn? */
+                sprange++; /* out of range */
+
+            /* 8254 counts DOWN. interval is (cn). If the new value is larger then the counter hit 0 and rolled back */
+            if (scc < spc) {/*likely*/
+                delta = spc - scc;
+            }
+            else {/*reset condition*/
+                delta = spc + (t8254_time_t)cn - scc;
+                spreset++;
+            }
+
+            spcnt += delta;
+        } while (tim > 0);
+
+        printf("tim=%lu spk=%lu spkres=%lu\n",tmcnt,spcnt,spreset);
+
+        if (kbhit()) {
+            int c = getch();
+            if (c == 27 || c == ' ') break;
+        }
+    }
+    t8254_pc_speaker_set_gate(PC_SPEAKER_GATE_OFF);
+
+	write_8254_system_timer(0); /* restore normal function to prevent BIOS from going crazy */
+	return 0;
+}
+
