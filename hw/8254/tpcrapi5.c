af71b0c684cd76c1a1f933172beea2fe4c817852
Jonathan Campbell <jonathan@castus.tv>
2019-01-03 21:00:27 -0800

    Revert "revise"
    This reverts commit afab6ec74987eb6e2d6b09cae77097d9fdf32455.

--- hw/8254/tpcrapi5.c
+++ hw/8254/tpcrapi5.c
@@ -51,16 +51,16 @@ int main() {
     t8254_pc_speaker_set_gate(PC_SPEAKER_GATE_ON);
 
     for (freq=0x10000;freq >= 0x800;) {
+        t8254_pc_speaker_set_gate(PC_SPEAKER_GATE_OFF);
+        write_8254_pc_speaker((t8254_time_t)freq);
+        t8254_pc_speaker_set_gate(PC_SPEAKER_GATE_ON);
+
         cyclesub = (0x100ul * (freq - 0x800ul)) / 0x10000ul;
         if (cyclesub == 0) cyclesub = 1;
 
         printf("Count 0x%lx sub=0x%lx... ",freq,cyclesub); fflush(stdout);
 
         for (count=0;count < 1;count++) {
-            t8254_pc_speaker_set_gate(PC_SPEAKER_GATE_OFF);
-            write_8254_pc_speaker((t8254_time_t)freq);
-            t8254_pc_speaker_set_gate(PC_SPEAKER_GATE_ON);
-
             cc = read_8254(T8254_TIMER_PC_SPEAKER);
             for (cycleout=0x400;cycleout < freq;cycleout += cyclesub) {
                 /* wait for the first half */
@@ -114,80 +114,22 @@ int main() {
     printf("The following test may be completely silent.\n");
 
     for (freq=0x10000;freq >= 0x800;) {
+        t8254_pc_speaker_set_gate(PC_SPEAKER_GATE_OFF);
+        write_8254_pc_speaker((t8254_time_t)freq);
+        t8254_pc_speaker_set_gate(PC_SPEAKER_GATE_ON);
+
         cyclesub = (0x100ul * (freq - 0x800ul)) / 0x10000ul;
         if (cyclesub == 0) cyclesub = 1;
 
         printf("Without waiting for first cycle, count 0x%lx sub=0x%lx... ",freq,cyclesub); fflush(stdout);
 
-        for (count=0;count < 1;count++) {
-            t8254_pc_speaker_set_gate(PC_SPEAKER_GATE_OFF);
-            write_8254_pc_speaker((t8254_time_t)freq);
-            t8254_pc_speaker_set_gate(PC_SPEAKER_GATE_ON);
-
+        cc = read_8254(T8254_TIMER_PC_SPEAKER);
+        do {
+            pcc = cc;
             cc = read_8254(T8254_TIMER_PC_SPEAKER);
-            for (cycleout=0x400;cycleout < freq;cycleout += cyclesub) {
-                /* wait for countdown cycle to hit our threshhold */
-                do {
-                    pcc = cc;
-                    cc = read_8254(T8254_TIMER_PC_SPEAKER);
-                } while (cc >= cycleout);
-
-                write_8254_pc_speaker((t8254_time_t)freq);
-
-                /* wait for countdown cycle to complete */
-                do {
-                    pcc = cc;
-                    cc = read_8254(T8254_TIMER_PC_SPEAKER);
-                } while (cc < pcc/*counts DOWN*/);
-            }
-        }
-
-        printf("done\n");
-
-        if (kbhit()) {
-            c = getch();
-            if (c == 27) {
-                freq = 0;
-                break;
-            }
-            else if (c == 32) {
-                break;
-            }
-        }
-
-        if (freq > 0xFFFEu)
-            freq--;
-         else if (freq >= 0x4000u) {
-            freq -= 0x2000u;
-            freq &= ~0x1FFFu;
-        }
-        else if (freq != 0u) {
-            freq -= 0x400u;
-            freq &= ~0x3FFu;
-        }
-    }
-
-    printf("Again, without waiting for the first countdown,\n");
-    printf("but waiting one cycle after setting the counter\n");
-    printf("The following test may be completely silent.\n");
-
-    for (freq=0x10000;freq >= 0x800;) {
-        cyclesub = (0x100ul * (freq - 0x800ul)) / 0x10000ul;
-        if (cyclesub == 0) cyclesub = 1;
-
-        printf("Without waiting for first cycle, count 0x%lx sub=0x%lx... ",freq,cyclesub); fflush(stdout);
+        } while (cc < pcc/*counts DOWN*/);
 
         for (count=0;count < 1;count++) {
-            t8254_pc_speaker_set_gate(PC_SPEAKER_GATE_OFF);
-            write_8254_pc_speaker((t8254_time_t)freq);
-            t8254_pc_speaker_set_gate(PC_SPEAKER_GATE_ON);
-
-            cc = read_8254(T8254_TIMER_PC_SPEAKER);
-            do {
-                pcc = cc;
-                cc = read_8254(T8254_TIMER_PC_SPEAKER);
-            } while (cc < pcc/*counts DOWN*/);
-
             cc = read_8254(T8254_TIMER_PC_SPEAKER);
             for (cycleout=0x400;cycleout < freq;cycleout += cyclesub) {
                 /* wait for countdown cycle to hit our threshhold */
