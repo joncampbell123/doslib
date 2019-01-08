f97740025527a7fad879ad6b5448c595f0c500eb
Jonathan Campbell <jonathan@castus.tv>
2019-01-07 16:51:30 -0800

    gate test

---
+++ hw/8254/tpcrapi8.c
@@ -0,0 +1,123 @@
+
+#include <stdio.h>
+#include <conio.h> /* this is where Open Watcom hides the outp() etc. functions */
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dos.h>
+
+#include <hw/8254/8254.h>
+
+#include <hw/dos/dos.h>
+#include <hw/dos/doswin.h>
+#include <hw/dos/dosbox.h>
+#include <hw/dos/dosntvdm.h>
+
+#ifdef TARGET_WINDOWS
+# define WINFCON_STOCK_WIN_MAIN
+# include <hw/dos/winfcon.h>
+#endif
+
+int main() {
+    unsigned long cycleout,cyclesub;
+    t8254_time_t pcc,cc;
+    unsigned long freq;
+    unsigned int count;
+    unsigned char ok;
+    int c;
+
+#if defined(TARGET_WINDOWS) && TARGET_WINDOWS == 32 && !defined(WIN386)
+    /* As a 32-bit Windows application, we *can* talk directly to the 8254 but only if running under Windows 3.1/95/98/ME.
+     * Windows NT would never permit us to directly talk to IO.
+     *
+     * However if we're a Win16 program or Watcom 386 application, Windows NT will trap and emulate the 8254 through NTVDM.EXE */
+    detect_windows();
+    if (!(windows_mode == WINDOWS_REAL || windows_mode == WINDOWS_STANDARD || windows_mode == WINDOWS_ENHANCED)) {
+        printf("This test program is only applicable to Windows 3.x/9x\n");
+        return 1;
+    }
+#endif
+
+	printf("8254 library test program\n");
+	if (!probe_8254()) {
+		printf("Chip not present. Your computer might be 2010-era hardware that dropped support for it.\n");
+		return 1;
+	}
+
+#if defined(TARGET_PC98)
+    /* PC-98 does not have an AND gate on the output of the timer */
+    printf("This test is not applicable to the PC-98 platform.\n");
+#else
+    printf("This program tests the AND gate on the PC speaker output.\n");
+
+//////////////////////////////////////////////////////////////////////////////
+    printf("Testing, applying gate to every cycle of the square wave.\n");
+
+	write_8254_system_timer(0); /* restore normal function to prevent BIOS from going crazy */
+    t8254_pc_speaker_set_gate(PC_SPEAKER_GATE_ON);
+
+    for (freq=0x10000;freq >= 0x800;) {
+        ok = 1;
+
+        t8254_pc_speaker_set_gate(PC_SPEAKER_GATE_OFF);
+        write_8254_pc_speaker((t8254_time_t)freq);
+        t8254_pc_speaker_set_gate(PC_SPEAKER_GATE_ON);
+
+        cyclesub = (0x100ul * (freq - 0x800ul)) / 0x10000ul;
+        if (cyclesub == 0) cyclesub = 1;
+
+        printf("Count 0x%lx sub=0x%lx... ",freq,cyclesub); fflush(stdout);
+
+        for (count=0;count < 1;count++) {
+            cc = read_8254(T8254_TIMER_PC_SPEAKER);
+            for (cycleout=0x400;cycleout < freq;cycleout += cyclesub) {
+                /* wait for countdown cycle to hit our threshhold */
+                do {
+                    pcc = cc;
+                    cc = read_8254(T8254_TIMER_PC_SPEAKER);
+                } while (cc >= cycleout);
+
+                /* mask but do not gate clock, let it keep ticking */
+                t8254_pc_speaker_set_gate(PC_SPEAKER_COUNTER_2_GATE);
+
+                /* wait for countdown cycle to complete */
+                do {
+                    pcc = cc;
+                    cc = read_8254(T8254_TIMER_PC_SPEAKER);
+                } while (cc < pcc/*counts DOWN*/);
+
+                t8254_pc_speaker_set_gate(PC_SPEAKER_GATE_ON);
+            }
+        }
+
+        printf("%s\n",ok?"PASS":"FAIL");
+
+        if (kbhit()) {
+            c = getch();
+            if (c == 27) {
+                freq = 0;
+                break;
+            }
+            else if (c == 32) {
+                break;
+            }
+        }
+
+        if (freq > 0xFFFEu)
+            freq--;
+         else if (freq >= 0x4000u) {
+            freq -= 0x2000u;
+            freq &= ~0x1FFFu;
+        }
+        else if (freq != 0u) {
+            freq -= 0x400u;
+            freq &= ~0x3FFu;
+        }
+    }
+
+    t8254_pc_speaker_set_gate(PC_SPEAKER_GATE_OFF);
+    write_8254_system_timer(0); /* restore normal function to prevent BIOS from going crazy */
+#endif
+	return 0;
+}
+
