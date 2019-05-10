83920ce5df7ed3d4e7982bf7cc61fa3386bfef5a
Jonathan Campbell <jonathan@castus.tv>
2019-05-09 23:54:33 -0700

    more

--- hw/86duino/test.c
+++ hw/86duino/test.c
@@ -14,6 +14,64 @@
 #include <hw/pci/pci.h>
 #include <hw/cpu/cpu.h>
 
+/* northbridge 0:0:0 */
+#define VORTEX86_PCI_NB_BUS         (0)
+#define VORTEX86_PCI_NB_DEV         (0)
+#define VORTEX86_PCI_NB_FUNC        (0)
+
+uint8_t vtx86_nb_readb(const uint8_t reg) {
+    return pci_read_cfgb(VORTEX86_PCI_NB_BUS,VORTEX86_PCI_NB_DEV,VORTEX86_PCI_NB_FUNC,reg);
+}
+
+uint16_t vtx86_nb_readw(const uint8_t reg) {
+    return pci_read_cfgw(VORTEX86_PCI_NB_BUS,VORTEX86_PCI_NB_DEV,VORTEX86_PCI_NB_FUNC,reg);
+}
+
+uint32_t vtx86_nb_readl(const uint8_t reg) {
+    return pci_read_cfgl(VORTEX86_PCI_NB_BUS,VORTEX86_PCI_NB_DEV,VORTEX86_PCI_NB_FUNC,reg);
+}
+
+void vtx86_nb_writeb(const uint8_t reg,const uint8_t val) {
+    pci_write_cfgb(VORTEX86_PCI_NB_BUS,VORTEX86_PCI_NB_DEV,VORTEX86_PCI_NB_FUNC,reg,val);
+}
+
+void vtx86_nb_writew(const uint8_t reg,const uint16_t val) {
+    pci_write_cfgw(VORTEX86_PCI_NB_BUS,VORTEX86_PCI_NB_DEV,VORTEX86_PCI_NB_FUNC,reg,val);
+}
+
+void vtx86_nb_writel(const uint8_t reg,const uint32_t val) {
+    pci_write_cfgl(VORTEX86_PCI_NB_BUS,VORTEX86_PCI_NB_DEV,VORTEX86_PCI_NB_FUNC,reg,val);
+}
+
+/* southbridge 0:7:0 */
+#define VORTEX86_PCI_SB_BUS         (0)
+#define VORTEX86_PCI_SB_DEV         (7)
+#define VORTEX86_PCI_SB_FUNC        (0)
+
+uint8_t vtx86_sb_readb(const uint8_t reg) {
+    return pci_read_cfgb(VORTEX86_PCI_SB_BUS,VORTEX86_PCI_SB_DEV,VORTEX86_PCI_SB_FUNC,reg);
+}
+
+uint16_t vtx86_sb_readw(const uint8_t reg) {
+    return pci_read_cfgw(VORTEX86_PCI_SB_BUS,VORTEX86_PCI_SB_DEV,VORTEX86_PCI_SB_FUNC,reg);
+}
+
+uint32_t vtx86_sb_readl(const uint8_t reg) {
+    return pci_read_cfgl(VORTEX86_PCI_SB_BUS,VORTEX86_PCI_SB_DEV,VORTEX86_PCI_SB_FUNC,reg);
+}
+
+void vtx86_sb_writeb(const uint8_t reg,const uint8_t val) {
+    pci_write_cfgb(VORTEX86_PCI_SB_BUS,VORTEX86_PCI_SB_DEV,VORTEX86_PCI_SB_FUNC,reg,val);
+}
+
+void vtx86_sb_writew(const uint8_t reg,const uint16_t val) {
+    pci_write_cfgw(VORTEX86_PCI_SB_BUS,VORTEX86_PCI_SB_DEV,VORTEX86_PCI_SB_FUNC,reg,val);
+}
+
+void vtx86_sb_writel(const uint8_t reg,const uint32_t val) {
+    pci_write_cfgl(VORTEX86_PCI_SB_BUS,VORTEX86_PCI_SB_DEV,VORTEX86_PCI_SB_FUNC,reg,val);
+}
+
 int main(int argc,char **argv) {
 	cpu_probe();
     probe_dos();
@@ -27,6 +85,25 @@ int main(int argc,char **argv) {
 		printf("PCI bus not found\n");
 		return 1;
 	}
+    /* check the northbridge and southbridge */
+    {
+        uint16_t vendor,device;
+
+        vendor = vtx86_nb_readw(0x00);
+        device = vtx86_nb_readw(0x02);
+        if (vendor == 0xFFFF || device == 0xFFFF || vendor != 0x17f3) {
+            printf("Northbridge does not seem correct\n");
+            return 1;
+        }
+
+        vendor = vtx86_sb_readw(0x00);
+        device = vtx86_sb_readw(0x02);
+        if (vendor == 0xFFFF || device == 0xFFFF || vendor != 0x17f3) {
+            printf("Southbridge does not seem correct\n");
+            return 1;
+        }
+    }
+
     printf("Looks like a Vortex86 SoC / 86Duino\n");
 	
 	return 0;
