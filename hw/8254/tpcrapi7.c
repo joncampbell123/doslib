10f191bf50ad13a2960166c534b4427f7876beb6
Jonathan Campbell <jonathan@castus.tv>
2019-05-11 22:38:46 -0700

    Fix win32s warning about no entry point

--- hw/8254/tpcrapi7.c
+++ hw/8254/tpcrapi7.c
@@ -1,7 +1,17 @@
 
 #include <stdio.h>
 
-int main(int argc,char **argv) {
+#include <hw/dos/dos.h>
+#include <hw/dos/doswin.h>
+#include <hw/dos/dosbox.h>
+#include <hw/dos/dosntvdm.h>
+
+#ifdef TARGET_WINDOWS
+# define WINFCON_STOCK_WIN_MAIN
+# include <hw/dos/winfcon.h>
+#endif
+
+int main() {
     printf("This program is obsolete\n");
     return 0;
 }
