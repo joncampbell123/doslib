41bf33a873eb2499ee6ce3873ce9ef30825cfd8e
Jonathan Campbell <jonathan@castus.tv>
2019-05-15 00:56:25 -0700

    SNDSB: SB16 resource reading, PC-98 version

--- hw/sndsb/sb16mres.c
+++ hw/sndsb/sb16mres.c
@@ -7,19 +7,31 @@ void sndsb_read_sb16_irqdma_resources(struct sndsb_ctx * const cx) {
 	const unsigned char dmam = sndsb_read_mixer(cx,0x81);
 
 	if (cx->irq < 0 && irqm != 0xFF && irqm != 0x00) {
+#if defined(TARGET_PC98)
+		if (irqm & 8)		cx->irq = 5;
+		else if (irqm & 4)	cx->irq = 12;
+		else if (irqm & 2)	cx->irq = 10;
+		else if (irqm & 1)	cx->irq = 3;
+#else
 		if (irqm & 8)		cx->irq = 10;
 		else if (irqm & 4)	cx->irq = 7;
 		else if (irqm & 2)	cx->irq = 5;
 		else if (irqm & 1)	cx->irq = 2;
+#endif
 
 		cx->do_not_probe_irq = 1;
 	}
 
     if (dmam != 0xFF && dmam != 0x00) {
 		if (cx->dma8 < 0) {
+#if defined(TARGET_PC98)
+			if (dmam & 2)	    cx->dma8 = 3;
+			else if (dmam & 1)	cx->dma8 = 0;
+#else
 			if (dmam & 8)		cx->dma8 = 3;
 			else if (dmam & 2)	cx->dma8 = 1;
 			else if (dmam & 1)	cx->dma8 = 0;
+#endif
 		}
 
 #if !defined(TARGET_PC98)
