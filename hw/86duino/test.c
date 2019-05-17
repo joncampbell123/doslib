1a9b40424bba172d08b7766632290779857d6d42
Jonathan Campbell <jonathan@castus.tv>
2019-05-17 08:09:13 -0700

    more

--- hw/86duino/test.c
+++ hw/86duino/test.c
@@ -482,48 +482,6 @@ void vtx86_analogWrite(const uint8_t pin, const uint16_t val) {
     }
 }
 
-/* NTS: This so far has only been tested against the 86Duino embedded systems */
-int probe_vtx86(void) {
-    if (!(vtx86_86duino_flags & VTX86_86DUINO_FLAG_DETECTED)) {
-        uint16_t vendor,device;
-
-        vtx86_86duino_flags = 0;
-
-        cpu_probe();
-        /* Vortex86 systems are (as far as I know) at least Pentium level (maybe 486?), and support CPUID with vendor "Vortex86 SoC" */
-        if (cpu_basic_level < CPU_486 || !(cpu_flags & CPU_FLAG_CPUID) || memcmp(cpu_cpuid_vendor,"Vortex86 SoC",12) != 0)
-            return 0;
-        /* They also have a PCI bus that's integral to locating and talking to the GPIO pins, etc. */
-        if (pci_probe(PCI_CFG_TYPE1/*Vortex86 uses this type, definitely!*/) == PCI_CFG_NONE)
-            return 0;
-
-        /* check the northbridge and southbridge */
-        vendor = vtx86_nb_readw(0x00);
-        device = vtx86_nb_readw(0x02);
-        if (vendor == 0xFFFF || device == 0xFFFF || vendor != 0x17f3)
-            return 0;
-
-        vendor = vtx86_sb_readw(0x00);
-        device = vtx86_sb_readw(0x02);
-        if (vendor == 0xFFFF || device == 0xFFFF || vendor != 0x17f3)
-            return 0;
-
-        vendor = vtx86_sb1_readw(0x00);
-        device = vtx86_sb1_readw(0x02);
-        if (!(vendor == 0xFFFF || device == 0xFFFF || vendor != 0x17f3))
-            vtx86_86duino_flags |= VTX86_86DUINO_FLAG_SB1;
-
-        vendor = vtx86_mc_readw(0x00);
-        device = vtx86_mc_readw(0x02);
-        if (!(vendor == 0xFFFF || device == 0xFFFF || vendor != 0x17f3))
-            vtx86_86duino_flags |= VTX86_86DUINO_FLAG_MC;
-
-        vtx86_86duino_flags |= VTX86_86DUINO_FLAG_DETECTED;
-    }
-
-    return !!(vtx86_86duino_flags & VTX86_86DUINO_FLAG_DETECTED);
-}
-
 int read_vtx86_gpio_pin_config(void) {
     unsigned int i;
     uint32_t tmp;
