36d5010671dbddebe4964c0b6fd957bfd622535e
Jonathan Campbell <jonathan@castus.tv>
2019-05-18 14:03:04 -0700

    vga2 probe

--- hw/vga2/test.c
+++ hw/vga2/test.c
@@ -1,7 +1,78 @@
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+uint16_t                        vga2_flags = 0;
+
+extern uint16_t                 vga2_flags;
+#define VGA2_FLAG_NONE              (0u)
+#define VGA2_FLAG_MDA               (1u << 0u)
+#define VGA2_FLAG_CGA               (1u << 1u)
+#define VGA2_FLAG_EGA               (1u << 2u)
+#define VGA2_FLAG_MCGA              (1u << 3u)
+#define VGA2_FLAG_VGA               (1u << 4u)
+#define VGA2_FLAG_MONO_DISPLAY      (1u << 5u)
+#define VGA2_FLAG_DIGITAL_DISPLAY   (1u << 6u)
+#define VGA2_FLAG_PGA               (1u << 7u)
+
+/* INT 10h AX=0x1A00 GET DISPLAY COMBINATION CODE (PS,VGA/MCGA) values of BL [http://www.ctyme.com/intr/rb-0219.htm] */
+#define probe_vga2_dcc_to_flags_sz 0x0D
+static const uint8_t probe_vga2_dcc_to_flags[probe_vga2_dcc_to_flags_sz] = {
+    VGA2_FLAG_NONE,                                                             // 0x00
+    VGA2_FLAG_MDA | VGA2_FLAG_MONO_DISPLAY,                                     // 0x01
+    VGA2_FLAG_CGA,                                                              // 0x02
+    VGA2_FLAG_NONE,                                                             // 0x03 reserved
+    VGA2_FLAG_CGA | VGA2_FLAG_EGA,                                              // 0x04
+    VGA2_FLAG_CGA | VGA2_FLAG_EGA | VGA2_FLAG_MONO_DISPLAY,                     // 0x05
+    VGA2_FLAG_CGA | VGA2_FLAG_PGA,                                              // 0x06
+    VGA2_FLAG_CGA | VGA2_FLAG_EGA | VGA2_FLAG_VGA | VGA2_FLAG_MONO_DISPLAY,     // 0x07
+    VGA2_FLAG_CGA | VGA2_FLAG_EGA | VGA2_FLAG_VGA,                              // 0x08
+    VGA2_FLAG_NONE,                                                             // 0x09
+    VGA2_FLAG_CGA | VGA2_FLAG_MCGA | VGA2_FLAG_DIGITAL_DISPLAY,                 // 0x0A
+    VGA2_FLAG_CGA | VGA2_FLAG_MCGA | VGA2_FLAG_MONO_DISPLAY,                    // 0x0B
+    VGA2_FLAG_CGA | VGA2_FLAG_MCGA                                              // 0x0C
+};
+
+/* INT 10h AX=0x1A00 GET DISPLAY COMBINATION CODE (PS,VGA/MCGA) [http://www.ctyme.com/intr/rb-0219.htm].
+ * Return value from BL (active display code). Does not return BH (alternate display code).
+ *
+ * If BL == 0xFF then the BIOS does not support the call. */
+uint8_t vga2_get_dcc_inline(void);
+#pragma aux vga2_get_dcc_inline = \
+    "mov        ax,1A00h" \
+    "xor        bx,bx" \
+    "dec        bx" \
+    "int        10h" \
+    value [bl] \
+    modify [ax bx];
+
+uint8_t vga2_get_dcc(void) {
+    return vga2_get_dcc_inline();
+}
+
+void probe_vga2(void) {
+    if (vga2_flags != 0)
+        return;
+
+    {
+        const uint8_t dcc = vga2_get_dcc_inline();
+        if (dcc < probe_vga2_dcc_to_flags_sz) {
+            vga2_flags = probe_vga2_dcc_to_flags[dcc];
+            return;
+        }
+    }
+}
 
 int main(int argc,char **argv) {
+    probe_vga2();
+
+    printf("VGA2 flags: 0x%x\n",vga2_flags);
+    if (vga2_flags & VGA2_FLAG_MDA)
+        printf("  - MDA\n");
+    if (vga2_flags & VGA2_FLAG_CGA)
+        printf("  - CGA\n");
+
     return 0;
 }
 
