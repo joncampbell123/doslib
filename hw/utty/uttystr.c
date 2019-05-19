f7e8870dd31985c0235b019d206cdeed7ac8d035
Jonathan Campbell <jonathan@castus.tv>
2019-05-19 10:17:31 -0700

    more

---
+++ hw/utty/uttystr.c
@@ -0,0 +1,60 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include <hw/dos/dos.h>
+#include <hw/utty/utty.h>
+
+unsigned int utty_string2ac(UTTY_ALPHA_CHAR *dst,unsigned int dst_max,const char **msgp,UTTY_ALPHA_CHAR refch) {
+#ifdef TARGET_PC98
+    struct ShiftJISDecoder sjis;
+    unsigned char c;
+#endif
+    const char *msg = *msgp;
+    unsigned int r = 0;
+
+    while (r < dst_max) {
+#ifdef TARGET_PC98
+        c = *msg;
+        if (c == 0) break;
+        msg++;
+
+        if (pc98_sjis_dec1(&sjis,c)) {
+            if ((r+2) > dst_max) break;
+
+            c = *msg;
+            if (c == 0) break;
+            msg++;
+
+            pc98_sjis_dec2(&sjis,c);
+
+            refch.f.ch = (sjis.b1 - 0x20u) + (sjis.b2 << 8u);
+            *(dst++) = refch;
+            r++;
+            *(dst++) = refch;
+            r++;
+        }
+        else { 
+            refch.f.ch = c;
+            *(dst++) = refch;
+            r++;
+        }
+#else
+        refch.f.ch = *msg;
+        if (refch.f.ch == 0) break;
+        msg++;
+
+        *(dst++) = refch;
+        r++;
+#endif
+    }
+
+    *msgp = msg;
+    return r;
+}
+
+unsigned int utty_string2ac_const(UTTY_ALPHA_CHAR *dst,unsigned int dst_max,const char *msg,UTTY_ALPHA_CHAR refch) {
+    return utty_string2ac(dst,dst_max,&msg,refch);
+}
+
