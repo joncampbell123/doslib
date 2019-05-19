bea816cf310d728659c93f9bf17c85189b69d647
Jonathan Campbell <jonathan@castus.tv>
2019-05-19 10:14:44 -0700

    more

--- hw/utty/test.c
+++ hw/utty/test.c
@@ -4,105 +4,12 @@
 #include <stdint.h>
 
 #include <hw/dos/dos.h>
-#ifdef TARGET_PC98
-# include <hw/necpc98/necpc98.h>
-#else
-# include <hw/vga/vga.h>
-# include <hw/vga2/vga2.h>
-#endif
-
 #include <hw/utty/utty.h>
 
-#if TARGET_MSDOS == 32
-# define UTTY_FAR
-#else
-# define UTTY_FAR   far
-#endif
-
-#define UTTY_ARRAY_LEN(x)       (sizeof(x) / sizeof(x[0]))
-#define UTTY_BLANK_CHAR         { .f.ch=' ' }
-
-typedef unsigned int            utty_offset_t;
-
-#ifdef TARGET_PC98
-# define UTTY_MAX_CHAR_WIDTH    2
-
-/* This represents the ideal pointer type for accessing VRAM. It does not necessarily contain ALL data for the char. */
-typedef uint16_t UTTY_FAR      *UTTY_ALPHA_PTR;
-
-/* PC-98 requires two WORDs, one for the 16-bit character and one for the 16-bit attribute (even if only the lower 8 are used) */
-# pragma pack(push,1)
-typedef union {
-    struct {
-        uint16_t                    ch,at;
-    } f;
-    uint32_t                        raw;
-} UTTY_ALPHA_CHAR;
-# pragma pack(pop)
-#else
-# define UTTY_MAX_CHAR_WIDTH    1
-
-/* This represents the ideal pointer type for accessing VRAM. It does not necessarily contain ALL data for the char. */
-typedef uint16_t UTTY_FAR      *UTTY_ALPHA_PTR;
-
-/* This data type represents one whole character. It doesn't necessarily match the typedef to video RAM. */
-# pragma pack(push,1)
-typedef union {
-    struct {
-        uint8_t                     ch,at;
-    } f;
-    uint16_t                        raw;
-} UTTY_ALPHA_CHAR;
-# pragma pack(pop)
-#endif
-
-static inline UTTY_ALPHA_PTR utty_seg2ptr(const unsigned short s) {
-#if TARGET_MSDOS == 32
-    return (UTTY_ALPHA_PTR)((unsigned int)s << 4u);
-#else
-    return (UTTY_ALPHA_PTR)MK_FP(s,0);
-#endif
-}
-
-struct utty_funcs_t {
-    UTTY_ALPHA_PTR          vram;
-    uint8_t                 w,h;
-    uint16_t                stride;         // in units of type UTTY_ALPHA_PTR
-
-    void                    (*update_from_screen)(void);
-    UTTY_ALPHA_CHAR         (*getchar)(utty_offset_t ofs);
-    utty_offset_t           (*setchar)(utty_offset_t ofs,UTTY_ALPHA_CHAR ch);
-    unsigned int            (*getcharblock)(utty_offset_t ofs,UTTY_ALPHA_CHAR *chptr,unsigned int count);
-    unsigned int            (*setcharblock)(utty_offset_t ofs,const UTTY_ALPHA_CHAR *chptr,unsigned int count);
-    utty_offset_t           (*getofs)(uint8_t y,uint8_t x);
-};
-
-struct utty_funcs_t         utty_funcs;
+struct utty_funcs_t                 utty_funcs;
 
 int utty_init(void) {
     return 1;
-}
-
-// COMMON
-utty_offset_t utty_funcs_common_getofs(uint8_t y,uint8_t x) {
-#if TARGET_MSDOS == 32
-    const unsigned int ofs = (unsigned int)(utty_funcs.vram);
-#else
-    const unsigned int ofs = FP_OFF(utty_funcs.vram);
-#endif
-    return ((((utty_offset_t)y * utty_funcs.stride) + (utty_offset_t)x) * sizeof(uint16_t)) + (utty_offset_t)ofs;
-}
-
-static inline utty_offset_t utty_offset_advance(const utty_offset_t o,const unsigned int amt) {
-    return o + (amt * 2u);
-}
-
-static inline UTTY_ALPHA_PTR _utty_ofs_to_ptr(const utty_offset_t o) {
-#if TARGET_MSDOS == 32
-    return (UTTY_ALPHA_PTR)(o);
-#else
-    return (UTTY_ALPHA_PTR)MK_FP(FP_SEG(utty_funcs.vram),(unsigned int)o);
-#endif
 }
 
 #ifdef TARGET_PC98
@@ -175,8 +82,7 @@ const struct utty_funcs_t utty_funcs_pc98_init = {
     .getchar =                          utty_pc98__getchar,
     .setchar =                          utty_pc98__setchar,
     .getcharblock =                     utty_pc98__getcharblock,
-    .setcharblock =                     utty_pc98__setcharblock,
-    .getofs =                           utty_funcs_common_getofs
+    .setcharblock =                     utty_pc98__setcharblock
 };
 
 int utty_init_pc98(void) {
@@ -241,8 +147,7 @@ const struct utty_funcs_t utty_funcs_vga_init = {
     .getchar =                          utty_vga__getchar,
     .setchar =                          utty_vga__setchar,
     .getcharblock =                     utty_vga__getcharblock,
-    .setcharblock =                     utty_vga__setcharblock,
-    .getofs =                           utty_funcs_common_getofs
+    .setcharblock =                     utty_vga__setcharblock
 };
 
 int utty_init_vgalib(void) {
@@ -346,7 +251,7 @@ int main(int argc,char **argv) {
 
     {
         const char *msg = "Hello world";
-        utty_offset_t o = utty_funcs.getofs(4/*row*/,4/*col*/);
+        utty_offset_t o = utty_offset_getofs(4/*row*/,4/*col*/);
         UTTY_ALPHA_CHAR uch;
         unsigned char c;
 
@@ -364,7 +269,7 @@ int main(int argc,char **argv) {
 
     {
         const char *msg = "Hello world";
-        utty_offset_t o = utty_funcs.getofs(5/*row*/,4/*col*/);
+        utty_offset_t o = utty_offset_getofs(5/*row*/,4/*col*/);
         UTTY_ALPHA_CHAR uach[12],uch;
         unsigned int i=0;
         unsigned char c;
@@ -389,7 +294,7 @@ int main(int argc,char **argv) {
 #else
         const char *msg = "Hello world";
 #endif
-        utty_offset_t o = utty_funcs.getofs(6/*row*/,4/*col*/);
+        utty_offset_t o = utty_offset_getofs(6/*row*/,4/*col*/);
         UTTY_ALPHA_CHAR uach[40],uch = UTTY_BLANK_CHAR;
         unsigned int i=0;
 
@@ -410,7 +315,7 @@ int main(int argc,char **argv) {
 #else
         const char *msg = "Hello world";
 #endif
-        utty_offset_t o = utty_funcs.getofs(7/*row*/,4/*col*/);
+        utty_offset_t o = utty_offset_getofs(7/*row*/,4/*col*/);
         UTTY_ALPHA_CHAR uach[UTTY_MAX_CHAR_WIDTH],uch = UTTY_BLANK_CHAR;
         const char *scan = msg,*scanfence = msg + strlen(msg);
         unsigned int i=0;
--- hw/utty/utty.h
+++ hw/utty/utty.h
@@ -1,1 +1,106 @@
-/*stub*/
+
+#include <i86.h>
+#include <stdint.h>
+
+#ifdef TARGET_PC98
+# include <hw/necpc98/necpc98.h>
+#else
+# include <hw/vga/vga.h>
+# include <hw/vga2/vga2.h>
+#endif
+
+#if TARGET_MSDOS == 32
+# define UTTY_FAR
+#else
+# define UTTY_FAR   far
+#endif
+
+#define UTTY_ARRAY_LEN(x)       (sizeof(x) / sizeof(x[0]))
+#define UTTY_BLANK_CHAR         { .f.ch=' ' }
+
+typedef unsigned int            utty_offset_t;
+
+#ifdef TARGET_PC98
+/////////////////////////////////////////////////////////////////////////////
+# define UTTY_MAX_CHAR_WIDTH    2
+
+/* This represents the ideal pointer type for accessing VRAM. It does not necessarily contain ALL data for the char. */
+typedef uint16_t UTTY_FAR      *UTTY_ALPHA_PTR;
+
+/* PC-98 requires two WORDs, one for the 16-bit character and one for the 16-bit attribute (even if only the lower 8 are used) */
+# pragma pack(push,1)
+typedef union {
+    struct {
+        uint16_t                    ch,at;
+    } f;
+    uint32_t                        raw;
+} UTTY_ALPHA_CHAR;
+# pragma pack(pop)
+/////////////////////////////////////////////////////////////////////////////
+#else
+/////////////////////////////////////////////////////////////////////////////
+# define UTTY_MAX_CHAR_WIDTH    1
+
+/* This represents the ideal pointer type for accessing VRAM. It does not necessarily contain ALL data for the char. */
+typedef uint16_t UTTY_FAR      *UTTY_ALPHA_PTR;
+
+/* This data type represents one whole character. It doesn't necessarily match the typedef to video RAM. */
+# pragma pack(push,1)
+typedef union {
+    struct {
+        uint8_t                     ch,at;
+    } f;
+    uint16_t                        raw;
+} UTTY_ALPHA_CHAR;
+# pragma pack(pop)
+/////////////////////////////////////////////////////////////////////////////
+#endif
+
+static inline UTTY_ALPHA_PTR utty_seg2ptr(const unsigned short s) {
+#if TARGET_MSDOS == 32
+    return (UTTY_ALPHA_PTR)((unsigned int)s << 4u);
+#else
+    return (UTTY_ALPHA_PTR)MK_FP(s,0);
+#endif
+}
+
+struct utty_funcs_t {
+    UTTY_ALPHA_PTR          vram;
+    uint8_t                 w,h;
+    uint16_t                stride;         // in units of type UTTY_ALPHA_PTR
+
+    void                    (*update_from_screen)(void);
+    UTTY_ALPHA_CHAR         (*getchar)(utty_offset_t ofs);
+    utty_offset_t           (*setchar)(utty_offset_t ofs,UTTY_ALPHA_CHAR ch);
+    unsigned int            (*getcharblock)(utty_offset_t ofs,UTTY_ALPHA_CHAR *chptr,unsigned int count);
+    unsigned int            (*setcharblock)(utty_offset_t ofs,const UTTY_ALPHA_CHAR *chptr,unsigned int count);
+};
+
+extern struct utty_funcs_t          utty_funcs;
+
+static inline utty_offset_t utty_offset_getofs(const uint8_t y,const uint8_t x) {
+#if TARGET_MSDOS == 32
+    const unsigned int ofs = (unsigned int)(utty_funcs.vram);
+#else
+    const unsigned int ofs = FP_OFF(utty_funcs.vram);
+#endif
+    return ((((utty_offset_t)y * utty_funcs.stride) + (utty_offset_t)x) * sizeof(uint16_t)) + (utty_offset_t)ofs;
+}
+
+static inline utty_offset_t utty_offset_advance(const utty_offset_t o,const unsigned int amt) {
+    return o + (amt * 2u);
+}
+
+static inline UTTY_ALPHA_PTR _utty_ofs_to_ptr(const utty_offset_t o) {
+#if TARGET_MSDOS == 32
+    return (UTTY_ALPHA_PTR)(o);
+#else
+    return (UTTY_ALPHA_PTR)MK_FP(FP_SEG(utty_funcs.vram),(unsigned int)o);
+#endif
+}
+
+int utty_init(void);
+
+unsigned int utty_string2ac(UTTY_ALPHA_CHAR *dst,unsigned int dst_max,const char **msgp,UTTY_ALPHA_CHAR refch);
+unsigned int utty_string2ac_const(UTTY_ALPHA_CHAR *dst,unsigned int dst_max,const char *msg,UTTY_ALPHA_CHAR refch);
+
