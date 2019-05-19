4f528e56c14df6f88ca06d5048ae9d582f83746e
Jonathan Campbell <jonathan@castus.tv>
2019-05-19 08:05:32 -0700

    more

--- hw/utty/test.c
+++ hw/utty/test.c
@@ -246,7 +246,54 @@ int utty_init_vgalib(void) {
 /////////////////////////////////////////////////////////////////////////////
 #endif
 
-// TODO: PC-98 targets need to parse Shift-JIS and convert to VRAM format
+#ifdef TARGET_PC98
+// TODO: Move to necpc98.h
+struct ShiftJISDecoder {
+    unsigned char       b1,b2;
+};
+
+unsigned int pc98_sjis_dec1(struct ShiftJISDecoder *j,const unsigned char c) {
+    if (c >= 0x81 && c <= 0x9F) {
+        /* Reverse:
+         *
+         *    s1 = ((j1 + 1) / 2) + 112
+         *    s1 - 112 = (j1 + 1) / 2
+         *    (s1 - 112) * 2 = j1 + 1
+         *    ((s1 - 112) * 2) - 1 = j1 */
+        j->b1 = (c - 112) * 2;
+        return 1u;
+    }
+    else if (c >= 0xE0 && c <= 0xEF) {
+        /* Reverse:
+         *
+         *    s1 = ((j1 + 1) / 2) + 176
+         *    s1 - 176 = (j1 + 1) / 2
+         *    (s1 - 176) * 2 = j1 + 1
+         *    ((s1 - 176) * 2) - 1 = j1 */
+        j->b1 = (c - 176) * 2;
+        return 1u;
+    }
+
+    return 0u;
+}
+
+unsigned int pc98_sjis_dec2(struct ShiftJISDecoder *j,const unsigned char c) {
+    // assume dec1() returned nonzero
+    if (c >= 0x9F) { /* j1 is even */
+        j->b2 = c - 126;
+        return 1u;
+    }
+    else if (c >= 0x40 && c != 0x7F) { /* j1 is odd */
+        j->b1--; /* (j1 + 1) / 2 */
+        j->b2 = c - 31;
+        if (c >= 0x80) j->b2--; /* gap at 0x7F */
+        return 1u;
+    }
+
+    return 0u;
+}
+#endif
+
 unsigned int utty_string2ac(UTTY_ALPHA_CHAR *dst,unsigned int dst_max,const char *msg,UTTY_ALPHA_CHAR refch) {
     unsigned int r = 0;
     unsigned char c;
