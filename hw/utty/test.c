f80881623bac86ec310b8f089ce6477b542bb117
Jonathan Campbell <jonathan@castus.tv>
2019-05-19 18:44:52 -0700

    more

--- hw/utty/test.c
+++ hw/utty/test.c
@@ -154,6 +154,9 @@ int main(int argc,char **argv) {
         utty_con_write("Hello world \x82\xb1\x82\xea\x82\xcd\x93\xfa\x96\x7b\x8c\xea\x82\xc5\x82\xb7\n");
 #endif
         utty_con_write("Very very very very very long long long long long very very long long long long long long long long long long too long\n");
+#ifdef TARGET_PC98
+        utty_con_write("Hello world very very very very very very long long long long too long \x82\xb1\x82\xea\x82\xcd\x93\xfa\x96\x7b\x8c\xea\x82\xc5\x82\xb7\n");
+#endif
     }
 
     return 0;
