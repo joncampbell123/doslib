26bf8b509bdd63d1b499a52f117d882d1e89c099
Jonathan Campbell <jonathan@castus.tv>
2019-05-20 21:32:15 -0700

    necpc98 library, header defs for INT DCh CL=0Ch/CL=0Dh AX=0 and AX=FFh

--- hw/necpc98/necpc98.h
+++ hw/necpc98/necpc98.h
@@ -3,6 +3,54 @@
 #define __HW_NECPC98_NECPC98_H
 
 #pragma pack(push,1)
+typedef union {
+    struct {                            /* 0x00-0x0F formatted display shortcut */
+        char        dmark;              /* 0x00-0x00 0xFE if disp text */
+        char        disp[5];            /* 0x01-0x05 display text */
+        char        skey[10];           /* 0x06-0x0F raw shortcut */
+    } d;
+    char            skey[16];           /* 0x00-0x0F raw shortcut */
+} nec_pc98_intdc_funckey;
+
+typedef struct {
+    char            skey[6];            /* 0x00-0x05 raw shortcut */
+} nec_pc98_intdc_editkey;
+
+// Key state, INT DCh CL=0Ch/CL=0Dh AX=00h
+typedef struct {
+    nec_pc98_intdc_funckey          func[10];           /* F1-F10 */
+    nec_pc98_intdc_funckey          shift_func[10];     /* shift + F1-F10 */
+    nec_pc98_intdc_editkey          editkey[11];        /* editor keys */
+} nec_pc98_intdc_keycode_state;
+
+// Key state, INT DCh CL=0Ch/CL=0Dh AX=FFh
+typedef struct {
+    nec_pc98_intdc_funckey          func[10];           /* F1-F10 */
+    nec_pc98_intdc_funckey          vfunc[5];           /* VF1-VF5 */
+    nec_pc98_intdc_funckey          shift_func[10];     /* shift + F1-F10 */
+    nec_pc98_intdc_funckey          shift_vfunc[5];     /* shift + VF1-VF10 */
+    nec_pc98_intdc_editkey          editkey[11];        /* editor keys */
+    nec_pc98_intdc_funckey          ctrl_func[10];      /* control + F1-F10 */
+    nec_pc98_intdc_funckey          ctrl_vfunc[5];      /* control + VF1-VF5 */
+} nec_pc98_intdc_keycode_state_ext;
+#pragma pack(pop)
+
+// indices into intdc_state editkey[]
+enum pc98_intdc_editkey {
+    PC98_INTDC_EK_ROLL_UP=0,
+    PC98_INTDC_EK_ROLL_DOWN=1,
+    PC98_INTDC_EK_INS=2,
+    PC98_INTDC_EK_DEL=3,
+    PC98_INTDC_EK_UP_ARROW=4,
+    PC98_INTDC_EK_LEFT_ARROW=5,
+    PC98_INTDC_EK_RIGHT_ARROW=6,
+    PC98_INTDC_EK_DOWN_ARROW=7,
+    PC98_INTDC_EK_HOME_CLR=8,
+    PC98_INTDC_EK_HELP=9,
+    PC98_INTDC_EK_KEYPAD_MINUS=10
+};
+
+#pragma pack(push,1)
 struct nec_pc98_state_t {
 	uint8_t			probed:1;
 	uint8_t			is_pc98:1;
