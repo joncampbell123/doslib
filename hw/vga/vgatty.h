6d5a3773d2fd65783074931e395089d5cfc5019e
Jonathan Campbell <jonathan@castus.tv>
2019-05-21 02:19:31 -0700

    Fn key fixup

--- hw/vga/vgatty.h
+++ hw/vga/vgatty.h
@@ -17,12 +17,12 @@ int vga_getch();
 # include <hw/necpc98/necpc98.h>
 void vga_tty_pc98_mapping(nec_pc98_intdc_keycode_state_ext *map);
 
-# define VGATTY_SC_Fn(x)        (0x7F30 + (x))
-# define VGATTY_SC_VFn(x)       (0x7F3A + (x))
-# define VGATTY_SC_sFn(x)       (0x7F40 + (x))
-# define VGATTY_SC_sVFn(x)      (0x7F4A + (x))
-# define VGATTY_SC_cFn(x)       (0x7F50 + (x))
-# define VGATTY_SC_cVFn(x)      (0x7F5A + (x))
+# define VGATTY_SC_Fn(x)        (0x7F30 + (x) - 1)
+# define VGATTY_SC_VFn(x)       (0x7F3A + (x) - 1)
+# define VGATTY_SC_sFn(x)       (0x7F40 + (x) - 1)
+# define VGATTY_SC_sVFn(x)      (0x7F4A + (x) - 1)
+# define VGATTY_SC_cFn(x)       (0x7F50 + (x) - 1)
+# define VGATTY_SC_cVFn(x)      (0x7F5A + (x) - 1)
 # define VGATTY_LEFT_ARROW      (0x7F60 + PC98_INTDC_EK_LEFT_ARROW)
 # define VGATTY_RIGHT_ARROW     (0x7F60 + PC98_INTDC_EK_RIGHT_ARROW)
 # define VGATTY_UP_ARROW        (0x7F60 + PC98_INTDC_EK_UP_ARROW)
@@ -32,9 +32,9 @@ void vga_tty_pc98_mapping(nec_pc98_intdc_keycode_state_ext *map);
 # define VGATTY_ALT_UP_ARROW    (0x7F60 + PC98_INTDC_EK_UP_ARROW)
 # define VGATTY_ALT_DOWN_ARROW  (0x7F60 + PC98_INTDC_EK_DOWN_ARROW)
 #else
-# define VGATTY_SC_Fn(x)        (0x3B00 + (x) + ((x) >= 11 ? (0x8500-0x4500) : 0))
-# define VGATTY_SC_sFn(x)       (0x5400 + (x) + ((x) >= 11 ? (0x8700-0x5E00) : 0))
-# define VGATTY_SC_cFn(x)       (0x5E00 + (x) + ((x) >= 11 ? (0x8900-0x6800) : 0))
+# define VGATTY_SC_Fn(x)        (0x3B00 + ((x) << 8) - 0x100 + ((x) >= 11 ? (0x8500-0x4500) : 0))
+# define VGATTY_SC_sFn(x)       (0x5400 + ((x) << 8) - 0x100 + ((x) >= 11 ? (0x8700-0x5E00) : 0))
+# define VGATTY_SC_cFn(x)       (0x5E00 + ((x) << 8) - 0x100 + ((x) >= 11 ? (0x8900-0x6800) : 0))
 # define VGATTY_LEFT_ARROW      (0x4B00)
 # define VGATTY_RIGHT_ARROW     (0x4D00)
 # define VGATTY_UP_ARROW        (0x4800)
