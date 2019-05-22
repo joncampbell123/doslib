16f5da56ea5e7bd46ccbce180b55ed4f12420df5
Jonathan Campbell <jonathan@castus.tv>
2019-05-21 18:54:49 -0700

    Revert "On second thought it is possible PC-98 INT 18h and MS-DOS do not return input for GRPH+keys. Follow EDIT.COM behavior and allow GRPH to be a toggle for menu entry, rather than holding down GRPH"
    This reverts commit 58062eaab52d3924e62a67cf59d4b67370873b3e.

--- hw/vga/vgagui.c
+++ hw/vga/vgagui.c
@@ -357,7 +357,6 @@ const struct vga_menu_item *vga_menu_bar_keymon() {
      * for whether or not the ALT key is down. */
         const unsigned int alt_patience_init = 5;
         unsigned int alt_patience = alt_patience_init;
-        unsigned char alt_toggle = 0;
 #endif
 
 		vga_menu_bar.sel = 0;
@@ -420,9 +419,6 @@ again:
                     int     18h
                 }
                 if (--alt_patience != 0u) continue;
-                /* user really did release GRPH */
-                alt_toggle ^= 1;
-                if (alt_toggle) continue;
 #endif
                 break;
             }
