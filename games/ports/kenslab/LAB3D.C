1cbc8eea3fc8d33bcf288c11689ae85ac287283c
Jonathan Campbell <jonathan@castus.tv>
2019-05-25 18:09:05 -0700

    more

--- games/ports/kenslab/LAB3D.C
+++ games/ports/kenslab/LAB3D.C
@@ -1,6 +1,7 @@
 #define MAIN
 #include "lab3d.h"
-unsigned char slotable[3][16] =
+
+static const unsigned char slotable[3][16] =
 {
 	5,2,4,5,3,0,4,1,2,4,5,3,5,4,1,3,
 	4,2,5,4,3,1,5,0,3,5,4,2,1,4,5,3,
