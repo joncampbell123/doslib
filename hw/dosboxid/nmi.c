fc5d9e545b9a4c4f1eb163cfbd16090780190c27
Jonathan Campbell <jonathan@castus.tv>
2019-06-01 00:54:46 -0700

    NMI inject

---
+++ hw/dosboxid/nmi.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+#include <conio.h> /* this is where Open Watcom hides the outp() etc. functions */
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <malloc.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <dos.h>
+
+#include <hw/dos/dos.h>
+#include <hw/dos/doswin.h>
+
+#include <hw/dosboxid/iglib.h>
+
+#ifdef TARGET_WINDOWS
+# define WINFCON_STOCK_WIN_MAIN
+# include <hw/dos/winfcon.h>
+#endif
+
+int main(int argc,char **argv,char **envp) {
+	probe_dos();
+	detect_windows();
+
+    if (windows_mode == WINDOWS_NT) {
+        printf("This program is not compatible with Windows NT\n");
+        return 1;
+    }
+
+	if (!probe_dosbox_id()) {
+		printf("DOSBox integration device not found\n");
+		return 1;
+	}
+	printf("DOSBox integration device found at I/O port %xh\n",dosbox_id_baseio);
+
+    dosbox_id_write_regsel(DOSBOX_ID_REG_INJECT_NMI);
+    dosbox_id_write_data(0);
+
+    return 0;
+}
+
