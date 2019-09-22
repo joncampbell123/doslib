557738c8f7db580228f0c165811d48b940fb1a4b
Jonathan Campbell <jonathan@castus.tv>
2019-09-21 19:03:08 -0700

    more

---
+++ hw/vga2/test5c.c
@@ -0,0 +1,26 @@
+
+#include <i86.h>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include <hw/vga2/vga2.h>
+
+int main(int argc,char **argv) {
+    /* base classicifcation */
+    probe_vga2();
+
+#ifdef TARGET_PC98
+#else
+    /* this version requires CGA/MDA/PCjr/Tandy */
+    if (((vga2_flags & (VGA2_FLAG_MDA|VGA2_FLAG_CGA)) == 0)/*neither MDA/CGA*/ ||
+        (vga2_flags & VGA2_FLAG_CARD_MASK & (~(VGA2_FLAG_MDA|VGA2_FLAG_CGA))) != 0/*something other than MDA/CGA*/) {
+        printf("This program requires CGA/MDA/PCjr/Tandy\n");
+        return 1;
+    }
+#endif
+
+    return 0;
+}
+
