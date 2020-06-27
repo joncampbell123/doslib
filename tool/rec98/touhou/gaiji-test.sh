29a4efa7d0df8e143a48ac49c9251d09d41509ff
Jonathan Campbell <jonathan@castus.tv>
2020-06-26 17:48:12 -0700

    more

--- tool/rec98/touhou/gaiji-test.sh
+++ tool/rec98/touhou/gaiji-test.sh
@@ -4,3 +4,13 @@ linux-host/bmp2arr -i gaiji.bmp -o gaiji.test.asm -of asm -sw 16 -sh 16 -sym 'sG
 linux-host/bmp2arr -i gaiji.bmp -o gaiji.test.bin -of bin -sw 16 -sh 16 -sym 'sGAIJI'
 linux-host/bmp2arr -i gaiji.bmp -o gaiji.test.bmp -of bmp -sw 16 -sh 16 -sym 'sGAIJI'
 
+linux-host/bmp2arr -i piano_l.bmp -o piano_l.test.c   -of c   -sw 8 -sh 8 -sym 'sPIANO_LABEL'
+linux-host/bmp2arr -i piano_l.bmp -o piano_l.test.asm -of asm -sw 8 -sh 8 -sym 'sPIANO_LABEL'
+linux-host/bmp2arr -i piano_l.bmp -o piano_l.test.bin -of bin -sw 8 -sh 8 -sym 'sPIANO_LABEL'
+linux-host/bmp2arr -i piano_l.bmp -o piano_l.test.bmp -of bmp -sw 8 -sh 8 -sym 'sPIANO_LABEL'
+
+linux-host/bmp2arr -i score.bmp -o score.test.c   -of c   -sw 8 -sh 8 -sym 'sSCORE' -u
+linux-host/bmp2arr -i score.bmp -o score.test.asm -of asm -sw 8 -sh 8 -sym 'sSCORE' -u
+linux-host/bmp2arr -i score.bmp -o score.test.bin -of bin -sw 8 -sh 8 -sym 'sSCORE' -u
+linux-host/bmp2arr -i score.bmp -o score.test.bmp -of bmp -sw 8 -sh 8 -sym 'sSCORE' -u
+
