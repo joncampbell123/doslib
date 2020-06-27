244ccc52b1ce5f0175a492b52591e248189d70e9
Jonathan Campbell <jonathan@castus.tv>
2020-06-26 20:04:53 -0700

    more

--- tool/rec98/touhou/test.sh
+++ tool/rec98/touhou/test.sh
@@ -12,11 +12,13 @@ linux-host/bmp2arr -i pointnum.bmp -o pointnum.test.asm -of asm -sw 8 -sh 8 -sym
 linux-host/bmp2arr -i pointnum.bmp -o pointnum.test.bin -of bin -sw 8 -sh 8 -sym 'sPOINTNUMS'
 linux-host/bmp2arr -i pointnum.bmp -o pointnum.test.bmp -of bmp -sw 8 -sh 8 -sym 'sPOINTNUMS'
 
+# th03 score
 linux-host/bmp2arr -i score.bmp -o score.test.c   -of c   -sw 8 -sh 8 -sym 'sSCORE' -u
 linux-host/bmp2arr -i score.bmp -o score.test.asm -of asm -sw 8 -sh 8 -sym 'sSCORE' -u
 linux-host/bmp2arr -i score.bmp -o score.test.bin -of bin -sw 8 -sh 8 -sym 'sSCORE' -u
 linux-host/bmp2arr -i score.bmp -o score.test.bmp -of bmp -sw 8 -sh 8 -sym 'sSCORE' -u
 
+# th04/th05 pellet bottom
 linux-host/bmp2arr -i pelletbt.bmp -o pelletbt.test.c   -of c   -sw 8 -sh 4 -sym 'sPELLET_BOTTOM' -pshf inner
 linux-host/bmp2arr -i pelletbt.bmp -o pelletbt.test.asm -of asm -sw 8 -sh 4 -sym 'sPELLET_BOTTOM' -pshf inner
 linux-host/bmp2arr -i pelletbt.bmp -o pelletbt.test.bin -of bin -sw 8 -sh 4 -sym 'sPELLET_BOTTOM' -pshf inner
@@ -28,11 +30,13 @@ linux-host/bmp2arr -i pointnum4.bmp -o pointnum4.test.asm -of asm -sw 8 -sh 8 -s
 linux-host/bmp2arr -i pointnum4.bmp -o pointnum4.test.bin -of bin -sw 8 -sh 8 -sym 'sPOINTNUMS' -pshf inner
 linux-host/bmp2arr -i pointnum4.bmp -o pointnum4.test.bmp -of bmp -sw 8 -sh 8 -sym 'sPOINTNUMS' -pshf inner
 
+# th05 gaiji
 linux-host/bmp2arr -i gaiji.bmp -o gaiji.test.c   -of c   -sw 16 -sh 16 -sym 'sGAIJI'
 linux-host/bmp2arr -i gaiji.bmp -o gaiji.test.asm -of asm -sw 16 -sh 16 -sym 'sGAIJI'
 linux-host/bmp2arr -i gaiji.bmp -o gaiji.test.bin -of bin -sw 16 -sh 16 -sym 'sGAIJI'
 linux-host/bmp2arr -i gaiji.bmp -o gaiji.test.bmp -of bmp -sw 16 -sh 16 -sym 'sGAIJI'
 
+# th05 piano label
 linux-host/bmp2arr -i piano_l.bmp -o piano_l.test.c   -of c   -sw 8 -sh 8 -sym 'sPIANO_LABEL'
 linux-host/bmp2arr -i piano_l.bmp -o piano_l.test.asm -of asm -sw 8 -sh 8 -sym 'sPIANO_LABEL'
 linux-host/bmp2arr -i piano_l.bmp -o piano_l.test.bin -of bin -sw 8 -sh 8 -sym 'sPIANO_LABEL'
