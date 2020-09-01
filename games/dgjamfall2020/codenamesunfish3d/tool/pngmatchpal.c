f85e1b6d1eaea303ff240ada24ae99b9daccd743
Jonathan Campbell <jonathan@castus.tv>
2020-09-01 14:03:48 -0700

    more

--- games/dgjamfall2020/codenamesunfish3d/tool/pngmatchpal.c
+++ games/dgjamfall2020/codenamesunfish3d/tool/pngmatchpal.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <png.h>    /* libpng */
+
 static char*        pal_png = NULL;
 static char*        in_png = NULL;
 static char*        out_png = NULL;
@@ -67,8 +69,61 @@ static int parse_argv(int argc,char **argv) {
     return 0;
 }
 
+static int load_palette_png(void) {
+    int ret = 1;
+
+    if (pal_png == NULL)
+        return 1;
+
+    FILE *fp = fopen(pal_png,"rb");
+    if (fp == NULL)
+        return 1;
+
+    png_structp png_context = NULL;
+    png_infop png_context_info = NULL;
+    png_infop png_context_end = NULL;
+    png_uint_32 png_width = 0,png_height = 0;
+    int png_bit_depth = 0;
+    int png_color_type = 0;
+    int png_interlace_method = 0;
+    int png_compression_method = 0;
+    int png_filter_method = 0;
+
+    png_context = png_create_read_struct(PNG_LIBPNG_VER_STRING,NULL/*error*/,NULL/*error fn*/,NULL/*warn fn*/);
+    if (png_context == NULL) goto fail;
+
+    png_context_info = png_create_info_struct(png_context);
+    if (png_context_info == NULL) goto fail;
+
+    png_init_io(png_context, fp);
+    png_read_info(png_context, png_context_info);
+
+    if (!png_get_IHDR(png_context, png_context_info, &png_width, &png_height, &png_bit_depth, &png_color_type, &png_interlace_method, &png_compression_method, &png_filter_method))
+        goto fail;
+
+    if (!(png_color_type & PNG_COLOR_MASK_PALETTE)) {
+        fprintf(stderr,"Palette PNG not paletted\n");
+        goto fail;
+    }
+
+    /* success */
+    ret = 0;
+fail:
+    if (png_context != NULL)
+        png_destroy_read_struct(&png_context,&png_context_info,&png_context_end);
+
+    if (ret)
+        fprintf(stderr,"Failed to load palette PNG\n");
+
+    fclose(fp);
+    return ret;
+}
+
 int main(int argc,char **argv) {
     if (parse_argv(argc,argv))
+        return 1;
+
+    if (!load_palette_png())
         return 1;
 
     return 0;
