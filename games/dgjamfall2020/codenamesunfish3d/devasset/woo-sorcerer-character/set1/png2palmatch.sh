a3b6393467624da8ce6c4f70af725496ee50bae3
Jonathan Campbell <jonathan@castus.tv>
2020-09-01 15:23:21 -0700

    more

---
+++ games/dgjamfall2020/codenamesunfish3d/devasset/woo-sorcerer-character/set1/png2palmatch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Imagemagick uses whatever palette order per image it likes.
+# This script runs our internal PNG palette remapper to make them consistent with palette.png.
+mkdir __DONE_PALORD__
+../../../tool/pngmatchpal "$1" "$1.palord.png" || exit 1
+mv -v "$1" __DONE_PALORD__/
