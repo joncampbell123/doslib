bab7088d973f1c8ffa981bd80b8d7e4c93fee41c
Jonathan Campbell <jonathan@castus.tv>
2020-09-02 16:10:18 -0700

    more

--- games/dgjamfall2020/codenamesunfish3d/devasset/arialfont/minipng/test.c
+++ games/dgjamfall2020/codenamesunfish3d/devasset/arialfont/minipng/test.c
@@ -328,18 +328,20 @@ int main(int argc,char **argv) {
         unsigned int i;
         unsigned char *row;
 
-        row = malloc(rdr->ihdr.width + 1); /* NTS: For some reason, PNGs have an extra pixel per row */
-        if (row != NULL) {
-            for (i=0;i < rdr->ihdr.height;i++) {
-                if (minipng_reader_read_idat(rdr,row,rdr->ihdr.width + 1) <= 0) break;
+        if (rdr->ihdr.bit_depth == 8) {
+            row = malloc(rdr->ihdr.width + 1); /* NTS: For some reason, PNGs have an extra pixel per row */
+            if (row != NULL) {
+                for (i=0;i < rdr->ihdr.height;i++) {
+                    if (minipng_reader_read_idat(rdr,row,rdr->ihdr.width + 1) <= 0) break;
 
 #if TARGET_MSDOS == 32
-                memcpy(vga_state.vga_graphics_ram + (i * 320),(unsigned char*)row,rdr->ihdr.width);
+                    memcpy(vga_state.vga_graphics_ram + (i * 320),(unsigned char*)row,rdr->ihdr.width);
 #else
-                _fmemcpy(vga_state.vga_graphics_ram + (i * 320),(unsigned char far*)row,rdr->ihdr.width);
+                    _fmemcpy(vga_state.vga_graphics_ram + (i * 320),(unsigned char far*)row,rdr->ihdr.width);
 #endif
+                }
+                free(row);
             }
-            free(row);
         }
     }
 
