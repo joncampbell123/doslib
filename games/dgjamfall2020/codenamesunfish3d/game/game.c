b377f0d6e892c0e9b7cf8e43cbd84de08cb0501c
Jonathan Campbell <jonathan@castus.tv>
2020-09-06 01:27:13 -0700

    Mode X initialization needs to zero video memory after setup to clear the junk that becomes visible. Hide the junk entirely by using the DAC mask

--- games/dgjamfall2020/codenamesunfish3d/game/game.c
+++ games/dgjamfall2020/codenamesunfish3d/game/game.c
@@ -227,7 +227,21 @@ void init_vga256unchained() {
 
     int10_setmode(19);
     update_state_from_vga();
+
+    /* Real hardware testing results show that the VGA BIOS will set up chained 256-color mode
+     * and only clear the bytes drawn and accessible by that. If you switch on unchained mode X,
+     * the junk between the chained DWORDs become visible (not cleared by VGA BIOS). So before
+     * we switch to it, use the DAC mask to blank the display first, then switch on unchained
+     * mode, clear video memory, then unblank the display by restoring the mask. */
+    outp(0x3C6,0x00); // DAC mask: set to 0 to blank display
+
     vga_enable_256color_modex(); // VGA mode X (unchained)
+
+    vga_write_sequencer(0x02/*map mask*/,0xF); // zero the video memory
+    vga_rep_stosw(vga_state.vga_graphics_ram,0,0x8000u/2u);
+
+    outp(0x3C6,0xFF); // DAC mask: restore display
+
     orig_vga_graphics_ram = vga_state.vga_graphics_ram;
     vga_state.vga_graphics_ram = orig_vga_graphics_ram + vga_next_page;
     vga_set_start_location(vga_cur_page);
