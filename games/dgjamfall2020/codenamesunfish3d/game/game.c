0e540531e2726dab662cd8544d88696c718798e3
Jonathan Campbell <jonathan@castus.tv>
2020-09-07 10:15:58 -0700

    more

--- games/dgjamfall2020/codenamesunfish3d/game/game.c
+++ games/dgjamfall2020/codenamesunfish3d/game/game.c
@@ -453,39 +453,48 @@ int font_bmp_do_load(struct font_bmp **fnt,const char *path) {
     return 0;
 }
 
-void font_bmp_draw_chardef(struct font_bmp *fnt,unsigned int cdef,unsigned int x,unsigned int y,unsigned char color) {
+unsigned int font_bmp_draw_chardef(struct font_bmp *fnt,unsigned int cdef,unsigned int x,unsigned int y,unsigned char color) {
+    unsigned int nx = x;
+
     if (fnt != NULL) {
         if (fnt->chardef != NULL && cdef < fnt->chardef_count) {
             const struct chardef *cdent = fnt->chardef + cdef;
-            const unsigned char *sp = fnt->fontbmp + (fnt->stride * cdent->y) + (cdent->x >> 3u);
-            unsigned char *dp = vga_state.vga_graphics_ram + (80u * y) + (x >> 2u);
-            unsigned char sm = 0x80u >> (cdent->x & 7u);
-            unsigned char dm = 1 << (x & 3u);
-            unsigned int sw = cdent->w;
+            x += (unsigned int)((int)cdent->xoffset);
+            y += (unsigned int)((int)cdent->yoffset);
+            nx += (unsigned int)((int)cdent->xadvance);
+            if (x < 640 && y < 400) {
+                const unsigned char *sp = fnt->fontbmp + (fnt->stride * cdent->y) + (cdent->x >> 3u);
+                unsigned char *dp = vga_state.vga_graphics_ram + (80u * y) + (x >> 2u);
+                unsigned char sm = 0x80u >> (cdent->x & 7u);
+                unsigned char dm = 1 << (x & 3u);
+                unsigned int sw = cdent->w;
 
-            while (sw-- > 0) {
-                unsigned int sh = cdent->h;
-                const unsigned char *rsp = sp;
-                unsigned char *rdp = dp;
+                while (sw-- > 0) {
+                    unsigned int sh = cdent->h;
+                    const unsigned char *rsp = sp;
+                    unsigned char *rdp = dp;
 
-                vga_write_sequencer(0x02/*map mask*/,dm);
-                while (sh-- > 0) {
-                    if ((*rsp) & sm) *rdp = color;
-                    rsp += fnt->stride;
-                    rdp += 80u;
-                }
+                    vga_write_sequencer(0x02/*map mask*/,dm);
+                    while (sh-- > 0) {
+                        if ((*rsp) & sm) *rdp = color;
+                        rsp += fnt->stride;
+                        rdp += 80u;
+                    }
 
-                if ((dm <<= 1u) & 0xF0) {
-                    dm = 1u;
-                    dp++;
-                }
-                if ((sm >>= 1u) == 0) {
-                    sm = 0x80;
-                    sp++;
+                    if ((dm <<= 1u) & 0xF0) {
+                        dm = 1u;
+                        dp++;
+                    }
+                    if ((sm >>= 1u) == 0) {
+                        sm = 0x80;
+                        sp++;
+                    }
                 }
             }
         }
     }
+
+    return nx;
 }
 
 /*---------------------------------------------------------------------------*/
@@ -740,10 +749,15 @@ void seq_intro() {
             }
 
             {
-                unsigned int i;
+                unsigned int i,x=5,y=5;
 
-                for (i=0;i < 10;i++)
-                    font_bmp_draw_chardef(arial_medium,i+1/*cdef*/,i*10,0,0x5F);
+                for (i=0;i < arial_medium->chardef_count;i++) {
+                    x = font_bmp_draw_chardef(arial_medium,i/*cdef*/,x,y,0x5F);
+                    if (x >= 300) {
+                        x = 5;
+                        y += 16;
+                    }
+                }
             }
 
             vga_swap_pages(); /* current <-> next */
