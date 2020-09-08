50759cf144e3115076f2debe988ff692e470e24f
Jonathan Campbell <jonathan@castus.tv>
2020-09-08 01:57:19 -0700

    more

--- games/dgjamfall2020/codenamesunfish3d/game/timer.c
+++ games/dgjamfall2020/codenamesunfish3d/game/timer.c
@@ -1,24 +1,9 @@
 
 #include <stdio.h>
-#include <conio.h> /* this is where Open Watcom hides the outp() etc. functions */
-#include <ctype.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <unistd.h>
-#include <assert.h>
-#include <fcntl.h>
-#include <math.h>
-#include <dos.h>
 
-#include <hw/cpu/cpu.h>
 #include <hw/dos/dos.h>
-#include <hw/dos/emm.h>
-#include <hw/dos/himemsys.h>
-#include <hw/vga/vga.h>
-#include <hw/vga/vrl.h>
 #include <hw/8254/8254.h>
 #include <hw/8259/8259.h>
-#include <fmt/minipng/minipng.h>
 
 #include "timer.h"
 
