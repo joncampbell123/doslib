44bb2e75dbbf2a6787975e11dc277c2b1f640fb9
Jonathan Campbell <jonathan@castus.tv>
2020-09-08 03:38:26 -0700

    more

--- games/dgjamfall2020/codenamesunfish3d/game/sorcpack.h
+++ games/dgjamfall2020/codenamesunfish3d/game/sorcpack.h
@@ -2,4 +2,29 @@
 #include "dumbpack.h"
 
 extern struct dumbpack *sorc_pack;
+/* PACK contents:
+
+    sorcwoo.pal             // 0 (0x00)
+    sorcwoo.sin
+    sorcwoo1.vrl
+    sorcwoo2.vrl
+    sorcwoo3.vrl
+    sorcwoo4.vrl            // 5 (0x05)
+    sorcwoo5.vrl
+    sorcwoo6.vrl
+    sorcwoo7.vrl
+    sorcwoo8.vrl
+    sorcwoo9.vrl            // 10 (0x0A)
+    sorcuhhh.vrl
+    sorcbwo1.vrl
+    sorcbwo2.vrl
+    sorcbwo3.vrl
+    sorcbwo4.vrl            // 15 (0x0F)
+    sorcbwo5.vrl
+    sorcbwo6.vrl
+    sorcbwo7.vrl
+    sorcbwo8.vrl
+    sorcbwo9.vrl            // 20 (0x14)
+                            //=21 (0x15)
+ */
 
