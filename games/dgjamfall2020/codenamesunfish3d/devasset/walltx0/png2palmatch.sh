e1e02bdf1f3f47a8a260c1988a743e78aa767c2c
Jonathan Campbell <jonathan@castus.tv>
2020-09-15 13:40:53 -0700

    more

--- games/dgjamfall2020/codenamesunfish3d/devasset/walltx0/png2palmatch.sh
+++ games/dgjamfall2020/codenamesunfish3d/devasset/walltx0/png2palmatch.sh
@@ -2,7 +2,7 @@
 #
 # Imagemagick uses whatever palette order per image it likes.
 # This script runs our internal PNG palette remapper to make them consistent with palette.png.
-make -C ../../../tool pngmatchpal || exit 1
+make -C ../../tool pngmatchpal || exit 1
 mkdir __DONE_PALORD__
-../../../tool/pngmatchpal -p palette.png -i "$1" -o "$1.palord.png" || exit 1
+../../tool/pngmatchpal -p palette.png -i "$1" -o "$1.palord.png" || exit 1
 mv -v "$1" __DONE_PALORD__/
