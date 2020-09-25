0fcf5d0be02e8679c45327a0555822a229a80d77
Jonathan Campbell <jonathan@castus.tv>
2020-09-25 01:12:58 -0700

    more

--- games/dgjamfall2020/codenamesunfish3d/game/craptn52.c
+++ games/dgjamfall2020/codenamesunfish3d/game/craptn52.c
@@ -1,14 +1,95 @@
 
-#include <conio.h>
 #include <stdio.h>
+#include <conio.h> /* this is where Open Watcom hides the outp() etc. functions */
+#include <ctype.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <assert.h>
+#include <malloc.h>
+#include <fcntl.h>
+#include <math.h>
+#include <dos.h>
+
+#include <hw/cpu/cpu.h>
+#include <hw/dos/dos.h>
+#include <hw/dos/emm.h>
+#include <hw/dos/himemsys.h>
+#include <hw/vga/vga.h>
+#include <hw/vga/vrl.h>
+#include <hw/8254/8254.h>
+#include <hw/8259/8259.h>
+#include <fmt/minipng/minipng.h>
+
+#include "timer.h"
+#include "vmode.h"
+#include "fonts.h"
+#include "vrlimg.h"
+#include "freein.h"
+#include "dbgheap.h"
+#include "fontbmp.h"
+#include "unicode.h"
+#include "commtmp.h"
+#include "sin2048.h"
+#include "vrldraw.h"
+#include "seqcomm.h"
+#include "keyboard.h"
+#include "dumbpack.h"
+#include "fzlibdec.h"
+#include "fataexit.h"
+#include "sorcpack.h"
+#include "rotozoom.h"
+#include "seqcanvs.h"
+#include "cutscene.h"
+
+#include <hw/8042/8042.h>
 
 int main(int argc,char **argv) {
     (void)argc;
     (void)argv;
 
-    printf("Minigame #1 placeholder. Hit ENTER to continue.\n");
-    while (getch() != 13);
+    probe_dos();
+	cpu_probe();
+    if (cpu_basic_level < 3) {
+        printf("This game requires a 386 or higher\n");
+        return 1;
+    }
+
+	if (!probe_8254()) {
+		printf("No 8254 detected.\n");
+		return 1;
+	}
+	if (!probe_8259()) {
+		printf("No 8259 detected.\n");
+		return 1;
+	}
+    if (!probe_vga()) {
+        printf("VGA probe failed.\n");
+        return 1;
+    }
+    if (!(vga_state.vga_flags & VGA_IS_VGA)) {
+        printf("This game requires VGA\n");
+        return 1;
+    }
+    detect_keyboard();
+
+#if TARGET_MSDOS == 16
+# if 0 // not using it yet
+    probe_emm();            // expanded memory support
+    probe_himem_sys();      // extended memory support
+# endif
+#endif
+
+    init_timer_irq();
+    init_vga256unchained();
+
+    gen_res_free();
+    check_heap();
+    unhook_irqs();
+    restore_text_mode();
+
+    //debug
+    dbg_heap_list();
 
     return 0;
 }
