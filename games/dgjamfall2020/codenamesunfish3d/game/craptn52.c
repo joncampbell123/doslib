aeb4e06923b36d45c6202c736b7ec720ae2c34eb
Jonathan Campbell <jonathan@castus.tv>
2020-09-26 02:19:34 -0700

    more

--- games/dgjamfall2020/codenamesunfish3d/game/craptn52.c
+++ games/dgjamfall2020/codenamesunfish3d/game/craptn52.c
@@ -52,6 +52,118 @@ void gen_res_free(void) {
 //    dumbpack_close(&sorc_pack);
 }
 
+/* WARNING: This function will expand bytes to a multiple of 2 pixels rounded up. Allocate your buffer accordingly. */
+void minipng_expand4to8(unsigned char *buf,unsigned int pixels) {
+    if (pixels > 0) {
+        unsigned int bytes = (pixels + 1u) / 2u;
+        unsigned char *w = buf + (bytes * 2u);
+        buf += bytes;
+
+        do {
+            unsigned char pb = *--buf;
+            w -= 2;
+            w[0] = (pb >> 4u) & 0xFu;
+            w[1] = (pb >> 0u) & 0xFu;
+        } while (--bytes != 0u);
+    }
+}
+
+static struct minipng_reader *woo_title_load_png(unsigned char *buf,unsigned int w,unsigned int h,const char *path) {
+    struct minipng_reader *rdr;
+    unsigned int y;
+
+    if ((rdr=minipng_reader_open(path)) == NULL)
+        return NULL;
+
+    if (minipng_reader_parse_head(rdr) || rdr->plte == NULL || rdr->plte_count == 0 || rdr->ihdr.width != w || rdr->ihdr.height != h) {
+        minipng_reader_close(&rdr);
+        return NULL;
+    }
+
+    for (y=0;y < h;y++) {
+        unsigned char *imgptr = buf + (y * w);
+
+        if (minipng_reader_read_idat(rdr,imgptr,1) != 1) { /* pad byte */
+            minipng_reader_close(&rdr);
+            return NULL;
+        }
+
+        if (rdr->ihdr.bit_depth == 8) {
+            if (minipng_reader_read_idat(rdr,imgptr,w) != w) { /* row */
+                minipng_reader_close(&rdr);
+                return NULL;
+            }
+        }
+        else if (rdr->ihdr.bit_depth == 4) {
+            if (minipng_reader_read_idat(rdr,imgptr,(w+1u)>>1u) != ((w+1u)>>1u)) { /* row */
+                minipng_reader_close(&rdr);
+                return NULL;
+            }
+            minipng_expand4to8(imgptr,w);
+        }
+    }
+
+    return rdr;
+}
+
+static void woo_title_display(unsigned char *imgbuf,unsigned int w,unsigned int h,const char *path) {
+    struct minipng_reader *rdr;
+    unsigned char *dp,*sp;
+    unsigned int i,j,c;
+
+    rdr = woo_title_load_png(imgbuf,w,h,path);
+    if (rdr == NULL) fatal("woo_title title %s",path);
+
+    /* set the VGA palette first. this will deliberately cause a palette flash and corruption
+     * of the old image's palette. furthermore we purposely draw it on screen slowly line by
+     * line. Because Mr. Wooo. Sorcerer programming mistakes and slow inefficient code. */
+    if (rdr->plte != NULL) {
+        vga_palette_lseek(0);
+        for (i=0;i < rdr->plte_count;i++)
+            vga_palette_write(rdr->plte[i].red>>2,rdr->plte[i].green>>2,rdr->plte[i].blue>>2);
+    }
+
+    /* 4-pixel rendering. This code is still using VGA unchained 256-color mode. */
+    for (i=0;i < h;i++) {
+        sp = imgbuf + (i * w);
+
+        for (c=0;c < 4;c++) {
+            dp = vga_state.vga_graphics_ram + (i * 80u);
+            vga_write_sequencer(0x02/*map mask*/,1u << c);
+            for (j=c;j < w;j += 4) *dp++ = sp[j];
+        }
+
+        if ((i&15) == 13) {
+            vga_wait_for_vsync();
+            vga_wait_for_vsync_end();
+        }
+    }
+
+    minipng_reader_close(&rdr);
+}
+
+void woo_title(void) {
+    unsigned char *imgbuf;
+
+    imgbuf = malloc(320u*200u); // 64000 bytes
+    if (imgbuf == NULL) fatal("woo_title imgbuf NULL");
+
+    /* as part of the gag, set the VGA mode X rendering to draw on active display.
+     * furthermore the code is written to set palette, then draw the code with
+     * deliberately poor performance. Going from one title image to another this
+     * causes a palette flash inbetween. The visual gag here is that Mr. Wooo. Sorcerer
+     * isn't the expert he thinks he is and makes some n00b programming mistakes like
+     * that. */
+    vga_cur_page = vga_next_page = VGA_PAGE_FIRST;
+    vga_state.vga_graphics_ram = orig_vga_graphics_ram + vga_next_page;
+    vga_set_start_location(vga_cur_page);
+
+    woo_title_display(imgbuf,320,200,"cr52ti1.png");
+    woo_title_display(imgbuf,320,200,"cr52ti2.png");
+
+    free(imgbuf);
+}
+
 int main(int argc,char **argv) {
     (void)argc;
     (void)argv;
@@ -90,6 +202,8 @@ int main(int argc,char **argv) {
 
     init_timer_irq();
     init_vga256unchained();
+
+    woo_title();
 
     gen_res_free();
     check_heap();
