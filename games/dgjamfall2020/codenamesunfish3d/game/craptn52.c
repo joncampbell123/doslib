23864af4cfd0e5348d06d85f372e2a4d9b5def2a
Jonathan Campbell <jonathan@castus.tv>
2020-09-26 02:24:14 -0700

    more

--- games/dgjamfall2020/codenamesunfish3d/game/craptn52.c
+++ games/dgjamfall2020/codenamesunfish3d/game/craptn52.c
@@ -144,6 +144,8 @@ static void woo_title_display(unsigned char *imgbuf,unsigned int w,unsigned int 
 
 void woo_title(void) {
     unsigned char *imgbuf;
+    uint32_t now,next;
+    int c;
 
     imgbuf = malloc(320u*200u); // 64000 bytes
     if (imgbuf == NULL) fatal("woo_title imgbuf NULL");
@@ -158,9 +160,39 @@ void woo_title(void) {
     vga_state.vga_graphics_ram = orig_vga_graphics_ram + vga_next_page;
     vga_set_start_location(vga_cur_page);
 
+    now = read_timer_counter();
+    next = now + (120u * 5u);
     woo_title_display(imgbuf,320,200,"cr52ti1.png");
-    woo_title_display(imgbuf,320,200,"cr52ti2.png");
+    do {
+        now = read_timer_counter();
+        if (kbhit()) {
+            c = getch();
+            if (c == 27) {
+                goto finishnow;
+            }
+            else if (c == ' ') {
+                next = now;
+            }
+        }
+    } while (now < next);
 
+    now = read_timer_counter();
+    next = now + (120u * 5u);
+    woo_title_display(imgbuf,320,200,"cr52ti2.png");
+    do {
+        now = read_timer_counter();
+        if (kbhit()) {
+            c = getch();
+            if (c == 27) {
+                goto finishnow;
+            }
+            else if (c == ' ') {
+                next = now;
+            }
+        }
+    } while (now < next);
+
+finishnow:
     free(imgbuf);
 }
 
