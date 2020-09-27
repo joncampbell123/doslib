f23d00c94e28ffef0b1a7cdc6b73f0f2869d08e7
Jonathan Campbell <jonathan@castus.tv>
2020-09-27 04:24:19 -0700

    more

---
+++ games/dgjamfall2020/codenamesunfish3d/game/ldwavsn.h
@@ -0,0 +1,3 @@
+
+int load_wav_into_buffer(unsigned long *length,unsigned long *srate,unsigned int *channels,unsigned int *bits,unsigned char FAR *buffer,unsigned long max_length,const char *path);
+
