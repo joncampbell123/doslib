07c3be69a5a3682f9de2bd1f904f4f66c1d750bf
Jonathan Campbell <jonathan@castus.tv>
2020-09-27 01:31:19 -0700

    craption 52 sound blaster

--- games/dgjamfall2020/codenamesunfish3d/game/craptn52.c
+++ games/dgjamfall2020/codenamesunfish3d/game/craptn52.c
@@ -20,6 +20,7 @@
 #include <hw/8237/8237.h>
 #include <hw/8254/8254.h>
 #include <hw/8259/8259.h>
+#include <hw/sndsb/sndsb.h>
 #include <fmt/minipng/minipng.h>
 
 #include "timer.h"
@@ -44,6 +45,8 @@
 
 #include <hw/8042/8042.h>
 
+struct sndsb_ctx*       sound_blaster_ctx = NULL;
+
 void gen_res_free(void) {
 //    seq_com_cleanup();
 //    sin2048fps16_free();
@@ -51,6 +54,11 @@ void gen_res_free(void) {
 //    font_bmp_free(&arial_medium);
 //    font_bmp_free(&arial_large);
 //    dumbpack_close(&sorc_pack);
+
+    if (sound_blaster_ctx != NULL) {
+        sndsb_free_card(sound_blaster_ctx);
+        sound_blaster_ctx = NULL;
+    }
 }
 
 /* WARNING: This function will expand bytes to a multiple of 2 pixels rounded up. Allocate your buffer accordingly. */
@@ -197,6 +205,58 @@ finishnow:
     free(imgbuf);
 }
 
+/* Sound Blaster detection using hw/sndsb */
+void detect_sound_blaster(void) {
+    struct sndsb_ctx* ctx;
+
+    /* First allow the user to control our detection with SET BLASTER=... in the environment block.
+     * Since DOSBox/DOSBox-X usually sets BLASTER this means we'll use whatever I/O port, IRQ, and DMA
+     * they assigned in dosbox.conf as well. */
+    if (sndsb_index_to_ctx(0)->baseio == 0/*NTS: currently does not return NULL*/ && sndsb_try_blaster_var() != NULL) {
+        if (!sndsb_init_card(sndsb_card_blaster))
+            sndsb_free_card(sndsb_card_blaster);
+    }
+
+    /* Otherwise, try the usual port 220h and port 240h most Sound Blaster cards are configured on,
+     * but only if we didn't get anything with SET BLASTER=...  Port 220h is VERY COMMON, Port 240h
+     * is much less common. */
+    if (sndsb_index_to_ctx(0)->baseio == 0/*NTS: currently does not return NULL*/)
+        sndsb_try_base(0x220);
+    if (sndsb_index_to_ctx(0)->baseio == 0/*NTS: currently does not return NULL*/)
+        sndsb_try_base(0x240);
+
+    /* Stop here if none detected */
+    if ((ctx=sndsb_index_to_ctx(0))->baseio == 0/*NTS: currently does not return NULL*/)
+        return;
+
+    printf("Possible Sound Blaster detected at I/O port %xh\n",ctx->baseio);
+
+    /* Autodetect the IRQ and DMA if not already obtained from SET BLASTER... */
+    if (ctx->irq < 0)
+        sndsb_probe_irq_F2(ctx);
+    if (ctx->irq < 0)
+        sndsb_probe_irq_80(ctx);
+    if (ctx->dma8 < 0)
+        sndsb_probe_dma8_E2(ctx);
+    if (ctx->dma8 < 0)
+        sndsb_probe_dma8_14(ctx);
+
+    /* No IRQ/DMA, no sound. Not doing Goldplay or Direct DAC in *this* game, sorry */
+    if (ctx->irq < 0 || ctx->dma8 < 0)
+        return;
+
+    /* Check card capabilities */
+    sndsb_update_capabilities(ctx);
+    sndsb_determine_ideal_dsp_play_method(ctx);
+
+    /* Ok, accept */
+    sound_blaster_ctx = ctx;
+
+    printf("Found Sound Blaster at %xh IRQ %d DMA %d\n",sound_blaster_ctx->baseio,sound_blaster_ctx->irq,sound_blaster_ctx->dma8);
+
+    t8254_wait(t8254_us2ticks(1000000)); /* 1 second */
+}
+
 int main(int argc,char **argv) {
     (void)argc;
     (void)argv;
@@ -228,7 +288,15 @@ int main(int argc,char **argv) {
         printf("This game requires VGA\n");
         return 1;
     }
+    if (!init_sndsb()) {
+        printf("Sound Blaster lib init fail.\n");
+        return 1;
+    }
+
+    write_8254_system_timer(0);
+
     detect_keyboard();
+    detect_sound_blaster();
 
 #if TARGET_MSDOS == 16
 # if 0 // not using it yet
