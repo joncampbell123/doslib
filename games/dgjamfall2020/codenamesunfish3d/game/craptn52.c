764871eb663589f0eb41b0b6d5a505add9c21025
Jonathan Campbell <jonathan@castus.tv>
2020-09-27 01:49:24 -0700

    more

--- games/dgjamfall2020/codenamesunfish3d/game/craptn52.c
+++ games/dgjamfall2020/codenamesunfish3d/game/craptn52.c
@@ -47,8 +47,79 @@
 
 struct sndsb_ctx*       sound_blaster_ctx = NULL;
 
+unsigned char           sound_blaster_irq_hook = 0;
+unsigned char           sound_blaster_old_irq_masked = 0; /* very important! If the IRQ was masked prior to running this program there's probably a good reason */
+void                    (interrupt *sound_blaster_old_irq)() = NULL;
+
+void interrupt sound_blaster_irq() {
+	unsigned char c;
+
+	sound_blaster_ctx->irq_counter++;
+
+	/* ack soundblaster DSP if DSP was the cause of the interrupt */
+	/* NTS: Experience says if you ack the wrong event on DSP 4.xx it
+	   will just re-fire the IRQ until you ack it correctly...
+	   or until your program crashes from stack overflow, whichever
+	   comes first */
+	c = sndsb_interrupt_reason(sound_blaster_ctx);
+	sndsb_interrupt_ack(sound_blaster_ctx,c);
+
+	/* FIXME: The sndsb library should NOT do anything in
+	   send_buffer_again() if it knows playback has not started! */
+	/* for non-auto-init modes, start another buffer */
+	sndsb_irq_continue(sound_blaster_ctx,c);
+
+	/* NTS: we assume that if the IRQ was masked when we took it, that we must not
+	 *      chain to the previous IRQ handler. This is very important considering
+	 *      that on most DOS systems an IRQ is masked for a very good reason---the
+	 *      interrupt handler doesn't exist! In fact, the IRQ vector could easily
+	 *      be unitialized or 0000:0000 for it! CALLing to that address is obviously
+	 *      not advised! */
+	if (sound_blaster_old_irq_masked || sound_blaster_old_irq == NULL) {
+		/* ack the interrupt ourself, do not chain */
+		if (sound_blaster_ctx->irq >= 8) p8259_OCW2(8,P8259_OCW2_NON_SPECIFIC_EOI);
+		p8259_OCW2(0,P8259_OCW2_NON_SPECIFIC_EOI);
+	}
+	else {
+		/* chain to the previous IRQ, who will acknowledge the interrupt */
+		sound_blaster_old_irq();
+	}
+}
+
 void sound_blaster_unhook_irq(void) {
-    // TODO
+    if (sound_blaster_irq_hook && sound_blaster_ctx != NULL) {
+        if (sound_blaster_ctx->irq >= 0) {
+            /* If the IRQ was masked on hooking, then mask the IRQ again */
+            if (sound_blaster_old_irq_masked)
+                p8259_mask(sound_blaster_ctx->irq);
+
+            /* Restore the old IRQ handler */
+            _dos_setvect(irq2int(sound_blaster_ctx->irq),sound_blaster_old_irq);
+        }
+
+        sound_blaster_old_irq = NULL;
+        sound_blaster_irq_hook = 0;
+    }
+}
+
+void sound_blaster_hook_irq(void) {
+    if (!sound_blaster_irq_hook && sound_blaster_ctx != NULL) {
+        if (sound_blaster_ctx->irq >= 0) {
+            /* If the IRQ was masked on entry, there's probably a good reason for it, such as
+             * a NULL vector, a BIOS (or DOSBox) with just an IRET instruction that doesn't
+             * acknowledge the interrupt, or perhaps some junk. Whatever the reason, take it
+             * as a sign not to chain to the previous interrupt handler. */
+            sound_blaster_old_irq_masked = p8259_is_masked(sound_blaster_ctx->irq);
+            if (vector_is_iret(irq2int(sound_blaster_ctx->irq)))
+                sound_blaster_old_irq_masked = 1;
+
+            /* hook the IRQ, install our own, then unmask the IRQ */
+            sound_blaster_irq_hook = 1;
+            sound_blaster_old_irq = _dos_getvect(irq2int(sound_blaster_ctx->irq));
+            _dos_setvect(irq2int(sound_blaster_ctx->irq),sound_blaster_irq);
+            p8259_unmask(sound_blaster_ctx->irq);
+        }
+    }
 }
 
 void my_unhook_irq(void) {
@@ -65,6 +136,9 @@ void gen_res_free(void) {
 
     if (sound_blaster_ctx != NULL) {
         sndsb_stop_dsp_playback(sound_blaster_ctx);
+        sound_blaster_unhook_irq();
+
+        /* NTS: This erases the sound card from the list, zeros the struct! */
         sndsb_free_card(sound_blaster_ctx);
         sound_blaster_ctx = NULL;
     }
@@ -262,6 +336,8 @@ void detect_sound_blaster(void) {
     sound_blaster_ctx = ctx;
 
     printf("Found Sound Blaster at %xh IRQ %d DMA %d\n",sound_blaster_ctx->baseio,sound_blaster_ctx->irq,sound_blaster_ctx->dma8);
+
+    sound_blaster_hook_irq();
 
     t8254_wait(t8254_us2ticks(1000000)); /* 1 second */
 }
