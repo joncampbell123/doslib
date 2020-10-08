abcb2d2a08d7395b91b0a6f83c8abf3c1928b6a0
Jonathan Campbell <jonathan@castus.tv>
2020-10-08 00:17:10 -0700

    more

--- games/dgjamfall2020/codenamesunfish3d/devasset/craption52vrl.sh
+++ games/dgjamfall2020/codenamesunfish3d/devasset/craption52vrl.sh
@@ -3,7 +3,7 @@ make -C ../../../../hw/vga png2vrl || exit 1
 
 ../../../../hw/vga/png2vrl -i cr52pal.png  -o cr52pal.vrl  -p cr52pal.pal  || exit 1
 
-for png in cr52r{n,l}??.png; do
+for png in cr52r{n,l}??.png cr52bt??.png; do
     vrl=`echo "$png" | sed -e 's/\.png$/.vrl/'`
     if [[ "$vrl" == "$png" ]]; then exit 1; fi
     pal=`echo "$png" | sed -e 's/\.png$/.pal/'`
