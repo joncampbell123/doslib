c8bcf1bd372943202a294a76415a4a28c4d68ff7
Jonathan Campbell <jonathan@castus.tv>
2020-10-18 15:45:42 -0700

    more

--- hw/dos/int16.c
+++ hw/dos/int16.c
@@ -20,11 +20,14 @@
 #include <fcntl.h>
 #include <dos.h>
 
+static unsigned char cmd = 0;
+
 static unsigned int int16_getch(void) {
+    unsigned char c=cmd;
     unsigned short r=0;
 
     __asm {
-        xor     ah,ah
+        mov     ah,c
         int     16h
         mov     r,ax
     }
@@ -36,10 +39,17 @@ int main(int argc,char **argv,char **envp) {
     unsigned int c;
     int esc=0;
 
+    if (argc > 1) {
+        cmd = (unsigned int)strtol(argv[1],NULL,0);
+        cmd &= 0x10u;
+    }
+
     printf("I will show what INT 16h on IBM PC systems is returning.\n");
     printf("Hit ESC three times to exit to DOS.\n");
     printf("The upper 8 bits are the BIOS scan code, and the lower are\n");
     printf("the ASCII code.\n");
+    printf("Using INT 16h AH=%02xh.\n",cmd);
+    printf("Specify 0 or 0x10 on command line to change.\n");
 
     while (esc < 3) {
         c = int16_getch();
