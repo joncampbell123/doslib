7d99f9abb34f9cc2be76904bc10717bc5d6d48c0
Jonathan Campbell <jonathan@castus.tv>
2020-10-20 01:10:41 -0700

    Anti pinning

--- games/dgjamfall2020/codenamesunfish3d/game/craptn52.c
+++ games/dgjamfall2020/codenamesunfish3d/game/craptn52.c
@@ -1148,6 +1148,7 @@ void game_0() {
     unsigned opp_y = 100;
     unsigned opp_dir = 0; // 0=right 1=down 2=left 3=up 4=stop 255=not moving 254=frowning
     unsigned opp_turn = 1;
+    uint32_t opp_turn_next = 0;
     /* smiley size */
     unsigned smilw = 26,smilh = 26,smilox = 32 - 16,smiloy = 32 - 16;
     /* other */
@@ -1227,9 +1228,15 @@ void game_0() {
         }
 
         /* opponent: avoid the player if too close */
-        if (abs(opp_x - player_x) < 50 && abs(opp_y - player_y) < 50) {
+        if (now < opp_turn_next) {
+            // keep going. The purpose of delaying the check again is to prevent
+            // cases where the player can pin the opponent in a corner because
+            // the opponent is constantly turning due to player proximity.
+        }
+        else if (abs(opp_x - player_x) < 50 && abs(opp_y - player_y) < 50) {
             if ((opp_x > player_x && opp_dir == 2) || // approaching from the left
                 (opp_x < player_x && opp_dir == 0)) { // approaching from the right
+                opp_turn_next = now + 30; // 30/120 or 1/4th a second
                 opp_turn = (~opp_turn) + 1;
                 if (opp_y < ((200*1)/3) || opp_y > ((200*2)/3))
                     opp_dir = (opp_x > player_x) ? 0 : 2; // reverse direction right/left
@@ -1239,6 +1246,7 @@ void game_0() {
             else if (
                 (opp_y > player_y && opp_dir == 3) || // approaching up from below
                 (opp_y < player_y && opp_dir == 1)) { // approaching down from above
+                opp_turn_next = now + 30; // 30/120 or 1/4th a second
                 opp_turn = (~opp_turn) + 1;
                 if (opp_x < ((320*1)/3) || opp_x > ((320*2)/3))
                     opp_dir = (opp_y > player_y) ? 1 : 3; // reverse direction up/down
