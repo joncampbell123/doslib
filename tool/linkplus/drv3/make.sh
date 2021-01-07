8f0b8187155ab964aea7593f23ac468da7d027bc
Jonathan Campbell <jonathan@castus.tv>
2021-01-07 08:52:00 -0800

    more

--- tool/linkplus/drv3/make.sh
+++ tool/linkplus/drv3/make.sh
@@ -27,8 +27,8 @@ fi
 if [ "$1" == "disk" ]; then
     # bootable win95 rescue disk with test program
     gunzip -c -d win95.dsk.gz >win95.dsk
-    mcopy -i win95.dsk dos86t/test.sys ::drv.sys
-    mcopy -i win95.dsk dos86t/test.exe ::drv.exe
+    mcopy -i win95.dsk dos86s/test.sys ::drv.sys
+    mcopy -i win95.dsk dos86s/test.exe ::drv.exe
 fi
 
 if [[ "$1" == "build" || "$1" == "" ]]; then
