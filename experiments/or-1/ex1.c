9cd6d663ca0e11c47dcdcf7f8291704d97a7cd5c
Jonathan Campbell <jonathan@castus.tv>
2021-02-20 23:10:45 -0800

    more

--- experiments/or-1/ex1.c
+++ experiments/or-1/ex1.c
@@ -6,5 +6,5 @@ int hello2(int a,int b) {
     return a+b;
 }
 
-__asm__ (".global startup\nstartup: l.j hello");
+__asm__ (".global startup\nstartup: l.j hello\nl.nop"); /* NTS: l.j with nop for delay slot */
 
