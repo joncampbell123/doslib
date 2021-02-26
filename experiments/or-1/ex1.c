b3224d1449b8cd83c80d07234df06c3ab420c578
Jonathan Campbell <jonathan@castus.tv>
2021-02-25 16:55:25 -0800

    more

--- experiments/or-1/ex1.c
+++ experiments/or-1/ex1.c
@@ -1,5 +1,15 @@
 
+int hello2(int a,int b);
+int hello3(int a,int b,int c);
+
 void hello() {
+    int x;
+
+    x = hello2(4,5);
+    *((volatile int*)0xA0000) = x;
+
+    x = hello3(1,2,3);
+    *((volatile int*)0xA0004) = x;
 }
 
 int hello2(int a,int b) {
