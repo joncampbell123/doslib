731187725798b89a31b8d6ebc1acb5a669d753ca
Jonathan Campbell <jonathan@castus.tv>
2021-03-01 12:38:57 -0800

    more

--- experiments/or-1/ex1.c
+++ experiments/or-1/ex1.c
@@ -1,6 +1,7 @@
 
 int hello2(int a,int b);
 int hello3(int a,int b,int c);
+int hello3if(int a,int b,int c);
 
 void hello() {
     int x;
@@ -10,10 +11,20 @@ void hello() {
 
     x = hello3(1,2,3);
     *((volatile int*)0xA0004) = x;
+
+    x = hello3if(1,-2,-3);
+    *((volatile int*)0xA0008) = x;
 }
 
 int hello2(int a,int b) {
     return a+b;
+}
+
+int hello3if(int a,int b,int c) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    if (c == 3) c++;
+    return a+b+c;
 }
 
 __asm__ (".global startup\nstartup: l.j hello\nl.nop"); /* NTS: l.j with nop for delay slot */
