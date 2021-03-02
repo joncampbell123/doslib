5a8a7e7f648f45919276285d708a75942f8c0295
Jonathan Campbell <jonathan@castus.tv>
2021-03-01 19:05:13 -0800

    more

---
+++ experiments/riscv-1/ex1.c
@@ -0,0 +1,31 @@
+
+int hello2(int a,int b);
+int hello3(int a,int b,int c);
+int hello3if(int a,int b,int c);
+
+void hello() {
+    int x;
+
+    x = hello2(4,5);
+    *((volatile int*)0xA0000) = x;
+
+    x = hello3(1,2,3);
+    *((volatile int*)0xA0004) = x;
+
+    x = hello3if(1,-2,-3);
+    *((volatile int*)0xA0008) = x;
+}
+
+int hello2(int a,int b) {
+    return a+b;
+}
+
+int hello3if(int a,int b,int c) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    if (c == 3) c++;
+    return a+b+c;
+}
+
+__asm__ (".global startup\nstartup: j hello");
+
---
+++ experiments/riscv-1/ex1b.c
@@ -0,0 +1,5 @@
+
+int hello3(int a,int b,int c) {
+    return a+b+c;
+}
+
