f7bdea5728df750d312045a785c53ff4c67b3fce
Jonathan Campbell <jonathan@castus.tv>
2021-07-02 15:37:22 -0700

    more

--- games/dgjamfall2020/codenamesunfish3d/tool/im-gen-pal.sh
+++ games/dgjamfall2020/codenamesunfish3d/tool/im-gen-pal.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-convert "$1" +dither -colors "$2" "$1.pal.png"
+# $1 = input file
+# $2 = number of colors (color palette size)
+# $3 = output PNG file
+convert "$1" +dither -colors "$2" "$3.pal.png"
