dcd4de2fb2840c5fa4ee077f00625a5b1a0f18f2
Jonathan Campbell <jonathan@castus.tv>
2021-07-12 01:06:02 -0700

    more

--- games/ifiction/first1/ifict.cpp
+++ games/ifiction/first1/ifict.cpp
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
 
 /* NTS: Do not use SDL2 if targeting MS-DOS or Windows 3.1/95, we will have our own framework to use for those targets */
 #include <SDL2/SDL.h>
@@ -58,8 +59,70 @@ int main(int argc,char **argv) {
 		IFEFatalError("SDL2 game surface (256-color) failed");
 	if ((sdl_game_palette=SDL_AllocPalette(256)) == NULL)
 		IFEFatalError("SDL2 game palette");
-	if (SDL_SetSurfacePalette(sdl_game_surface,sdl_game_palette) != 0)
-		IFEFatalError("SDL2 game palette set");
+
+	{
+		unsigned int i;
+		SDL_Color pal[256];
+
+		for (i=0;i < 64;i++) {
+			pal[i].r = i;
+			pal[i].g = i;
+			pal[i].b = i;
+			pal[i].a = 0xFF;
+
+			pal[i+64u].r = i;
+			pal[i+64u].g = 0;
+			pal[i+64u].b = 0;
+			pal[i+64u].a = 0xFF;
+
+			pal[i+128u].r = 0;
+			pal[i+128u].g = i;
+			pal[i+128u].b = 0;
+			pal[i+128u].a = 0xFF;
+
+			pal[i+192u].r = 0;
+			pal[i+192u].g = 0;
+			pal[i+192u].b = i;
+			pal[i+192u].a = 0xFF;
+		}
+
+		if (SDL_SetPaletteColors(sdl_game_palette,pal,0,256) != 0)
+			IFEFatalError("SDL2 game palette set colors");
+		if (SDL_SetSurfacePalette(sdl_game_surface,sdl_game_palette) != 0)
+			IFEFatalError("SDL2 game palette set");
+	}
+
+	{
+		unsigned int x,y;
+
+		if (SDL_MUSTLOCK(sdl_game_surface) && SDL_LockSurface(sdl_game_surface) != 0)
+			IFEFatalError("SDL2 game surface lock fail");
+		if (sdl_game_surface->pixels == NULL)
+			IFEFatalError("SDL2 game surface pixels == NULL");
+
+		memset(sdl_game_surface->pixels,0,sdl_game_surface->pitch * sdl_game_surface->h);
+
+		for (y=0;y < (unsigned int)sdl_game_surface->h;y++) {
+			unsigned char *row = (unsigned char*)sdl_game_surface->pixels + (y * sdl_game_surface->pitch);
+			for (x=0;x < (unsigned int)sdl_game_surface->w;x++) {
+				if ((x & 0xF0) != 0xF0)
+					row[x] = (y & 0xFF);
+				else
+					row[x] = 0;
+			}
+		}
+
+		if (SDL_MUSTLOCK(sdl_game_surface))
+			SDL_UnlockSurface(sdl_game_surface);
+
+		if (SDL_BlitSurface(sdl_window_surface,NULL,sdl_game_surface,NULL) != 0)
+			IFEFatalError("Game to window BlitSurface");
+
+		if (SDL_UpdateWindowSurface(sdl_window) != 0)
+			IFEFatalError("Window surface update");
+
+		sleep(3);
+	}
 
 	IFEShutdownVideo();
 	return 0;
