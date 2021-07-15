2aebbf9d66c9a4805618d34945574763cc762d02
Jonathan Campbell <jonathan@castus.tv>
2021-07-15 13:25:07 -0700

    cooked keyboard map

--- games/ifiction/first1/keyboard.cpp
+++ games/ifiction/first1/keyboard.cpp
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <string.h>
 
 #include "ifict.h"
 #include "utils.h"
@@ -20,6 +21,37 @@ void IFEKeyQueueEmptyAll(void) {
 }
 
 void IFEKeyboardProcessRawToCooked(const IFEKeyEvent &ev) {
-	(void)ev;
+	IFECookedKeyEvent cke;
+
+	memset(&cke,0,sizeof(cke));
+
+#define SCHRMAP(x,norm,shift,ctrl,alt) \
+		case x: \
+			if (ev.flags & (IFEKeyEvent_FLAG_LALT|IFEKeyEvent_FLAG_RALT)) \
+				cke.code = (alt); \
+			else if (ev.flags & (IFEKeyEvent_FLAG_LCTRL|IFEKeyEvent_FLAG_RCTRL)) \
+				cke.code = (ctrl); \
+			else if (ev.flags & (IFEKeyEvent_FLAG_LSHIFT|IFEKeyEvent_FLAG_RSHIFT)) \
+				cke.code = (shift); \
+			else \
+				cke.code = (norm); \
+			break;
+
+	switch (ev.code) {
+		/* IFEKEY                                        Norm,   Shift,  Ctrl,   Alt */
+		SCHRMAP(IFEKEY_RETURN,                           13,     0,      0,      0);
+		SCHRMAP(IFEKEY_ESCAPE,                           27,     0,      0,      0);
+		SCHRMAP(IFEKEY_BACKSPACE,                        8,      0,      0,      0);
+		SCHRMAP(IFEKEY_TAB,                              9,      0,      0,      0);
+		SCHRMAP(IFEKEY_SPACE,                            ' ',    ' ',    0,      0);
+		default: break;
+	}
+
+	if (ev.code != 0) {
+		if (!IFECookedKeyQueue.add(cke))
+			IFEDBG("ProcessKeyboardEvent: Cooked queue full");
+	}
+
+#undef SCHRMAP
 }
 
