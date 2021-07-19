a7566f5ecbaed887e31c9f731305e5b93bac30c2
Jonathan Campbell <jonathan@castus.tv>
2021-07-19 11:50:54 -0700

    Host standard cursors

--- games/ifiction/first1/mouse.h
+++ games/ifiction/first1/mouse.h
@@ -23,6 +23,10 @@ extern IFESimpleQueue<IFEMouseEvent,IFEMouseQueueSize>			IFEMouseQueue;
 #define IFEMouseStatus_MBUTTON		(1u << 1u)
 #define IFEMouseStatus_RBUTTON		(1u << 2u)
 
+#define IFEStdCursor_NONE		(0u)
+#define IFEStdCursor_POINTER		(1u)
+#define IFEStdCursor_WAIT		(2u)
+
 /* moved: mouse moved */
 /* rel moved: mouse movement was relative and added to rx/ry */
 /* abs moved: mouse movement was absolute and set to x/y (i.e. touchscreen, tablet, DOSBox-X mouse integration) */
