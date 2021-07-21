213e0e7ab9ba0a2a927ba882bb8ad176a18c3670
Jonathan Campbell <jonathan@castus.tv>
2021-07-21 11:45:14 -0700

    DOSLIB support: Look only for modes the VESA BIOS is willing to provide XResolution/YResolution for. VBE 1.2 and higher are required to provide that info, but VBE 1.0 allows it to be optional

--- games/ifiction/first1/t_doslib.cpp
+++ games/ifiction/first1/t_doslib.cpp
@@ -596,12 +596,17 @@ static void p_InitVideo(void) {
 	/* Find 640x480 256-color mode.
 	 * Linear framebuffer required (we'll support older bank switched stuff later) */
 	{
-		const uint32_t wantf1 = VESA_MODE_ATTR_HW_SUPPORTED | VESA_MODE_ATTR_GRAPHICS_MODE;
+		/* we're looking for: Mode supported by hardware configuration, graphics mode, and mode information contains XResolution/YResolution required by VBE 1.2 */
+		/* Note that according to the VBE 1.0 standard, VBE BIOSes prior to 1.2 are allowed to omit fields beyond BytesPerScanLine and that's why that matters */
+		const uint32_t wantf1 = VESA_MODE_ATTR_HW_SUPPORTED | VESA_MODE_ATTR_GRAPHICS_MODE | VESA_MODE_ATTR_VBE_1X_MOREINFO;
 		struct vbe_mode_info mi = {0};
 		uint16_t found_mode_w = 0;
 		uint16_t found_mode = 0;
 		unsigned int entry;
 		uint16_t mode;
+
+		IFEDBG("VBE BIOS detected version %u.%u",vbe_info->version>>8u,vbe_info->version&0xFFu);
+		if (vbe_info->version < 0x102) IFEDBG("You have a really old VBE BIOS!");
 
 		memset(&ifevidinfo_doslib,0,sizeof(ifevidinfo_doslib));
 
