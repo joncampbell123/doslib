9794ab0d5f0ed1a4f720ef91bd82c563b33892fc
Jonathan Campbell <jonathan@castus.tv>
2021-12-08 11:56:02 -0800

    more

---
+++ tool/opcc/make.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+rel=../..
+if [ x"$TOP" == x ]; then TOP=`pwd`/$rel; fi
+. $rel/linux-ow.sh
+
+if [ "$1" == "clean" ]; then
+    do_clean
+    make clean
+    exit 0
+fi
+
+if [ "$1" == "disk" ]; then
+    true
+fi
+
+if [[ "$1" == "build" || "$1" == "" ]]; then
+    true
+fi
+
