71457f41e38b4fcaaf6dee8f0267b879fb4112a3
Jonathan Campbell <jonathan@castus.tv>
2023-01-06 18:18:30 -0800

    more

---
+++ fmt/omf/testfile/0007.asm
@@ -0,0 +1,33 @@
+
+; assemble with Netwide Assembler
+
+	segment	CODE class=CODE align=16 USE16
+	global	_global1
+	global	x1,x2
+
+	nop
+_global1:
+	nop
+..start:
+	ret
+
+	bits	32
+	segment	CODE2 class=CODE align=16 USE32
+	global	_global2
+	global	x1,x2
+
+	mov	eax,ebx
+_global2:
+	mov	esi,0x12345678
+	ret
+
+	segment BSS class=BSS align=16 nobits
+
+x1:	resw 1
+x2:	resd 1
+
+	segment stack STACK class=STACK align=4
+
+stack1:
+	resb 1024
+
--- fmt/omf/testfile/make.sh
+++ fmt/omf/testfile/make.sh
@@ -13,5 +13,6 @@ if [[ "$1" == "build" || "$1" == "" ]]; then
 	nasm -o 0004.obj -f obj 0004.asm || exit 1
 	nasm -o 0005.obj -f obj 0005.asm || exit 1
 	nasm -o 0006.obj -f obj 0006.asm || exit 1
+	nasm -o 0007.obj -f obj 0007.asm || exit 1
 fi
 
