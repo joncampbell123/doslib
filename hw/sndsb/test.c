d5d2787d5d6a17d266404fbe16ab63ef8a92dddf
Jonathan Campbell <jonathan@castus.tv>
2023-02-12 08:52:24 -0800

    Update copyright

--- hw/sndsb/test.c
+++ hw/sndsb/test.c
@@ -5250,7 +5250,7 @@ int main(int argc,char **argv) {
 #endif
 			else if (mitem == &main_menu_help_about) {
 				struct vga_msg_box box;
-				vga_msg_box_create(&box,"Sound Blaster test program v1.4 for DOS\n\n(C) 2008-2019 Jonathan Campbell\nALL RIGHTS RESERVED\n"
+				vga_msg_box_create(&box,"Sound Blaster test program v1.5 for DOS\n\n(C) 2008-2023 Jonathan Campbell\nALL RIGHTS RESERVED\n"
 #if TARGET_MSDOS == 32
 					"32-bit protected mode version"
 #elif defined(__LARGE__)
