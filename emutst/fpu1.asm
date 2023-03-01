3c8950be014d1978ea49861b27f80bd15f436676
Jonathan Campbell <jonathan@castus.tv>
2023-02-28 23:49:14 -0800

    more

--- emutst/fpu1.asm
+++ emutst/fpu1.asm
@@ -3,7 +3,31 @@
 	org	100h
 
 start:
+	finit
+	fldz
+	fld1
+	faddp
+	fld	dword [data1]
+	fld	qword [data2]
+	fld	tword [data3]
+	faddp
+	faddp
+	faddp
+
+	nop
+	nop
+
+	finit
+	fld	dword [data1]
+	fst
+	fmulp
 
 end:
 	ret
 
+data1:
+	dd	1.23
+data2:
+	dq	1.23
+data3:
+	dt	1.23
