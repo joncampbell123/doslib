39372de3a462abfeb11f1f9b56650576c08595b8
Jonathan Campbell <jonathan@castus.tv>
2024-01-05 23:54:45 -0800

    declspec(fld) failed, remove

--- hw/dos/testprnb.c
+++ hw/dos/testprnb.c
@@ -11,19 +11,11 @@ static void TEST(const unsigned char *x,size_t sz) {
 	printf("\n");
 }
 
-#ifdef __WATCOMC__
-# define FLD __declspec(fld)
-#else
-# define FLD
-#endif
-
 static const long double			lv = 1.0l;
-static const FLD long double			lv2 = 1.0l;
 static const double				dv = 1.0;
 
 int main(void) {
 	printf("long double:                  "); TEST((unsigned char*)(&lv), sizeof(lv));
-	printf("long double __declspec(fld):  "); TEST((unsigned char*)(&lv2),sizeof(lv2));
 	printf("double:                       "); TEST((unsigned char*)(&dv), sizeof(dv));
 	return 0;
 }
