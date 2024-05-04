f258a78104e3d1e7db550cfc0ef1650a611cb42a
Jonathan Campbell <jonathan@castus.tv>
2024-05-04 11:11:20 -0700

    Start over, try again

---
+++ lang/cim/cimcc.cpp
@@ -0,0 +1,316 @@
+
+#include <unistd.h>
+#include <string.h>
+#include <assert.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+#include <limits>
+#include <new>
+
+namespace CIMCC {
+
+	/////////
+
+	typedef int (*refill_function_t)(unsigned char *at,size_t how_much,void *context,size_t context_size);
+
+	static int refill_null(unsigned char *,size_t,void*,size_t) { return 0; }
+
+	/////////
+
+	struct context_t {
+		unsigned char*		ptr = NULL;
+		size_t			ptr_size = 0;
+		bool			ownership = false;
+
+		bool alloc(size_t sz) {
+			if (ptr != NULL)
+				return false;
+			if (sz == 0 || sz > 65536)
+				return false;
+
+			ptr = new(std::nothrow) unsigned char[sz];
+			if (ptr == NULL)
+				return false;
+
+			ownership = true;
+			ptr_size = sz;
+			return true;
+		}
+
+		void free(void) {
+			if (ptr && ownership) delete[] ptr;
+			ownership = false;
+			ptr_size = 0;
+			ptr = NULL;
+		}
+
+		context_t() { }
+
+		~context_t() { free(); }
+	};
+
+	struct parse_buffer {
+		unsigned char*		buffer = NULL; /* base of the allocated buffer */
+		unsigned char*		read = NULL; /* read position */
+		unsigned char*		end = NULL; /* last valid byte (and where more is loaded) */
+		size_t			buffer_size = 0; /* allocated buffer size */
+		bool			ownership = false;
+
+		bool is_alloc(void) const {
+			return buffer != NULL;
+		}
+
+		inline unsigned char* fence(void) const {
+			return buffer + buffer_size;
+		}
+
+		bool alloc(size_t sz=0) {
+			/* No, we're not going to allow changing the buffer size on the fly. Free it first. */
+			if (buffer != NULL) return false;
+
+			/* Be reasonable, don't ask for large buffers and don't make it too small */
+			if (sz < 2048 || sz > 8192) sz = 4096;
+
+			/* Allocate the buffer, return false if it fails.
+			 * Use std::nothrow to avoid having to deal with C++ exceptions. */
+			/* Allocating the buffer resets all pointers. */
+			buffer = new(std::nothrow) unsigned char[sz];
+			if (buffer == NULL) return false;
+			read = end = buffer;
+			buffer_size = sz;
+			ownership = true;
+			return true;
+		}
+
+		void free(void) {
+			if (buffer && ownership) delete[] buffer;
+			buffer = read = end = NULL;
+			ownership = false;
+			buffer_size = 0;
+		}
+
+		bool sanity_check(void) const {
+			return buffer <= read && read <= end && end <= fence();
+		}
+
+		bool eof(void) const {
+			return read >= end;
+		}
+
+		void flush(size_t keep=0/*how much to keep prior to read ptr*/) {
+			if (buffer) {
+				assert(sanity_check());
+
+				/* it is hard to flush if the caller asks to keep too much */
+				if (keep > (buffer_size/4u))
+					keep = (buffer_size/4u);
+
+				if (keep > size_t(read-buffer))
+					keep = size_t(read-buffer);
+
+				size_t howmuch = size_t(end-read);
+				if (howmuch != 0 && read != (buffer + keep)) {
+					assert((buffer + keep + howmuch) <= fence());
+					assert((read + howmuch) <= fence());
+					assert((buffer + keep) < read); /* already checked read != (buffer + keep) */
+					memmove(buffer + keep,read,howmuch);
+				}
+				read = buffer + keep;
+				end = read + howmuch;
+
+				assert(sanity_check());
+			}
+		}
+
+		void lazy_flush(size_t keep=0) {
+			if (size_t(fence() - end) < (buffer_size / 2u))
+				flush(keep);
+		}
+
+		void refill(refill_function_t f,context_t &c) {
+			/* caller must lazy flush to make room */
+			if (f && end < fence()) {
+				int got = f(end,size_t(fence() - end),c.ptr,c.ptr_size);
+				if (got > 0) {
+					end += size_t(got);
+					assert(sanity_check());
+				}
+			}
+		}
+
+		parse_buffer() { }
+
+		~parse_buffer() { free(); }
+	};
+
+	struct compiler {
+		compiler() {
+		}
+
+		~compiler() {
+		}
+
+		void set_source_cb(refill_function_t f) {
+			pb_refill = f;
+		}
+
+		void set_source_ctx(void *ptr=NULL,size_t sz=0) {
+			pb_ctx.free();
+			pb_ctx.ptr_size = sz;
+			pb_ctx.ownership = false;
+			pb_ctx.ptr = (unsigned char*)ptr;
+		}
+
+		bool alloc_source_ctx(size_t sz) {
+			return pb_ctx.alloc(sz);
+		}
+
+		void free_source_ctx(void) {
+			pb_ctx.free();
+		}
+
+		unsigned char *source_ctx(void) {
+			return pb_ctx.ptr;
+		}
+
+		size_t source_ctx_size(void) const {
+			return pb_ctx.ptr_size;
+		}
+
+		bool compile(void);
+
+		private:
+		parse_buffer		pb;
+		context_t		pb_ctx;
+		refill_function_t	pb_refill = refill_null;
+	};
+
+	enum class token_type_t {
+		none=0,
+		intval=1,
+		floatval=2,
+		comma=3,
+		semicolon
+	};
+
+	/* do not define constructor or destructor because this will be used in a union */
+	struct token_intval_t {
+		static constexpr unsigned int FL_SIGNED = (1u << 0u);
+
+		enum {
+			T_UNSPEC=0,
+			T_CHAR=1,
+			T_SHORT=2,
+			T_INT=3,
+			T_LONG=4,
+			T_LONGLONG=5
+		};
+
+		unsigned char				flags = FL_SIGNED;
+		unsigned char				itype = T_UNSPEC;
+		union {
+			unsigned long long		u;
+			signed long long		v;
+		} v;
+	};
+
+	/* do not define constructor or destructor because this will be used in a union */
+	struct token_floatval_t {
+		enum {
+			T_UNSPEC=0,
+			T_HALF=1,
+			T_FLOAT=2,
+			T_DOUBLE=3,
+			T_LONGDOUBLE=4
+		};
+
+		unsigned char				ftype = T_UNSPEC;
+		long double				val = std::numeric_limits<long double>::quiet_NaN();
+	};
+
+	struct token_t {
+		token_type_t		type = token_type_t::none;
+
+		union token_value_t {
+			token_intval_t		intval;		// type == intval
+			token_floatval_t	floatval;	// type == floatval
+		};
+
+		token_t() { }
+		~token_t() { }
+	};
+
+	bool compiler::compile(void) {
+		if (!pb.is_alloc()) {
+			if (!pb.alloc(4096))
+				return false;
+		}
+
+		assert(pb.sanity_check());
+		pb.lazy_flush();
+		pb.refill(pb_refill,pb_ctx);
+		assert(pb.sanity_check());
+		while (!pb.eof()) {
+			while (pb.read < pb.end) {
+				write(1,pb.read,1);
+				pb.read++;
+			}
+
+			assert(pb.sanity_check());
+			pb.lazy_flush();
+			pb.refill(pb_refill,pb_ctx);
+			assert(pb.sanity_check());
+		}
+
+		return true;
+	}
+
+}
+
+///////////////////////////////
+struct src_ctx {
+	int				fd = -1;
+
+	void close(void) {
+		if (fd >= 0) ::close(fd);
+		fd = -1;
+	}
+
+	src_ctx() { }
+	~src_ctx() { close(); }
+};
+
+static int refill_fdio(unsigned char *at,size_t sz,void *context,size_t context_size) {
+	assert(context_size >= sizeof(src_ctx));
+	assert(context != NULL);
+	src_ctx *ctx = (src_ctx*)context;
+	return read(ctx->fd,at,sz);
+}
+
+int main(int argc,char **argv) {
+	if (argc < 2) {
+		fprintf(stderr,"Specify file\n");
+		return 1;
+	}
+
+	{
+		CIMCC::compiler cc;
+		src_ctx fdctx;
+
+		fdctx.fd = open(argv[1],O_RDONLY);
+		if (fdctx.fd < 0) {
+			fprintf(stderr,"Failed to open file\n");
+			return 1;
+		}
+		cc.set_source_cb(refill_fdio);
+		cc.set_source_ctx(&fdctx,sizeof(fdctx));
+
+		cc.compile();
+
+		/* fdctx destructor closes file descriptor */
+	}
+
+	return 0;
+}
+
