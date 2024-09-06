5ca9b93225a17946bb84e0c6ef48ff7054301393
Jonathan Campbell <jonathan@castus.tv>
2024-09-06 15:32:39 -0700

    C-style comments

--- lang/cim/cimcc.cpp
+++ lang/cim/cimcc.cpp
@@ -1737,8 +1737,31 @@ private:
 		return 1;
 	}
 
+	int eat_c_comment(unsigned int level,rbuf &buf,source_file_object &sfo) {
+		/* caller already ate the / and the * if level == 1 */
+		assert(level == 0 || level == 1);
+
+		do {
+			if (buf.data_avail() < 8) rbuf_sfd_refill(buf,sfo);
+
+			if (buf.peekb(0) == '/' && buf.peekb(1) == '*') {
+				buf.discardb(2);
+				level++;
+			}
+			else if (buf.peekb(0) == '*' && buf.peekb(1) == '/') {
+				buf.discardb(2);
+				if (--level == 0) break;
+			}
+			else {
+				buf.discardb();
+			}
+		} while(1);
+
+		return 1;
+	}
+
 	int lgtok(rbuf &buf,source_file_object &sfo,token_t &t) {
-		t = token_t();
+try_again:	t = token_t();
 
 		eat_whitespace(buf,sfo);
 		if (buf.data_avail() < 8) rbuf_sfd_refill(buf,sfo);
@@ -1770,6 +1793,7 @@ private:
 			case '/':
 				t.type = token_type_t::forwardslash; buf.discardb();
 				if (buf.peekb() == '=') { t.type = token_type_t::forwardslashequals; buf.discardb(); } /* /= */
+				else if (buf.peekb() == '*') { buf.discardb(2); eat_c_comment(1,buf,sfo); goto try_again; }
 				break;
 			case '%':
 				t.type = token_type_t::percent; buf.discardb();
