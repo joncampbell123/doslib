4b68129c7cae66d73f7a8e36e7478df24ca4dbad
Jonathan Campbell <jonathan@castus.tv>
2024-11-04 22:11:59 -0800

    more

--- windows/bethesda/hello.c
+++ windows/bethesda/hello.c
@@ -7,7 +7,19 @@
 /* Bethesda joke */
 
 int PASCAL WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine,int nCmdShow) {
-    MessageBox(NULL,"A Bethesda.net account is required to run this legacy operating system\n\nPlease connect to the internet to continue.","Always-on requirement",MB_OK|MB_ICONHAND|MB_SYSTEMMODAL);
+	(void)hInstance;
+	(void)hPrevInstance;
+	(void)lpCmdLine;
+	(void)nCmdShow;
+
+	MessageBox(
+		NULL,
+		"A Bethesda.net account is required to run this legacy operating system\n"
+		"\n"
+		"Please connect to the internet to continue.",
+		"Always-on requirement",
+		MB_OK|MB_ICONHAND|MB_SYSTEMMODAL);
+
 	return 0;
 }
 
