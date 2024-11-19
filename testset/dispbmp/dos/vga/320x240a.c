c24b2a9896043ba20a240fc37024d0dba49a5ec0
Jonathan Campbell <jonathan@castus.tv>
2024-11-19 13:14:14 -0800

    more

---
+++ testset/dispbmp/dos/vga/320x240a.c
@@ -0,0 +1,97 @@
+
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <conio.h>
+#include <stdio.h>
+#include <errno.h>
+#include <i86.h>
+
+#include "libbmp.h"
+
+/* VGA 320x240 256-color mode.
+ * Standard INT 13h mode but with unchained 256-color mode and tweaked 320x240 mode */
+
+static const char bmpfile[] = "240l8v.bmp";
+static const unsigned int img_width = 320;
+static const unsigned int img_height = 240;
+
+#include "dr_unch.h"
+
+/* CRTC mode parameters.
+ * Basically 640x480 mode applied to 256-color mode */
+static const uint16_t crtcchg[] = {
+	0x0011,		/* vertical retrace end 0x11: turn off protect */
+	0x0B06,		/* vertical total 0x06 from 640x480 */
+	0x3E07,		/* overflow 0x07 from same */
+	0x4109,		/* maximum scan line register 0x09 */
+	0xEA10,		/* vertical retrace start 0x10 */
+	0xDF12,		/* vertical display end 0x12 */
+	0xE715,		/* start vertical blanking 0x15 */
+	0x0416,		/* end vertical blanking 0x16 */
+	0x8C11		/* vertical retrace end 0x11 which also sets protect, which is why this is the last write */
+};
+
+int main() {
+	struct BMPFILEREAD *bfr;
+	unsigned int dispw,i;
+	uint16_t iobase;
+
+	bfr = open_bmp(bmpfile);
+	if (bfr == NULL) {
+		fprintf(stderr,"Failed to open BMP, errno %s\n",strerror(errno));
+		return 1;
+	}
+	if (bfr->bpp != 8 || bfr->colors == 0 || bfr->colors > 256 || bfr->palette == 0) {
+		fprintf(stderr,"BMP wrong format\n");
+		return 1;
+	}
+
+	/* set 320x200x256 VGA then tweak into unchained 256-color mode */
+	__asm {
+		mov	ax,0x0013	; AH=0x00 AL=0x13
+		int	0x10
+	}
+	/* read 0x3CC to determine color vs mono */
+	iobase = (inp(0x3CC) & 1) ? 0x3D0 : 0x3B0;
+
+	/* CRTC byte mode */
+	outpw(iobase+4,0x0014); /* underline register 0x14 turn off doubleword addressing */
+	outpw(iobase+4,0xE317); /* crtc mode control 0x17 SE=1 bytemode AW=1 DIV2=0 SLDIV=0 MAP14=1 MAP13=1 */
+	outpw(iobase+4,0x2813); /* offset register 0x13: 0x28 for 320 pixels across */
+	outpw(0x3C4,0x0604); /* sequencer memory mode: disable chain4, disable odd/even, extended memory enable */
+
+	/* CRTC mode set */
+	for (i=0;i < (sizeof(crtcchg)/sizeof(crtcchg[0]));i++)
+		outpw(iobase+4,crtcchg[i]);
+
+	/* set palette */
+	outp(0x3C8,0);
+	for (i=0;i < bfr->colors;i++) {
+		outp(0x3C9,bfr->palette[i].rgbRed >> 2);
+		outp(0x3C9,bfr->palette[i].rgbGreen >> 2);
+		outp(0x3C9,bfr->palette[i].rgbBlue >> 2);
+	}
+
+	/* load and render */
+	dispw = bfr->width;
+	if (dispw > img_width) dispw = img_width;
+	while (read_bmp_line(bfr) == 0)
+		draw_scanline((unsigned int)bfr->current_line,bfr->scanline,dispw);
+
+	/* done reading */
+	close_bmp(&bfr);
+
+	/* wait for ENTER */
+	while (getch() != 13);
+
+	/* restore text */
+	__asm {
+		mov	ax,0x0003	; AH=0x00 AL=0x03
+		int	0x10
+	}
+	return 0;
+}
+
--- testset/dispbmp/dos/vga/common.mak
+++ testset/dispbmp/dos/vga/common.mak
@@ -17,6 +17,7 @@ $(LIBBMP_LIB):
 ! ifndef TARGET_WINDOWS
 320X200A_EXE =     $(SUBDIR)$(HPS)320x200a.$(EXEEXT)
 320X200B_EXE =     $(SUBDIR)$(HPS)320x200b.$(EXEEXT)
+320X240A_EXE =     $(SUBDIR)$(HPS)320x240a.$(EXEEXT)
 ! endif
 !endif
 
@@ -33,7 +34,7 @@ all: $(OMFSEGDG) lib exe
        
 lib: $(LIBBMP_LIB) .symbolic
 	
-exe: $(320X200A_EXE) $(320X200B_EXE) .symbolic
+exe: $(320X200A_EXE) $(320X200B_EXE) $(320X240A_EXE) .symbolic
 
 !ifdef 320X200A_EXE
 $(320X200A_EXE): $(LIBBMP_LIB) $(SUBDIR)$(HPS)320x200a.obj
@@ -48,6 +49,14 @@ $(320X200B_EXE): $(LIBBMP_LIB) $(SUBDIR)$(HPS)320x200b.obj
 	%write tmp.cmd option quiet option map=$(320X200B_EXE).map system $(WLINK_CON_SYSTEM) library $(LIBBMP_LIB) file $(SUBDIR)$(HPS)320x200b.obj name $(320X200B_EXE)
 	@wlink @tmp.cmd
 	@$(COPY) ..$(HPS)..$(HPS)img$(HPS)256$(HPS)w320$(HPS)200vga.bmp $(SUBDIR)$(HPS)200l8v.bmp
+	@$(COPY) ..$(HPS)..$(HPS)..$(HPS)..$(HPS)dos32a.dat $(SUBDIR)$(HPS)dos4gw.exe
+!endif
+
+!ifdef 320X240A_EXE
+$(320X240A_EXE): $(LIBBMP_LIB) $(SUBDIR)$(HPS)320x240a.obj
+	%write tmp.cmd option quiet option map=$(320X240A_EXE).map system $(WLINK_CON_SYSTEM) library $(LIBBMP_LIB) file $(SUBDIR)$(HPS)320x240a.obj name $(320X240A_EXE)
+	@wlink @tmp.cmd
+	@$(COPY) ..$(HPS)..$(HPS)img$(HPS)256$(HPS)w320$(HPS)240vga.bmp $(SUBDIR)$(HPS)240l8v.bmp
 	@$(COPY) ..$(HPS)..$(HPS)..$(HPS)..$(HPS)dos32a.dat $(SUBDIR)$(HPS)dos4gw.exe
 !endif
 
