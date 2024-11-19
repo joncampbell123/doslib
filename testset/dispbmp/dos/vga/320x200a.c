f8106eca0ad38b23070f19b082c36a04dd217dd5
Jonathan Campbell <jonathan@castus.tv>
2024-11-19 13:02:27 -0800

    more

--- testset/dispbmp/dos/vga/320x200a.c
+++ testset/dispbmp/dos/vga/320x200a.c
@@ -18,17 +18,7 @@ static const char bmpfile[] = "200l8v.bmp";
 static const unsigned int img_width = 320;
 static const unsigned int img_height = 200;
 
-static void draw_scanline(unsigned int y,unsigned char *src,unsigned int pixels) {
-	if (y < img_height) {
-#if TARGET_MSDOS == 32
-		unsigned char *d = (unsigned char*)0xA0000 + (y * img_width);
-		memcpy(d,src,pixels);
-#else
-		unsigned char far *d = MK_FP(0xA000,y * img_width);
-		_fmemcpy(d,src,pixels);
-#endif
-	}
-}
+#include "dr_ch.h"
 
 int main() {
 	struct BMPFILEREAD *bfr;
--- testset/dispbmp/dos/vga/320x200b.c
+++ testset/dispbmp/dos/vga/320x200b.c
@@ -18,22 +18,7 @@ static const char bmpfile[] = "200l8v.bmp";
 static const unsigned int img_width = 320;
 static const unsigned int img_height = 200;
 
-static void draw_scanline(unsigned int y,unsigned char *src,unsigned int pixels) {
-	if (y < img_height) {
-#if TARGET_MSDOS == 32
-		unsigned char *d = (unsigned char*)0xA0000 + (y * (img_width >> 2u));
-#else
-		unsigned char far *d = MK_FP(0xA000,y * (img_width >> 2u));
-#endif
-		unsigned char bitplane;
-		unsigned int x,o;
-
-		for (bitplane=0;bitplane < 4;bitplane++) {
-			outpw(0x3C4,0x0002 + (0x100 << bitplane)); /* sequencer map mask */
-			o = 0; for (x=bitplane;x < pixels;x += 4) d[o++] = src[x];
-		}
-	}
-}
+#include "dr_unch.h"
 
 int main() {
 	struct BMPFILEREAD *bfr;
