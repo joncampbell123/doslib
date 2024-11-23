3f6f52c1e45e885bb7ce3fa17249afb194036f76
Jonathan Campbell <jonathan@castus.tv>
2024-11-22 21:36:52 -0800

    more

---
+++ testset/dispbmp/dos/vga/640x480m.c
@@ -0,0 +1,88 @@
+
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <conio.h>
+#include <stdio.h>
+#include <errno.h>
+#include <i86.h>
+
+#include "libbmp.h"
+
+/* VGA 640x480 16-color mode.
+ * Standard INT 13h mode without any tweaks. */
+
+static const char bmpfile[] = "480m.bmp";
+static const unsigned int img_width = 640;
+static const unsigned int img_height = 480;
+static const unsigned int img_stride = 640 / 8;
+
+#include "dr_1bp.h"
+#include "dr_aci.h"
+
+int main() {
+	struct BMPFILEREAD *bfr;
+	unsigned int dispw,i;
+
+	bfr = open_bmp(bmpfile);
+	if (bfr == NULL) {
+		fprintf(stderr,"Failed to open BMP, errno %s\n",strerror(errno));
+		return 1;
+	}
+	if (bfr->bpp != 1 || bfr->colors == 0 || bfr->colors > 2 || bfr->palette == 0) {
+		fprintf(stderr,"BMP wrong format\n");
+		return 1;
+	}
+
+	/* set 640x480x2 MCGA */
+	__asm {
+		mov	ax,0x0011	; AH=0x00 AL=0x11
+		int	0x10
+	}
+
+	/* VGA may make this mode the same as 16-color but with a mono palette.
+	 * To get what we want, some additional programming is needed. */
+	outpw(0x3C4,0x0102); /* write only bitplane 0 (map mask) */
+	outpw(0x3CE,0x0005); /* write mode 0 (read mode 0) */
+	outpw(0x3CE,0xFF08); /* write all bits */
+
+	/* enable only the first bitplane */
+	inp(0x3DA);
+	outp(0x3C0,0x12);
+	outp(0x3C0,0x01);
+	inp(0x3DA);
+	outp(0x3C0,0x20); /* reenable the display */
+
+	/* make sure AC palette is identity mapping to eliminate EGA-like default. */
+	vga_ac_identity_map();
+
+	/* set palette */
+	outp(0x3C8,0);
+	for (i=0;i < bfr->colors;i++) {
+		outp(0x3C9,bfr->palette[i].rgbRed >> 2);
+		outp(0x3C9,bfr->palette[i].rgbGreen >> 2);
+		outp(0x3C9,bfr->palette[i].rgbBlue >> 2);
+	}
+
+	/* load and render */
+	dispw = bfr->width;
+	if (dispw > img_width) dispw = img_width;
+	while (read_bmp_line(bfr) == 0)
+		draw_scanline((unsigned int)bfr->current_line,bfr->scanline,dispw);
+
+	/* done reading */
+	close_bmp(&bfr);
+
+	/* wait for ENTER */
+	while (getch() != 13);
+
+	/* restore text */
+	__asm {
+		mov	ax,0x0003	; AH=0x00 AL=0x03
+		int	0x10
+	}
+	return 0;
+}
+
