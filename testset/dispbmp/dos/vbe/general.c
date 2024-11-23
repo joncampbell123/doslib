1a2589c4c578c470d32dac11cc8796bdd8426100
Jonathan Campbell <jonathan@castus.tv>
2024-11-22 18:06:58 -0800

    Optimize 4bpp planar draw

--- testset/dispbmp/dos/vbe/general.c
+++ testset/dispbmp/dos/vbe/general.c
@@ -627,14 +627,24 @@ static inline void vga_rmw(unsigned char far *d,const unsigned char b) {
 
 #if TARGET_MSDOS == 32
 static void vga4pcpy(unsigned char *d,unsigned char *src,unsigned int pixels) {
+	register unsigned char *w;
 #else
 static void vga4pcpy(unsigned char far *d,unsigned char *src,unsigned int pixels) {
+	register unsigned char far *w;
 #endif
-	unsigned int x,b;
+	register unsigned char *s;
+	register uint16_t m;
+	unsigned int b;
 
-	for (b=0;b < 8;b++) {
-		outpw(0x3CE,0x0008 + (0x8000 >> b)); /* bit mask */
-		for (x=b;x < pixels;x += 8) vga_rmw(&d[x>>3u],(src[x>>1u] >> (((x^1u)&1u)*4u))&0xFu);
+	m = 0x8000;
+	for (b=0;b < 8 && b < pixels;b += 2) {
+		const unsigned int count = (pixels + 7u - b) >> 3u;
+		register unsigned int x;
+
+		if (count == 0u) break;
+
+		outpw(0x3CE,0x0008 + m); m >>= 1u; w = d; s = src;   x = count; do { vga_rmw(w++,*s >>  4u); s += 4; } while (--x != 0u);
+		outpw(0x3CE,0x0008 + m); m >>= 1u; w = d; s = src++; x = count; do { vga_rmw(w++,*s & 0xFu); s += 4; } while (--x != 0u);
 	}
 }
 
