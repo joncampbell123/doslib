7edc4316db5fb19d8b1bb53938654fa7723d12d3
Jonathan Campbell <jonathan@castus.tv>
2024-11-24 23:25:17 +0000

    more

---
+++ testset/dispbmp/dos/cga/640x200a.c
@@ -0,0 +1,77 @@
+
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <conio.h>
+#include <stdio.h>
+#include <errno.h>
+#include <i86.h>
+
+#include "libbmp.h"
+
+/* CGA 640x200 2-color mode.
+ * Standard INT 13h mode without any tweaks. */
+
+static const char bmpfile[] = "200m.bmp";
+static const unsigned int img_width = 640;
+static const unsigned int img_height = 200;
+static const unsigned int img_stride = 640 / 8;
+
+#include "dr_1bp.h"
+
+static void display(struct BMPFILEREAD *bfr) {
+	unsigned int dispw;
+
+	/* load and render */
+	dispw = bfr->width;
+	if (dispw > img_width) dispw = img_width;
+	while (read_bmp_line(bfr) == 0)
+		draw_scanline((unsigned int)bfr->current_line,bfr->scanline,dispw);
+}
+
+static struct BMPFILEREAD *load(const char *path) {
+	struct BMPFILEREAD *bfr;
+
+	bfr = open_bmp(path);
+	if (bfr == NULL) {
+		fprintf(stderr,"Failed to open BMP, errno %s\n",strerror(errno));
+		close_bmp(&bfr);
+		return NULL;
+	}
+	if (bfr->bpp != 1 || bfr->colors == 0 || bfr->colors > 2 || bfr->palette == 0) {
+		fprintf(stderr,"BMP wrong format\n");
+		close_bmp(&bfr);
+		return NULL;
+	}
+
+	return bfr;
+}
+
+int main() {
+	struct BMPFILEREAD *bfr;
+
+	if ((bfr=load(bmpfile)) == NULL)
+		return 1;
+
+	/* set 640x480x2 CGA */
+	__asm {
+		mov	ax,0x0006	; AH=0x00 AL=0x06
+		int	0x10
+	}
+
+	display(bfr);
+	close_bmp(&bfr);
+
+	/* wait for ENTER */
+	while (getch() != 13);
+
+	/* restore text */
+	__asm {
+		mov	ax,0x0003	; AH=0x00 AL=0x03
+		int	0x10
+	}
+	return 0;
+}
+
---
+++ testset/dispbmp/dos/cga/dr_1bp.h
@@ -0,0 +1,15 @@
+
+/* draw scanline in 1bpp cga mode */
+static void draw_scanline(unsigned int y,unsigned char *src,unsigned int pixels) {
+	if (y < img_height) {
+		const unsigned int bytes = (pixels + 7u) >> 3u;
+#if TARGET_MSDOS == 32
+		unsigned char *d = (unsigned char*)0xB8000 + ((y >> 1) * img_stride) + ((y & 1) * 0x2000);
+		memcpy(d,src,bytes);
+#else
+		unsigned char far *d = MK_FP(0xB800,((y >> 1) * img_stride) + ((y & 1) * 0x2000));
+		_fmemcpy(d,src,bytes);
+#endif
+	}
+}
+
