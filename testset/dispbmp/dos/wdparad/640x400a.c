0afb00719ec8026396e70c1c66ae3d7d6904575a
Jonathan Campbell <jonathan@castus.tv>
2024-11-25 23:05:22 -0800

    more

---
+++ testset/dispbmp/dos/wdparad/640x400a.c
@@ -0,0 +1,75 @@
+
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <conio.h>
+#include <stdio.h>
+#include <errno.h>
+#include <i86.h>
+
+#include "libbmp.h"
+
+#define PVGA1A
+
+static const char bmpfile[] = "640400_8.bmp";
+static const unsigned int img_width = 640;
+static const unsigned int img_height = 400;
+
+#include "dr_bnkl.h"
+#include "detect.h"
+
+static draw_scanline_func_t draw_scanline;
+
+int main() {
+	struct BMPFILEREAD *bfr;
+	unsigned int dispw,i;
+
+	draw_scanline = draw_scanline_pvga1a;
+
+	bfr = open_bmp(bmpfile);
+	if (bfr == NULL) {
+		fprintf(stderr,"Failed to open BMP, errno %s\n",strerror(errno));
+		return 1;
+	}
+	if (bfr->bpp != 8 || bfr->colors == 0 || bfr->colors > 256 || bfr->palette == 0) {
+		fprintf(stderr,"BMP wrong format\n");
+		return 1;
+	}
+
+	/* set 640x400x256 Tseng ET3k/ET4k */
+	__asm {
+		mov	ax,0x005E	; AH=0x00 AL=0x5E
+		int	0x10
+	}
+	pvga1a_unlock();
+
+	/* set palette */
+	outp(0x3C8,0);
+	for (i=0;i < bfr->colors;i++) {
+		outp(0x3C9,bfr->palette[i].rgbRed >> 2);
+		outp(0x3C9,bfr->palette[i].rgbGreen >> 2);
+		outp(0x3C9,bfr->palette[i].rgbBlue >> 2);
+	}
+
+	/* load and render */
+	dispw = bfr->width;
+	if (dispw > img_width) dispw = img_width;
+	while (read_bmp_line(bfr) == 0)
+		draw_scanline((unsigned int)bfr->current_line,bfr->scanline,dispw);
+
+	/* done reading */
+	close_bmp(&bfr);
+
+	/* wait for ENTER */
+	while (getch() != 13);
+
+	/* restore text */
+	__asm {
+		mov	ax,0x0003	; AH=0x00 AL=0x03
+		int	0x10
+	}
+	return 0;
+}
+
