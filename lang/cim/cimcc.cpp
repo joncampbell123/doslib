5815f21e7b368c34040ffb5c477a3315aa28bb64
Jonathan Campbell <jonathan@castus.tv>
2025-03-14 20:17:05 -0700

    binary xor

--- lang/cim/cimcc.cpp
+++ lang/cim/cimcc.cpp
@@ -751,6 +751,7 @@ namespace CIMCC/*TODO: Pick a different name by final release*/ {
 		op_logical_or,				// 150
 		op_logical_and,
 		op_binary_or,
+		op_binary_xor,
 
 		__MAX__
 	};
@@ -1297,7 +1298,8 @@ namespace CIMCC/*TODO: Pick a different name by final release*/ {
 		"op:comma",
 		"op:log-or",				// 150
 		"op:log-and",
-		"op:bin-or"
+		"op:bin-or",
+		"op:bin-xor"
 	};
 
 	static const char *token_type_t_str(const token_type_t t) {
@@ -5232,10 +5234,24 @@ try_again_w_token:
 	int exclusive_or_expression(cc_state_t &cc,ast_node_id_t &aroot) {
 		int r;
 
-		/* TODO: exclusive_or_expression '^' and_expression */
-
 		if ((r=primary_expression(cc,aroot)) < 1)
 			return r;
+
+		while (cc.tq_peek().type == token_type_t::caret) {
+			cc.tq_discard();
+
+			ast_node_id_t expr1 = aroot; aroot = ast_node_none;
+
+			aroot = ast_node_alloc();
+			ast_node(aroot).t = token_t(token_type_t::op_binary_xor);
+			ast_node(aroot).set_child(expr1); ast_node(expr1).release();
+
+			ast_node_id_t expr2 = ast_node_none;
+			if ((r=primary_expression(cc,expr2)) < 1)
+				return r;
+
+			ast_node(expr1).set_next(expr2); ast_node(expr2).release();
+		}
 
 		return 1;
 	}
