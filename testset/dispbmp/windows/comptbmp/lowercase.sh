577122bdec8be1bb473cea999c4600026b11dfb3
Jonathan Campbell <jonathan@castus.tv>
2025-08-03 10:07:53 -0700

    more

--- testset/dispbmp/windows/comptbmp/lowercase.sh
+++ testset/dispbmp/windows/comptbmp/lowercase.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 for file in *.{ico,ICO,rc,RC} RESOURCE.H resource.h; do
-	lc=`echo "$file" | tr '[:upper:]' '[:lower:]'`
-	if [ "$file" == "$lc" ]; then
-		true
-	else
-		mv -v "$file" "$lc" || exit 1
+	if [ -f "$file" ]; then
+		lc=`echo "$file" | tr '[:upper:]' '[:lower:]'`
+		if [ "$file" == "$lc" ]; then
+			true
+		else
+			mv -v "$file" "$lc" || exit 1
+		fi
 	fi
 done
 
--- testset/dispbmp/windows/comptbmp/resource.h
+++ testset/dispbmp/windows/comptbmp/resource.h
@@ -1,17 +1,4 @@
-//{{NO_DEPENDENCIES}}
-// App Studio generated include file.
-// Used by GENERAL1.RC
-//
-#define IDI_APPICON                     101
 
-// Next default values for new objects
-// 
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
+#define IDD_ABOUT                       102
+#define IDC_STATIC                      -1
 
-#define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1000
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
