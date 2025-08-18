fd7f6a9b3821264adcb377fe6832a09944a43eca
Jonathan Campbell <jonathan@castus.tv>
2025-08-18 14:55:42 -0700

    color palette matching, test

--- tool/palq/octt1.c
+++ tool/palq/octt1.c
@@ -14,12 +14,34 @@
 #include "libbmp.h"
 
 struct rgb_t {
-	unsigned int	r,g,b;
+	unsigned char	r,g,b;
 };
+
+unsigned char map2palette(unsigned char r,unsigned char g,unsigned char b,struct rgb_t *pal,unsigned int colors) {
+	unsigned int mdst = ~0u,dst,col = 0,i = 0;
+
+	while (i < colors) {
+		dst  = (unsigned int)abs((int)r - (int)(pal->r));
+		dst += (unsigned int)abs((int)g - (int)(pal->g));
+		dst += (unsigned int)abs((int)b - (int)(pal->b));
+
+		if (mdst > dst) {
+			mdst = dst;
+			col = i;
+		}
+
+		pal++;
+		i++;
+	}
+
+	return col;
+}
 
 int main(int argc,char **argv) {
 	struct BMPFILEIMAGE *membmp = NULL;
+	struct BMPFILEIMAGE *memdst = NULL;
 	unsigned int target_colors = 256;
+	unsigned char paldepthbits = 8;
 	struct rgb_t *palette;
 	unsigned int i;
 
@@ -30,12 +52,26 @@ int main(int argc,char **argv) {
 	if (!membmp)
 		return 1;
 
+	memdst = bmpfileimage_alloc();
+	if (!memdst)
+		return 1;
+
 	if (argc > 3) {
-		fprintf(stderr,"%s\n",argv[3]);
-		target_colors = strtoul(argv[3],NULL,0);
+		char *s = argv[3];
+
+		target_colors = strtoul(s,&s,0);
 		if (target_colors == 0u || target_colors > 256u)
 			return 1;
+
+		if (*s == ':') {
+			s++;
+			paldepthbits = strtoul(s,&s,0);
+			if (paldepthbits == 0 || paldepthbits > 8)
+				return 1;
+		}
 	}
+
+	fprintf(stderr,"Quant to %u colors with %u bits per palette entry\n",target_colors,paldepthbits);
 
 	palette = malloc(sizeof(struct rgb_t) * 256);
 	if (!palette)
@@ -77,15 +113,47 @@ int main(int argc,char **argv) {
 		close_bmp(&bfr);
 	}
 
-#if defined(LINUX)
-	srand(time(NULL));
-#endif
+	memdst->bpp = 8;
+	memdst->width = membmp->width;
+	memdst->height = membmp->height;
 
-	/* random palette */
+	if (bmpfileimage_alloc_image(memdst)) {
+		fprintf(stderr,"Failed to allocate memory\n");
+		return 1;
+	}
+
+	/* test palette, a 6x8x5 color cube */
 	for (i=0;i < target_colors;i++) {
-		palette[i].r = (unsigned char)rand();
-		palette[i].g = (unsigned char)rand();
-		palette[i].b = (unsigned char)rand();
+		palette[i].r = (unsigned char)(((i % 6u) * 255u) / 5u);
+		palette[i].g = (unsigned char)((((i / 6u) % 8u) * 255u) / 7u);
+		palette[i].b = (unsigned char)(((i / 6u / 8u) * 255u) / 4u);
+	}
+
+	/* quant palette */
+	if (paldepthbits < 8) {
+		const unsigned char msk = (0xFF00u >> paldepthbits) & 0xFFu;
+		for (i=0;i < target_colors;i++) {
+			palette[i].r &= msk;
+			palette[i].g &= msk;
+			palette[i].b &= msk;
+		}
+	}
+
+	/* convert 24bpp to 8bpp image */
+	{
+		unsigned char *s24,*d8;
+		unsigned int x,y;
+
+		for (y=0;y < membmp->height;y++) {
+			s24 = bmpfileimage_row(membmp,y);
+			d8 = bmpfileimage_row(memdst,y);
+			assert(d8 != NULL && s24 != NULL);
+
+			for (x=0;x < membmp->width;x++) {
+				*d8++ = map2palette(s24[2],s24[1],s24[0],palette,target_colors); /* Windows BMPs are BGR order */
+				s24 += 3;
+			}
+		}
 	}
 
 	/* write it out */
@@ -125,10 +193,10 @@ int main(int argc,char **argv) {
 		}
 
 		for (y=0;y < membmp->height;y++) {
-			s = bmpfileimage_row(membmp,bfw->current_line);
+			s = bmpfileimage_row(memdst,bfw->current_line);
 			assert(s != NULL);
 
-			if (write_bmp_line(bfw,s,membmp->stride)) {
+			if (write_bmp_line(bfw,s,bfw->stride)) {
 				fprintf(stderr,"Scanline write err\n");
 				return 1;
 			}
@@ -139,6 +207,7 @@ int main(int argc,char **argv) {
 
 	/* free bitmap */
 	bmpfileimage_free(&membmp);
+	bmpfileimage_free(&memdst);
 	free(palette);
 	return 0;
 }
