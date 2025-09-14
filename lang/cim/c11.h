9d91996b4ce5056f3868cb263fd9b7ea7e64e149
Jonathan Campbell <jonathan@castus.tv>
2025-09-14 10:44:03 -0700

    more

--- lang/cim/c11.h
+++ lang/cim/c11.h
@@ -13,7 +13,7 @@ struct c11yy_struct_base {
 };
 
 struct c11yy_struct_integer {
-	enum c11yytokentype			t; /* from c11.y.h */
+	enum c11yytokentype			t; /* from c11.y.h == I_CONSTANT */
 	union {
 		uint64_t			u;
 		int64_t				s;
@@ -25,16 +25,30 @@ struct c11yy_struct_integer {
 #define C11YY_FLOATF_SPECIAL			(1u << 1u) /* i.e. NaN, inf, etc */
 
 struct c11yy_struct_float {
-	enum c11yytokentype			t; /* from c11.y.h */
+	enum c11yytokentype			t; /* from c11.y.h == F_CONSTANT */
 	uint64_t				mant;
 	int					exponent;
 	uint8_t					flags;
 	uint8_t					sz; /* 0 if no size, else in bits */
 };
 
+enum c11yystringtype {
+	C11YY_STRT_LOCAL=0,			// no prefix given, in whatever non-unicode locale
+	C11YY_STRT_UTF8,			// UTF-8 encoding
+	C11YY_STRT_UTF16,			// UTF-16 encoding
+	C11YY_STRT_UTF32			// UTF-32 encoding
+};
+
+struct c11yy_struct_strliteral {
+	enum c11yytokentype			t; /* from c11.y.h == STRING_LITERAL */
+	c11yy_string_token_id			id;
+	enum c11yystringtype			stype;
+};
+
 union c11yy_struct {
-	struct c11yy_struct_base		b;
+	struct c11yy_struct_base		base;
 	struct c11yy_struct_integer		intval;
 	struct c11yy_struct_float		floatval;
+	struct c11yy_struct_strliteral		strlitval;
 };
 
