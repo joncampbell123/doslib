04a9a3ba10495df921317f1fe0245cac741d2d33
Jonathan Campbell <jonathan@castus.tv>
2025-09-20 21:54:37 -0700

    more

--- lang/cim/c11iconst_readc.cpp
+++ lang/cim/c11iconst_readc.cpp
@@ -7,10 +7,8 @@ extern "C" {
 
 int c11yy_iconst_readc(const unsigned int base,const char* &s) {
 	const int v = c11yy_char2digit(*s);
-
 	if ((unsigned int)v >= base) return -1; /* do not increment s */
-	else if (v >= 0) s++; /* v < 0, do not increment s, else, increment s */
-
+	if (v >= 0) s++; /* v < 0, do not increment s, else, increment s */
 	return v;
 }
 
