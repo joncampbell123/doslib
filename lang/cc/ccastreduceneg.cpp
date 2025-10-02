522704f60241c0a9bfd770da68fb06e922c848cc
Jonathan Campbell <jonathan@castus.tv>
2025-10-02 15:35:57 -0700

    more

--- lang/cc/ccastreduceneg.cpp
+++ lang/cc/ccastreduceneg.cpp
@@ -22,10 +22,21 @@ static bool ast_constexpr_negate_integer(token_t &tr,const token_t &top) {
 	return true;
 }
 
+static bool ast_constexpr_negate_floating(token_t &tr,const token_t &top) {
+	const struct floating_value_t &op = top.v.floating;
+	struct floating_value_t &r = tr.v.floating;
+
+	tr = top;
+	r.flags ^= floating_value_t::FL_NEGATIVE;
+	return true;
+}
+
 bool ast_constexpr_negate(token_t &r,const token_t &op) {
 	switch (op.type) {
 		case token_type_t::integer:
 			return ast_constexpr_negate_integer(r,op);
+		case token_type_t::floating:
+			return ast_constexpr_negate_floating(r,op);
 		default:
 			break;
 	};
