aefb9eaa9d76cd38f8ecd0c1c93e41dda6ba861f
Jonathan Campbell <jonathan@castus.tv>
2025-10-01 22:24:27 -0700

    more

--- lang/cc/ccminic.cpp
+++ lang/cc/ccminic.cpp
@@ -12,47 +12,6 @@
 
 #include "cc.hpp"
 
-bool declaration_specifiers_t::empty(void) const {
-	return 	storage_class == 0 && type_specifier == 0 && type_qualifier == 0 &&
-		type_identifier_symbol == symbol_none && enum_list.empty() && count == 0 &&
-		align == addrmask_none && dcs_flags == 0 && size == data_size_none;
-}
-
-declaration_specifiers_t::declaration_specifiers_t() { }
-declaration_specifiers_t::declaration_specifiers_t(const declaration_specifiers_t &x) { common_copy(x); }
-declaration_specifiers_t &declaration_specifiers_t::operator=(const declaration_specifiers_t &x) { common_copy(x); return *this; }
-declaration_specifiers_t::declaration_specifiers_t(declaration_specifiers_t &&x) { common_move(x); }
-declaration_specifiers_t &declaration_specifiers_t::operator=(declaration_specifiers_t &&x) { common_move(x); return *this; }
-
-declaration_specifiers_t::~declaration_specifiers_t() {
-}
-
-void declaration_specifiers_t::common_move(declaration_specifiers_t &o) {
-	storage_class = o.storage_class; o.storage_class = 0;
-	type_specifier = o.type_specifier; o.type_specifier = 0;
-	type_qualifier = o.type_qualifier; o.type_qualifier = 0;
-	type_identifier_symbol = o.type_identifier_symbol;
-	size = o.size; o.size = data_size_none;
-	align = o.align; o.align = addrmask_none;
-	dcs_flags = o.dcs_flags; o.dcs_flags = 0;
-	enum_list = std::move(o.enum_list);
-	count = o.count; o.count = 0;
-}
-
-void declaration_specifiers_t::common_copy(const declaration_specifiers_t &o) {
-	storage_class = o.storage_class;
-	type_specifier = o.type_specifier;
-	type_qualifier = o.type_qualifier;
-	type_identifier_symbol = o.type_identifier_symbol;
-	size = o.size;
-	align = o.align;
-	dcs_flags = o.dcs_flags;
-	enum_list = o.enum_list;
-	count = o.count;
-}
-
-//////////////////////////////////////////////////////////////////////////////
-
 bool pointer_t::operator==(const pointer_t &o) const {
 	return tq == o.tq;
 }
