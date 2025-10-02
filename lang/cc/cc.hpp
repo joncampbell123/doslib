2e077dfa48563065895fc0d6361c6a370f258ef5
Jonathan Campbell <jonathan@castus.tv>
2025-10-02 13:01:03 -0700

    more

--- lang/cc/cc.hpp
+++ lang/cc/cc.hpp
@@ -1455,6 +1455,9 @@ bool ast_constexpr_logical_and(token_t &r,token_t &op1,token_t &op2);
 
 bool ast_constexpr_negate(token_t &r,const token_t &op);
 
+bool ast_constexpr_add(token_t &r,const token_t &op1,const token_t &op2);
+bool ast_constexpr_subtract(token_t &r,const token_t &op1,const token_t &op2);
+
 //////////////////////////////////////////////////////////////////////////////
 
 void debug_dump_ast(const std::string prefix,ast_node_id_t r);
--- lang/cc/ccastreduce.cpp
+++ lang/cc/ccastreduce.cpp
@@ -12,47 +12,6 @@
 
 #include "cc.hpp"
 
-static bool ast_constexpr_add_integer(token_t &tr,const token_t &top1,const token_t &top2) {
-	const struct integer_value_t &op1 = top1.v.integer;
-	const struct integer_value_t &op2 = top2.v.integer;
-	struct integer_value_t &r = tr.v.integer;
-
-	tr = top1;
-	r.v.u = op1.v.u + op2.v.u;
-	if (r.v.u < op1.v.u) r.flags |= integer_value_t::FL_OVERFLOW;
-	r.flags |= op2.flags & integer_value_t::FL_SIGNED;
-	return true;
-}
-
-bool ast_constexpr_add(token_t &r,token_t &op1,token_t &op2) {
-	if (op1.type == op2.type) {
-		switch (op1.type) {
-			case token_type_t::integer:
-				return ast_constexpr_add_integer(r,op1,op2);
-			default:
-				break;
-		};
-	}
-
-	return false;
-}
-
-bool ast_constexpr_subtract(token_t &r,token_t &op1,token_t &op2) {
-	/* TODO: type promotion/conversion */
-	if (op1.type == op2.type) {
-		switch (op1.type) {
-			case token_type_t::integer:
-				r = op1;
-				r.v.integer.v.u -= op2.v.integer.v.u;
-				return true;
-			default:
-				break;
-		};
-	}
-
-	return false;
-}
-
 bool ast_constexpr_multiply(token_t &r,token_t &op1,token_t &op2) {
 	/* TODO: type promotion/conversion */
 	if (op1.type == op2.type) {
