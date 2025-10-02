f25b29d2f2e97ba7ca4295243ee1ce006f5bcfa6
Jonathan Campbell <jonathan@castus.tv>
2025-10-02 09:18:31 -0700

    more

--- lang/cc/ccastreducealignof.cpp
+++ lang/cc/ccastreducealignof.cpp
@@ -12,41 +12,46 @@
 
 #include "cc.hpp"
 
+static bool ast_constexpr_alignof_calc_common(token_t &r,declaration_specifiers_t &spec,ddip_list_t &ddip,size_t ptr_deref=0) {
+	data_size_t sz = calc_alignof(spec,ddip,ptr_deref);
+	if (sz != data_size_none) {
+		r = token_t(token_type_t::integer);
+		r.v.integer.flags = 0;
+		r.v.integer.v.u = sz;
+		return true;
+	}
+
+	return false;
+}
+
+static bool ast_constexpr_alignof_symbol(token_t &r,const symbol_id_t symbol_id,size_t ptr_deref) {
+	if (symbol_id != symbol_none) {
+		auto &sym = symbol(symbol_id);
+		if (sym.sym_type == symbol_t::FUNCTION || sym.sym_type == symbol_t::NONE)
+			return false;
+
+		return ast_constexpr_alignof_calc_common(r,sym.spec,sym.ddip,ptr_deref);
+	}
+
+	return false;
+}
+
+static bool ast_constexpr_alignof_declaration(token_t &r,declaration_t *decl) {
+	if (decl) {
+		if (decl->declor.size() == 1) {
+			return ast_constexpr_alignof_calc_common(r,decl->spec,decl->declor[0].ddip);
+		}
+	}
+
+	return false;
+}
+
 bool ast_constexpr_alignof(token_t &r,token_t &op,size_t ptr_deref) {
 	switch (op.type) {
 		case token_type_t::op_symbol:
-			{
-				if (op.v.symbol != symbol_none) {
-					auto &sym = symbol(op.v.symbol);
-					if (sym.sym_type == symbol_t::FUNCTION || sym.sym_type == symbol_t::NONE)
-						return false;
-
-					addrmask_t sz = calc_alignof(sym.spec,sym.ddip,ptr_deref);
-					if (sz != addrmask_none) {
-						r = token_t(token_type_t::integer);
-						r.v.integer.v.u = sz;
-						r.v.integer.flags = 0;
-						return true;
-					}
-				}
-				break;
-			}
+			return ast_constexpr_alignof_symbol(r,op.v.symbol,ptr_deref);
 		case token_type_t::op_declaration:
-			{
-				declaration_t *decl = op.v.declaration;
-				assert(decl != NULL);
-
-				if (decl->declor.size() == 1) {
-					addrmask_t sz = calc_alignof(decl->spec,decl->declor[0].ddip);
-					if (sz != addrmask_none) {
-						r = token_t(token_type_t::integer);
-						r.v.integer.v.u = sz;
-						r.v.integer.flags = 0;
-						return true;
-					}
-				}
-				break;
-			}
+			return ast_constexpr_alignof_declaration(r,op.v.declaration);
 		default:
 			break;
 	};
