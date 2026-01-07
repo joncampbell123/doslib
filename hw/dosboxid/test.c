92c2509be47f0ae4d31286a696b935f1fed4734e
Jonathan Campbell <jonathan@castus.tv>
2026-01-07 15:13:23 -0800

    vmemsize

--- hw/dosboxid/test.c
+++ hw/dosboxid/test.c
@@ -145,15 +145,20 @@ int main(int argc,char **argv,char **envp) {
 		printf("\n");
 	}
 	{
-		uint32_t caps;
+		uint32_t caps,memsz;
 
 		dosbox_id_write_regsel(DOSBOX_ID_REG_VGAIG_CAPS);
 		caps = dosbox_id_read_data();
+
+		dosbox_id_write_regsel(DOSBOX_ID_REG_VGAIG_VRAMSIZE);
+		memsz = dosbox_id_read_data();
 
 		printf("DOSBox Integrated Graphics caps: ");
 		if (caps & DOSBOX_ID_REG_VGA1G_CAPS_ENABLED) printf("enabled");
 		else printf("not-available");
 		printf("\n");
+
+		printf("DOSBox vmemsize: %luKB (0x%lx)",(unsigned long)memsz / 1024ul,(unsigned long)memsz);
 	}
 
 	return 0;
