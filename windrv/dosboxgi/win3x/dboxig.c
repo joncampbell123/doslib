e6e509a67f159c92f8f8de4a93f99f8aa5d941d4
Jonathan Campbell <jonathan@castus.tv>
2026-01-26 08:46:47 -0800

    32-bit long printf

--- windrv/dosboxgi/win3x/dboxig.c
+++ windrv/dosboxgi/win3x/dboxig.c
@@ -35,6 +35,40 @@ static char debug_tmp[128],*debug_tw;
 
 static const char *debug_hexes = "0123456789ABCDEF";
 
+static unsigned int DEBUG_uldiv(unsigned long *v,const unsigned int d);
+/* long division.
+ * H = upper 16 bits of v
+ * L = lower 16 bits of v
+ *
+ *     hq  lq         remainder lr
+ *   ________
+ * d ) H   L
+ *                    hq = H / D
+ *                    hr = H % D
+ *     hr  L          next, work with 32-bit value (hr:L), which is (16-bit hr:16-bit L) combined into 32 bits
+ *                    lq = (hr:L) / D
+ *                    lr = (hr:L) % D
+ *     hr  lr
+ *
+ * On x86 processors, in 16-bit mode, DIV divides DX:AX by whatever memory or CPU register you specify,
+ * and if the result is small enough to fit into 16 bits, DX is the remainder and AX is the quotient.
+ * At each DIV, the upper 16 bits (DX) used in the divide is the remainder from the previous DIV.
+ * Remainder is returned from DX by the function.
+ */
+#pragma aux DEBUG_uldiv = \
+    "xor dx,dx" \
+     \
+    "mov ax,WORD PTR es:[si+2]" \
+    "div cx" \
+    "mov WORD PTR es:[si+2],ax" \
+    \
+    "mov ax,WORD PTR es:[si]" \
+    "div cx" \
+    "mov WORD PTR es:[si],ax" \
+    parm [es si] [cx] \
+    modify [dx ax] \
+    value [dx];
+
 static void DEBUG_OUTstr(const char *str) {
     unsigned char c;
 
@@ -44,6 +78,11 @@ static void DEBUG_OUTstr(const char *str) {
 static void DEBUG_OUTuint(unsigned int v) { /* set debug_tw = debug_tmp_LAST, *debug_tmp = 0, then call */
     *(--debug_tw) = (char)(v % 10u) + '0'; v /= 10u;
     while (v != 0u) { *(--debug_tw) = (char)(v % 10u) + '0'; v /= 10u; }
+}
+
+static void DEBUG_OUTulint(unsigned long v) { /* set debug_tw = debug_tmp_LAST, *debug_tmp = 0, then call */
+    *(--debug_tw) = (char)DEBUG_uldiv(&v,10u) + '0';
+    while (v != 0u) { *(--debug_tw) = (char)DEBUG_uldiv(&v,10u) + '0'; }
 }
 
 static void DEBUG_OUTuhex(unsigned int v) { /* set debug_tw = debug_tmp_LAST, *debug_tmp = 0, then call */
@@ -75,6 +114,19 @@ void DEBUG_OUTF(const char *fmt,...) {
                     if ((c=((unsigned char)(*fmt++))) == 0)
                         break;
                     switch (c) {
+                        case 'd': {
+                            long v = va_arg(va,long);
+                            if (v < 0) {
+                                v = -v;
+                                dosbox_id_write_data_nrl_u8('-');
+                            }
+                            debug_tw = debug_tmp_LAST; *debug_tw = 0; DEBUG_OUTulint((unsigned long)v);
+                            DEBUG_OUTstr(debug_tw);
+                            break; }
+                        case 'u': {
+                            debug_tw = debug_tmp_LAST; *debug_tw = 0; DEBUG_OUTulint(va_arg(va,unsigned long));
+                            DEBUG_OUTstr(debug_tw);
+                            break; }
                         case 'x': {
                             debug_tw = debug_tmp_LAST; *debug_tw = 0; DEBUG_OUTulhex(va_arg(va,unsigned long));
                             DEBUG_OUTstr(debug_tw);
@@ -129,7 +181,7 @@ int init_dosbox_ig(void) {
     dosbox_id_write_regsel(DOSBOX_ID_REG_GET_VGA_MEMSIZE);
     vga_memsize = dosbox_id_read_data();
 
-    DEBUG_OUTF("VGA lfb=0x%lx memsize=0x%lx(%uMB)\n",vga_lfb,vga_memsize,(unsigned int)((vga_memsize + 0x7FFFFul) >> 20ul));
+    DEBUG_OUTF("VGA lfb=0x%lx memsize=0x%lx(%lu)\n",(unsigned long)vga_lfb,(unsigned long)vga_memsize,(unsigned long)vga_memsize);
     if (!vga_memsize) return 0;
 
     return 1;
